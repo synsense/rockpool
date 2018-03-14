9243d7ec3840ecced0c05982c3b8169f10fec650
Dylan Richard Muir <dylan.richard.muir@gmail.com>
2018-03-14 11:46:01 +0100

    Updated unitary eigenvalue net generation (matlab)

--- ReservoirGenerationMatlab/RandomEIUnitaryEigenvalueNet.m
+++ ReservoirGenerationMatlab/RandomEIUnitaryEigenvalueNet.m
@@ -1,10 +1,6 @@
-function mfWHat = RandomEIUnitaryEigenvalueNet(nNumExc, nNumInh, fInhTauFactor, fInhWFactor, fhRand)
+function mfW = RandomEIUnitaryEigenvalueNet(nNumExc, nNumInh, fInhWFactor, fhRand)
 
 % - Check arguments
-if ~exist('fInhTauFactor', 'var') || isempty(fInhTauFactor)
-   fInhTauFactor = 1;
-end
-
 if ~exist('fhRand', 'var') || isempty(fhRand)
    fhRand = @(N)randn(N)./sqrt(N);
 end
@@ -26,26 +22,6 @@ vfD = diag(D) ./ abs(diag(D));
 
 vfD = complex(abs(real(vfD)), sign(imag(vfD)));
 
-vfD(real(vfD) > 1-eps) = 0;
+vfD(real(vfD) > 1-eps) = -1;
 
 mfW = real(V * diag(vfD) / V);
-
-% - Compute Jacobian
-mfJ = mfW - eye(size(mfW));
-mfJ(vnInh, :) = mfJ(vnInh, :) ./ fInhTauFactor;
-
-% - Check for unstable eigenvectors
-[V, D] = eig(mfJ);
-
-% - Identify and wipe unstable eigenvectors
-vbUnstablePartition = all(V > 0, 1);
-D(vbUnstablePartition, vbUnstablePartition) = 0;
-% fScale = max(real(diag(D(~vbUnstablePartition, ~vbUnstablePartition))));
-
-% - Reconstruct weight matrix
-mfJHat = real(V * D / V);
-mfWHat = mfJHat;
-mfWHat(vnInh, :) = mfJHat(vnInh, :) .* fInhTauFactor;
-mfWHat = mfWHat + eye(size(mfWHat));
-mfWHat = mfWHat;% ./ fScale;
-
