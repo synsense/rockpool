ce6eac9e49ec9cece14e1725d0c85ac1e3e35de4
Felix Bauer <felix.bauer@ai-ctx.com>
2018-05-24 15:49:21 +0200

    Adapted network interface for initialization and adding layers to desired form

--- NetworksPython/network.py
+++ NetworksPython/network.py
@@ -3,7 +3,7 @@ import numpy as np
 from layers import feedforward, recurrent
 
 class Network():
-    def __init__(self, fDt=1, kwInput={}, kwReservoir={}, kwOutput={}):
+    def __init__(self, lyrIn, lyrRes, lyrOut):
         """
         Create Network instance consisting of input layer, output
         layer and an arbitrary number of reservoirs. Create forward
@@ -15,95 +15,39 @@ class Network():
         
         self.__fDt = fDt
 
-        # Default parameters for layers
-        dParamsIn = {'nSize' : 1,
-                     'sKind' : 'pass'}
-        dParamsOut = {'nSize' : 1,
-                      'sKind' : 'ffrate'}
-        dParamsRes = {'nSize' : 256,
-                      # 'nReservoirs' : 1,
-                      # 'sResConn' : 'serial',
-                      'sKind' : 'reservoir'}
-        dParamsIn.update(kwInput)
-        dParamsRes.update(kwReservoir)
-        dParamsOut.update(kwOutput)
-        try:
-            dParamsRes['vTau_n'] = kwReservoir['vTau_n']  
-        except KeyError:
-            dParamsRes['vTau_n'] = np.random.rand(dParamsRes['nSize'])
-
         # Maintain set of all layers
         self.setLayers = set()
         
-        # - Generate layers
-        self.add_layer(sName='In', **dParamsIn)
-        self.add_layer(sName='Out', **dParamsOut)
-        self.add_layer(sName='Res', **dParamsRes)
-
-        # self.nReservoirs = dParamsRes.pop('nReservoirs')
-        # sResConn = dParamsRes.pop('sResConn')
-        # # Broadcast single paramters to arrays that apply to all reservoirs
-        # #   Time constants
-        # if (len(np.atleast_2d(dParamsRes['vTau_n'])) == 1
-        #       and len(dParamsRes['vTau_n']) != self.nReservoirs):
-        #     dParamsRes['vTau_n'] = np.repeat(np.atleast_2d(dParamsRes['vTau_n']),
-        #                                      self.nReservoirs, axis=0)
-        # #  All other parameters
-        # for sParam in dParamsRes.keys():
-        #     if sParam != 'vTau_n':
-        #         try:
-        #             dParamsRes[sParam] = np.full(self.nReservoirs, dParamsRes[sParam])
-        #         except ValueError:
-        #             raise ValueError('The number of arguments for {} '.format(sParam) 
-        #                              + '({0}) does not match nReservoirs ({1})'.format(
-        #                             len(dParamsRes), self.nReservoirs))
-        # for i in range(self.nReservoirs):
-        #     setattr(self, 'lyrRes{}'.format(i), 
-        #             Recurrent.RecLayer(**{s : v[i] for s, v in dParamsRes.items()}))
-    
-        # - Generate connections: Each layer except input has a set with
-        # references to its input layer
-        self.connect(source=self.lyrIn, target=self.lyrRes)
-        self.connect(source=self.lyrRes, target=self.lyrOut)
-        
-        # if sResConn == 'serial':
-        #     self.lyrRes0.setIn = {self.lyrIn}
-        #     for i in range(1, self.nReservoirs):
-        #         getattr(self, 'lyrRes{}'.format(i)).setIn = {getattr(self, 'lyrRes{}'.format(i-1))}
-        #     self.lyrOut.setIn = {getattr(self, 'lyrRes{}'.format(self.nReservoirs-1))}
-        # elif sResConn == 'parallel':
-        #     for i in Range(self.nReservoirs):
-        #         getattr(self, 'lyrRes{}'.format(i)).setIn = {self.lyrIn}
-        #     self.lyrOut.setIn = {getattr(self, 'lyrRes{}'.format(i))
-        #                        for i in range(self.nReservoirs)}
-        # else:
-        #     raise NetworkError('Connection type "{}" not understood'.format(sResConn))
-    
-    def add_layer(self, sKind, sName, **kwargs):
-        """Add feedforward or recurrent layer to network and maintain 
-        set containing all layers."""
-        sLyrName = 'lyr'+sName
+        # - Add layers
+        self.lyrIn = self.add_layer(lyrIn)
+        self.lyrRes = self.add_layer(lyrRes,  setIn={self.lyrIn})
+        self.LyrOut = self.add_layer(lyrOut,  setInIn={self.lyrRes})
+               
+    def add_layer(self, lyr, tplIn=None, tplOut=None):
+        """Add lyr to self and to self.setLayers. Its attribute name
+        is 'lyr'+lyr.sName. Check whether layer with this name 
+        already exists (replace anyway). 
+        Connect lyr to those in setIn and setOut.
+        Return lyr.
+            lyr : layer to be added to self
+            setIn : set of input layers to lyr
+            setOut : set of layers to which lyr is input layer
+        """
+        sLyrName = 'lyr' + lyr.sName
         if hasattr(self, sLyrName):
-            raise NameError('There already exists a layer with this name')
-
-        if sKind == 'ffrate':
-            setattr(self, sLyrName, FeedForward.FFRate(sName=sName, fDt=self.__fDt, **kwargs))
-            print('Rate-based feedforward layer "{}" has been added to network.'.format(sName))
-        
-        elif sKind == 'pass':
-            setattr(self, sLyrName, FeedForward.PassThrough(sName=sName, fDt=self.__fDt, **kwargs))
-            print('PassThrough layer "{}" has been added to network.'.format(sName))
-        
-        elif sKind in ['Reservoir', 'reservoir', 'Recurrent', 'recurrent', 'res', 'rec']:
-            setattr(self, sLyrName, Recurrent.RecLayer(sName=sName, fDt=self.__fDt, **kwargs))
-            print('Recurrent layer "{}" has been added to network.'.format(sName))
-
+            print('Layer {} will be replaced'.format(sLyrName))
         else:
-            raise NetworkError('Layer type not understood')
-        
+            print('Adding layer {} to netowrk'.format(sLyrName))
+        setattr(self, sLyrName, lyr)
         # Update inventory of layers
-        self.setLayers.add(getattr(self, sLyrName))
+        self.setLayers.add(lyr)
 
+        #Connect in- and outputs
+        for lyrIn in tplIn:
+            self.connect(lyrIn, lyr)
+        for lyrOut in tplOut:
+            self.connect(lyr, lyrOut)
+        
     def remove_layer(self, lyrDel):
         # Remove connections from lyrDel to others
         for lyr in self.setLayers:
@@ -115,7 +59,7 @@ class Network():
         del lyrDel
         self.lEvolOrder = self.evolution_order()
 
-    def connect(self, source, target):
+    def connect(self, lyrSource, lyrTarget):
         try:
             target.setIn.add(source)
         except AttributeError:
@@ -185,3 +129,83 @@ class Network():
 
 class NetworkError(Exception):
     pass
+
+
+"""Older stuff that might be useful again
+        # Default parameters for layers
+        dParamsIn = {'nSize' : 1,
+                     'sKind' : 'pass'}
+        dParamsOut = {'nSize' : 1,
+                      'sKind' : 'ffrate'}
+        dParamsRes = {'nSize' : 256,
+                      # 'nReservoirs' : 1,
+                      # 'sResConn' : 'serial',
+                      'sKind' : 'reservoir'}
+        dParamsIn.update(kwInput)
+        dParamsRes.update(kwReservoir)
+        dParamsOut.update(kwOutput)
+        try:
+            dParamsRes['vTau_n'] = kwReservoir['vTau_n']  
+        except KeyError:
+            dParamsRes['vTau_n'] = np.random.rand(dParamsRes['nSize'])
+
+
+        Constructing reservoirs in series or parallel
+        # self.nReservoirs = dParamsRes.pop('nReservoirs')
+        # sResConn = dParamsRes.pop('sResConn')
+        # # Broadcast single paramters to arrays that apply to all reservoirs
+        # #   Time constants
+        # if (len(np.atleast_2d(dParamsRes['vTau_n'])) == 1
+        #       and len(dParamsRes['vTau_n']) != self.nReservoirs):
+        #     dParamsRes['vTau_n'] = np.repeat(np.atleast_2d(dParamsRes['vTau_n']),
+        #                                      self.nReservoirs, axis=0)
+        # #  All other parameters
+        # for sParam in dParamsRes.keys():
+        #     if sParam != 'vTau_n':
+        #         try:
+        #             dParamsRes[sParam] = np.full(self.nReservoirs, dParamsRes[sParam])
+        #         except ValueError:
+        #             raise ValueError('The number of arguments for {} '.format(sParam) 
+        #                              + '({0}) does not match nReservoirs ({1})'.format(
+        #                             len(dParamsRes), self.nReservoirs))
+        # for i in range(self.nReservoirs):
+        #     setattr(self, 'lyrRes{}'.format(i), 
+        #             Recurrent.RecLayer(**{s : v[i] for s, v in dParamsRes.items()}))
+    
+        # - Generate connections: Each layer except input has a set with
+        # references to its input layer
+
+        # if sResConn == 'serial':
+        #     self.lyrRes0.setIn = {self.lyrIn}
+        #     for i in range(1, self.nReservoirs):
+        #         getattr(self, 'lyrRes{}'.format(i)).setIn = {getattr(self, 'lyrRes{}'.format(i-1))}
+        #     self.lyrOut.setIn = {getattr(self, 'lyrRes{}'.format(self.nReservoirs-1))}
+        # elif sResConn == 'parallel':
+        #     for i in Range(self.nReservoirs):
+        #         getattr(self, 'lyrRes{}'.format(i)).setIn = {self.lyrIn}
+        #     self.lyrOut.setIn = {getattr(self, 'lyrRes{}'.format(i))
+        #                        for i in range(self.nReservoirs)}
+        # else:
+        #     raise NetworkError('Connection type "{}" not understood'.format(sResConn))
+
+        ""Add feedforward or recurrent layer to network and maintain 
+        set containing all layers.""
+        sLyrName = 'lyr'+sName
+        if hasattr(self, sLyrName):
+            raise NameError('There already exists a layer with this name')
+
+        if sKind == 'ffrate':
+            setattr(self, sLyrName, FeedForward.FFRate(sName=sName, fDt=self.__fDt, **kwargs))
+            print('Rate-based feedforward layer "{}" has been added to network.'.format(sName))
+        
+        elif sKind == 'pass':
+            setattr(self, sLyrName, FeedForward.PassThrough(sName=sName, fDt=self.__fDt, **kwargs))
+            print('PassThrough layer "{}" has been added to network.'.format(sName))
+        
+        elif sKind in ['Reservoir', 'reservoir', 'Recurrent', 'recurrent', 'res', 'rec']:
+            setattr(self, sLyrName, Recurrent.RecLayer(sName=sName, fDt=self.__fDt, **kwargs))
+            print('Recurrent layer "{}" has been added to network.'.format(sName))
+
+        else:
+            raise NetworkError('Layer type not understood')
+"""
