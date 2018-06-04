e2e182c83d2f837f2bc76c44bb0bb9a35d731ff6
Felix Bauer <felix.bauer@ai-ctx.com>
2018-06-04 17:22:42 +0200

    Batch training for FFRateEuler. Not tested yet.

--- NetworksPython/layers/feedforward.py
+++ NetworksPython/layers/feedforward.py
@@ -226,6 +226,109 @@ class FFRateEuler(Layer):
 
         return ts.TimeSeries(vtTime, mSamplesAct)
 
+    def train_rr(self,
+                 tsTarget: TimeSeries,
+                 tsInput: TimeSeries = None,
+                 fRegularize=0,
+                 bFirst = True,
+                 bFinal = False):
+        """
+        train_rr - Train self with ridge regression over one of possibly
+                   many batches. Use Kahan summation to reduce rounding
+                   errors when adding data to existing matrices.
+        :param tsTarget:    TimeSeries - target for current batch
+        :param tsInput:     TimeSeries - input to self for current batch
+        :fRegularize:       float - regularization for ridge regression
+        :bFirst:            bool - True if current batch is the first in training
+        :bFinal:            bool - True if current batch is the last in training
+        """
+
+        # - Discrete time steps for evaluating input and target time series
+        vtTimeBase = self._gen_time_trace(tsTarget.tStart, tsTarget.tDuration)
+
+        if not bFinal:
+            # - Discard last sample to avoid counting time points twice
+            vtTimeBase = vtTimeBase[:-1]
+        
+        # - Prepare target data, check dimensions
+        mfTarget = tsTarget(vtTimeTrace)
+        assert mfTarget.shape[-1] == self.nSize, 
+            ('Target dimensions ({}) does not match layer size ({})'.format(
+                mfTarget.shape[-1], self.nSize))
+        
+        # - Prepare input data
+        if tsInput is not None:
+            # Warn if intput time range does not cover whole target time range
+            if not tsInput.contains(vtTimeBase) or tsInput.bPeriodic:
+                print('WARNING: tsInput (t = {} to {}) does not cover '.format(
+                      tsInput.tStart, tsInput.tStop)
+                      +'full time range of tsTarget (t = {} to {})'.format(
+                      tsTarget.tStart, tsTarget.tStop)
+                      +'Assuming input to be 0 outside of defined range.')
+
+            # - Sample input trace and check for correct dimensions
+            mfInput = self._check_input_dims(tsInput(vtTimeBase))
+            # - Treat "NaN" as zero inputs
+            mfInput[np.where(np.isnan(mfInput))] = 0
+
+        else:
+            # - Assume zero input
+            mfInput = np.zeros((np.size(vtTimeBase), self.nDimIn))
+            print('No tsInput defined, assuming input to be 0.')
+            
+        # - For first batch, initialize summands
+        if bFirst:
+            # Matrices to be updated for each batch
+            self.mfYXT = np.zeros((nDimOut, nNetSize))
+            self.mfXXT = np.zeros((nNetSize, nNetSize))
+            # Corresponding Kahan compensations 
+            self.mfKahanCompYXT = np.zeros_like(mfYXT)
+            self.mfKahanCompXXT = np.zeros_like(mfXXT)
+
+        # - Actual computations
+        self._computation_training(mfTarget, mfInput, bFinal)
+
+    @njit
+    def _computation_training(self,
+                              mfTarget: np.ndarray,
+                              mfInput: np.ndarray,
+                              bFinal: bool):
+        """
+        _computation_training - Perform matrix updates for training and
+                                in final batch also update weights
+        :param mfTarget:    2D-Array - training target     
+        :param mfInput:     2D-Array - training input to layer
+        :param bFinal:      bool - True for final batch
+        """
+
+        # - New data to be added, including compensation from last batch
+        mfUpdYXT = mfTarget.T@mfInput - mfKahanCompYXT
+        mfUpdXXT = mfInput.T@mfInput - mfKahanCompXXT
+
+        if not bFinal:
+            # - Update matrices with new data
+            mfNewYXT = self.mfYXT + mfUpdYXT
+            mfNewXXT = self.mfXXT + mfUpdXXT
+            # - Calculate rounding error for compensation in next batch
+            self.mfKahanCompYXT = (mfNewYXT-self.mfYXT) - mfUpdYXT
+            self.mfKahanCompXXT = (mfNewXXT-self.mfXXT) - mfUpdXXT
+            # - Store updated matrices
+            self.mfYXT = mfNewYXT
+            self.mfXXT = mfNewXXT
+
+        else:
+            # - In final step do not calculate rounding error but update matrices directly
+            self.mfYXT += mfUpdYXT
+            self.mfXXT += mfUpdXXT 
+
+            # - Weight update by ridge regression
+            self.mfW = np.linalg.solve(self.mfXXT+fRegularize*np.eye(nNetSize),
+                                       self.mfYXT.T).T
+
+            # - Remove dat stored during this trainig
+            self.mfYXT = self.mfXXT = self.mfKahanCompYXT = self.mfKahanCompXXT = None              
+
+
     @njit
     def potential(self, vInput: np.ndarray) -> np.ndarray:
         return (self._vfAlpha * noisy(vInput*self.vfGain + self.vfBias, self.fNoiseStd)
