c16e6db9f41eed74b6e2958bf6b84ad89e0ebed6
Dylan Richard Muir <dylan.richard.muir@gmail.com>
2018-06-15 17:20:09 +0200

    Implemented Spiking streaming layer and Network that can handle all-spiking layers

--- NetworksPython/test_streaming.py
+++ NetworksPython/test_streaming.py
@@ -11,8 +11,51 @@ class TestLayer(Layer):
     def evolve(self):
         pass
 
-    def stream(self, tDuration, tDt):
-        print("Layer: I'm preparing")
+    def stream(self, tDuration, tDt, bVerbose = False):
+        # - Initialise simulation, determine how many tDt to evolve fot
+        if bVerbose: print("Layer: I'm preparing")
+        vtTimeTrace = np.arange(0, tDuration, tDt)
+        nNumSteps = np.size(vtTimeTrace)
+        mfNoise = np.random.rand(nNumSteps) * self.fNoiseStd
+
+        nStep = 0
+        vfInput = None
+        self.state = 0
+        if bVerbose: print("Layer: Prepared")
+
+        # - Loop over tDt steps
+        for nStep in range(nNumSteps):
+            # - Convert from current state to output
+            if bVerbose: print('Layer: Yielding state: ', self.state)
+            if bVerbose: print('Layer: step', nStep)
+            if bVerbose: print('Layer: Waiting for input...')
+
+            # - Yield current output, receive input for next time step
+            tInput = yield (self._t, self.state)
+
+            if bVerbose: print('Layer: Input was: ', tInput)
+
+            # - Process input, if supplied
+            if tInput is not None:
+                self.state += tInput[1]
+
+            # - Incorporate noise trace
+            self.state += mfNoise[nStep]
+
+            # - Increment time
+            self._t += tDt
+
+        # - Retrun final state
+        return (self._t, self.state)
+
+
+class TestSpikeLayer(Layer):
+    def evolve(self):
+        pass
+
+    def stream(self, tDuration, tDt, bVerbose = False):
+        # - Initialise simulation, determine how many tDt to evolve for
+        if bVerbose: print("Layer: I'm preparing")
         vtTimeTrace = np.arange(0, tDuration, tDt)
         nNumSteps = np.size(vtTimeTrace)
         mfNoise = np.random.rand(nNumSteps)
@@ -20,22 +63,50 @@ class TestLayer(Layer):
         nStep = 0
         vfInput = None
         self.state = 0
-        print("Layer: Prepared")
+        if bVerbose: print("Layer: Prepared")
+
         for nStep in range(nNumSteps):
-            print('Layer: Yielding state: ', self.state)
-            print('Layer: step', nStep)
-            print('Layer: Waiting for input...')
-            tInput = yield (self._t, self.state)
+            # - Convert from current state to output
+            nNumSpikes = np.random.randint(0, 2)
+            if nNumSpikes > 0:
+                vnChannels = np.random.randint(0, 10, nNumSpikes)
+                vtSpikeTimes = np.repeat(self._t, nNumSpikes)
+                vfSamples = None
+            else:
+                vnChannels = None
+                vtSpikeTimes = None
+                vfSamples = None
 
-            print('Layer: Input was: ', tInput)
+            if bVerbose: print('Layer: Yielding {} spikes: '.format(nNumSpikes))
+            if bVerbose: print('Layer: step', nStep)
 
+            if bVerbose: print('Layer: Waiting for input spikes...')
+
+            # - Yield output for current time step, receive input for next time step
+            tInput = yield (vtSpikeTimes, vnChannels, vfSamples)
+
+            if bVerbose: print('Layer: Input was: ', tInput)
+
+            # - Process input, if supplied
             if tInput is not None:
-                self.state += tInput[1]
+                self.state += np.size(tInput[0])
 
+            # - Incorporate noise trace
             self.state += mfNoise[nStep]
+
+            # - Increment time
             self._t += tDt
 
-        return (self._t, self.state)
+        # - Return final state
+        return (self._t, self.state, None)
+
+    @property
+    def cInput(self):
+        return TSEvent
+
+    @property
+    def cOutput(self):
+        return TSEvent
 
 
 class TestNetwork(Network):
@@ -114,6 +185,88 @@ class TestNetwork(Network):
                 for nLayer in range(nNumLayers)
                 }
 
+
+class TestSpikeNetwork(Network):
+    def stream(self, tsExtInput, tDuration, bVerbose = False):
+        # - Find the largest common tDt
+        self.ltDts = [lyr.tDt for lyr in self.setLayers]
+        self.tCommonDt = max(self.ltDts)
+        if bVerbose: print('Net: Common time base: ', self.tCommonDt)
+
+        # - Prepare time base
+        vtTimeBase = np.arange(0, tDuration + self.tCommonDt, self.tCommonDt) + self._t
+        vtTimeBase = vtTimeBase[vtTimeBase <= self._t + tDuration]
+        tDuration = vtTimeBase[-1] - vtTimeBase[0]
+        nNumSteps = np.size(vtTimeBase)
+
+        # - Prepare all layers
+        self.lStreamers = [lyr.stream(tDuration, self.tCommonDt) for lyr in self.lEvolOrder]
+        nNumLayers = np.size(self.lEvolOrder)
+
+        # - Prepare external input
+        if tsExtInput is not None:
+            lInputSpikes = [tsExtInput.find((t, t + self.tCommonDt))
+                            for t in vtTimeBase]
+        else:
+            lInputSpikes = [None] * nNumSteps
+
+        # - Get initial state of all layers
+        if bVerbose: print('Net: getting initial state')
+
+        vtLastState = [(0, 0, 0)] + [lyr.send(None) for lyr in self.lStreamers]
+
+        # - Get initial state from layers
+        ldLayerOutputs = [{'vtTime':     [vtLastState[nLayer + 1][0]],
+                           'vnChannels': [vtLastState[nLayer + 1][1]],
+                           'mfSamples':  [vtLastState[nLayer + 1][2]]}
+                          for nLayer in range(nNumLayers)]
+
+        if bVerbose: print('Net: got initial state')
+
+        # - Streaming loop
+        vtState = vtLastState.copy()
+        nOutputPointer = 0
+        for nStep in range(nNumSteps - 1):
+            if bVerbose: print('Net: Start of step', nStep)
+
+            # - Set up external input
+            vtLastState[0] = (lInputSpikes[nStep])
+
+            for nLayerInd in range(nNumLayers):
+                try:
+                    vtState[nLayerInd + 1] = self.lStreamers[nLayerInd].send(vtLastState[nLayerInd])
+                except StopIteration as e:
+                    vtState[nLayerInd + 1] = e.args[0]
+
+            # - Save layer outputs
+            for nLayer in range(nNumLayers):
+                ldLayerOutputs[nLayer]['vtTime'].append(vtState[nLayer + 1][0])
+                ldLayerOutputs[nLayer]['vnChannels'].append(vtState[nLayer + 1][1])
+                ldLayerOutputs[nLayer]['mfSamples'].append(vtState[nLayer + 1][2])
+
+            # - Save last state to use as input for next step
+            vtLastState = vtState.copy()
+
+        # - Build return dictionary
+        dReturn = dict()
+        for nLayer in range(nNumLayers):
+            # - Concatenate time series
+            vtTimeTrace = np.array(ldLayerOutputs[nLayer]['vtTime'], 'float')
+            vnChannels = np.array(ldLayerOutputs[nLayer]['vnChannels'], 'float')
+            mfSamples = np.array(ldLayerOutputs[nLayer]['mfSamples'], 'float')
+
+            # - Filter out nans in time trace
+            vbUseSamples = np.invert(np.isnan(vtTimeTrace))
+
+            if bVerbose: print(vtTimeTrace[vbUseSamples])
+
+            # - Build dictionary
+            dReturn[self.lEvolOrder[nLayer].strName] = TSEvent(vtTimeTrace[vbUseSamples],
+                                                               vnChannels[vbUseSamples],
+                                                               mfSamples[vbUseSamples]
+                                                               )
+
+        return dReturn
 
 if __name__ == '__main__':
     lyr1 = TestLayer(0)
