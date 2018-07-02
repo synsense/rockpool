1d8f8cca2f1aa815d87adb6482d90f012e29d3a6
Felix Bauer <felix.bauer@ai-ctx.com>
2018-07-02 18:27:38 +0200

    improved recurrent weights in simulations

--- NetworksPython/ecg_anomaly_detection_spiking.py
+++ NetworksPython/ecg_anomaly_detection_spiking.py
@@ -1,6 +1,8 @@
 import numpy as np
-from brian2 import second  ##
+from brian2 import second, amp  ##
 from matplotlib import pyplot as plt
+import seaborn as sb
+
 plt.ion()
 
 # - Local imports
@@ -21,24 +23,37 @@ from layers.recurrent.weights import IAFSparseNet  ##
 tDt = 0.005  # Length of time step in seconds
 fHeartRate = 1  # Heart rate in rhythms per second
 
-nTrialsTr = 1500  # Number ECG rhythms for training
-nTrialsTe = 500  # Number ECG rhythms for testing
+nTrialsTr = 1000  # Number ECG rhythms for Training
+nTrialsVa = 500  # Number ECG rhythms for validation
+nRepsVa = 4  # Number repetitions of validation runs
+nTrialsTe = 1000  # Number ECG rhythms for testing
 
 nDimIn = 1  # Input dimensions
 nDimOut = 1  # Output dimensions
 
 nResSize = 512  # Reservoir size  ##
+fConnectivity = 0.4  # Percentage of non-zero recurrent weights  ##
+fMeanScale = -9e-6  # Scale mean of recurrent weights  ##
+fStdScale = 11e-6  # Scale standard dev. of recurrent weights  ##
+
 tTauN = 35 * tDt  # Reservoir neuron time constant  ##
-tTauS = 350 * tDt # Reservoir synapse time constant  ##
+tTauS = 350 * tDt  # Reservoir synapse time constant  ##
 tTauO = 35 * tDt  # Readout time constant  ##
 
-tDurBatch  = 500 # Training batch duration
+fBiasMin = 0 * amp
+fBiasMax = 0.02 * amp
+vfBias = (fBiasMax - fBiasMin) * np.random.rand(nResSize) + fBiasMin
+# vfBias = 0.0105*amp
+
+tDurBatch = 500  # Training batch duration
 fRegularize = 0.001  # Regularization parameter for training with ridge regression
 
 # Parameters concerning reservoir weights
 kwResWeights = {
     "nResSize": nResSize,
-    "fDensity" : 0.4,
+    "fDensity": fConnectivity,
+    "fMean" : fMeanScale / fConnectivity / nResSize,
+    "fStd" : fStdScale / np.sqrt(fConnectivity),
     # "fConnectivity": 0.4,  # Connectivity
     # "bPartitioned": False,  # Partition reservoir into excitatory/inhibitory
     # "fRatioExc": 0.5,  # Ratio of excitatory neurons
@@ -48,18 +63,18 @@ kwResWeights = {
 
 # Probabilities for anomalies in ECG rhythms
 pNormal = 0.8  # Probability of normal input rhythm
-pAnomal = (1 - pNormal) / 8  # Probability of abnormal input rhythm
+pAnomal = (1 - pNormal) / 6.  # Probability of abnormal input rhythm
 
 dProbs = {
-    "complete_normal": pNormal,     # Normal ECG
-    "complete_noP": pAnomal,      # Missing P-wave
-    "complete_Pinv": pAnomal,     # Inverted P-wave
-    "complete_noQRS": pAnomal,    # Missing QRS complex
-    "complete_Tinv": pAnomal,     # Inverted T-wave
-    "complete_STelev": pAnomal,   # Elevated ST-segment
-    "complete_STdepr": pAnomal,   # Depressed ST-segment
-    "complete_tach": pAnomal,     # Tachycardia
-    "complete_brad": pAnomal,     # Bradycardia
+    "complete_normal": pNormal,  # Normal ECG
+    "complete_noP": pAnomal,  # Missing P-wave
+    "complete_Pinv": pAnomal,  # Inverted P-wave
+    "complete_noQRS": pAnomal,  # Missing QRS complex
+    "complete_Tinv": pAnomal,  # Inverted T-wave
+    "complete_STelev": pAnomal,  # Elevated ST-segment
+    "complete_STdepr": pAnomal,  # Depressed ST-segment
+    # "complete_tach": pAnomal,     # Tachycardia
+    # "complete_brad": pAnomal,     # Bradycardia
 }
 
 # - Kwargs for signal_and_target function
@@ -69,7 +84,8 @@ kwSignal = {
     "fHeartRate": fHeartRate,
     "tDt": tDt,
     "bVerbose": True,
-    "nMinWidth": int(0.5*fHeartRate / tDt),
+    "nMinWidth": int(0.5 * fHeartRate / tDt),
+    "bDetailled": True,
 }
 
 
@@ -87,8 +103,8 @@ def cTrain(net: nw.Network, dtsSignal: dict, bFirst: bool, bFinal: bool):
     # - Input to flOut
     tsInput = dtsSignal[flOut.lyrIn.strName]
     # - Infer time range of current batch
-    tStart = dtsSignal['external'].tStart
-    tStop = dtsSignal['external'].tStop
+    tStart = dtsSignal["external"].tStart
+    tStop = dtsSignal["external"].tStop
     # - Sample target within time range of current batch
     tsTarget = tsTgtTr.resample_within(tStart, tStop)
     # - Train the layer
@@ -105,102 +121,97 @@ def ts_ecg_target(nRhythms: int, **kwargs) -> (ts.TimeSeries, ts.TimeSeries):
     """
 
     # - Input signal and target
-    vfInput, vfTarget = signal_and_target(nTrials=nRhythms, **kwargs)
-
+    vfInput, mfTarget = signal_and_target(nTrials=nRhythms, **kwargs)
+    vfTarget1D = mfTarget.any(axis=1)
     # - Time base
-    tDt = kwargs['tDt']
-    vtTime = np.arange(0, vfInput.size * tDt, tDt)[: vfInput.size]
+    tDt = kwargs["tDt"]
+    vtTime = np.arange(0, vfInput.size * tDt, tDt)[: len(vfInput)]
 
     # - Genrate time series
     tsInput = ts.TimeSeries(vtTime, vfInput)
-    tsTarget = ts.TimeSeries(vtTime, vfTarget)
+    tsTarget = ts.TimeSeries(vtTime, vfTarget1D)
 
-    return tsInput, tsTarget
+    return tsInput, tsTarget, mfTarget
 
 
 ### --- Network generation
 
 # - Generate weight matrices
 mfW_in = 2 * (np.random.rand(nDimIn, nResSize) - 0.5)
+# mfW_in = np.random.rand(nDimIn, nResSize)
 # mfW_res = RndmSparseEINet(**kwResWeights)
-mfW_res = IAFSparseNet(**kwResWeights)  ##
+mfW_res = IAFSparseNet(**kwResWeights) ##
 
 # - Generate layers
-flIn = PassThrough(mfW=mfW_in, tDt=tDt, tDelay=0, strName='input')   ##
-rlRes = RecIAFBrian(mfW=mfW_res, vtTauN=tTauN, vtTauSynR=tTauS, tDt=tDt * second, strName="reservoir")  ##
-flOut = FFExpSyn(mfW=np.zeros((nResSize, nDimOut)), tTauSyn=tTauO, tDt=tDt, strName="output")  ##
+flIn = PassThrough(mfW=mfW_in, tDt=tDt, tDelay=0, strName="input")  ##
+rlRes = RecIAFBrian(
+    mfW=mfW_res, vtTauN=tTauN, vfBias=vfBias, vtTauSynR=tTauS, tDt=tDt * second, strName="reservoir"
+)  ##
+flOut = FFExpSyn(
+    mfW=np.zeros((nResSize, nDimOut)), tTauSyn=tTauO, tDt=tDt, strName="output"
+)  ##
 
 # - Generate network
 net = nw.Network(flIn, rlRes, flOut)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     ### --- Training
 
     # - Training signal
-    # Generate training data and time trace
-    tsInTr, tsTgtTr = ts_ecg_target(nTrialsTr, **kwSignal)
-
-    # d = net.evolve(tsInTr)
-    # plt.scatter(d['reservoir'].vtTimeTrace, d['reservoir'].vnChannels)
-    # plt.plot(tsInTr.vtTimeTrace, tsInTr.mfSamples*30+50, color='y')
-
+    tsInTr, tsTgtTr, __ = ts_ecg_target(nTrialsTr, **kwSignal)
 
     # - Run training
-    net.train(cTrain, tsInTr, tDurBatch=tDurBatch, bHighVerbosity=True)
+    net.train(cTrain, tsInTr, tDurBatch=tDurBatch)
     net.reset_all()
 
-
-    # # - Sanity check with training signal
-    # dTr = net.evolve(tsInTr)
-    # net.reset_all()
-    # 
-    # # Output TimeSeries
-    # tsOutTr = dTr[flOut.strName]
-
-    # # Plot input, target and output
-    # plt.figure()
-    # plt.plot(tsOutTr.vtTimeTrace, tsOutTr.mfSamples)
-    # plt.plot(tsTgtTr.vtTimeTrace, tsTgtTr.mfSamples)
-    # plt.plot(tsInTr.vtTimeTrace, 0.2*tsInTr.mfSamples, color='k', alpha=0.3, zorder=-1)
-
-    # # Plot smoothed output
-    # nWindowSize = 200
-    # vWindow = np.ones(nWindowSize) / nWindowSize
-    # vfSmoothed = np.convolve(tsOutTr.mfSamples.flatten(), vWindow, 'full')
-    # plt.plot(tsOutTr,vtTimeTrace, vfSmoothed[-(tsOutTr.vtTimeTrace.size):])
+    # # - Plot weight distribution
+    # figWeights = plt.figure()
+    # plt.suptitle("Readout weights")
+    # sb.distplot(flOut.mfW)
 
 
     ### --- Validation run for threshold determination
 
-    # - Validation signal
-    # Generate test data and time trace
-    tsInVa, tsTgtVa = ts_ecg_target(nTrialsTe, **kwSignal)
+    print("Finding threshold")
 
-    # - Validation run
-    dVa = net.evolve(tsInVa)
-    net.reset_all()
+    # - Lists for storing thresholds
+    lfThr = []
+    
+    # - Validation runs
+    for i in range(nRepsVa):
 
-    vOutVa = dVa[flOut.strName].mfSamples
-    # - Determine threshold for analysis of test run
-    fThr = an.find_threshold(
-        vOutput=vOutVa,
-        vTarget=tsTgtVa.mfSamples.flatten(),
-        nWindow=int(fHeartRate / tDt),
-        nClose=int(fHeartRate / tDt),
-        nAttempts=16,
-        nRecursions=4,
-    )
+        print("\tRun {} of {}".format(i + 1, nRepsVa), end='\r')
+        # - Signal generation
+        tsInVa, tsTgtVa, __ = ts_ecg_target(nTrialsTe, **dict(kwSignal, bVerbose=False))
+
+        # - Validation run
+        dVa = net.evolve(tsInVa, bVerbose=False)
+        net.reset_all()
+
+        # - Determine threshold for analysis of test run
+        lfThr.append(
+            an.find_threshold(
+                vOutput=dVa[flOut.strName].mfSamples,
+                vTarget=tsTgtVa.mfSamples.flatten(),
+                nWindow=int(fHeartRate / tDt),
+                nClose=int(fHeartRate / tDt),
+                nAttempts=16,
+                nRecursions=4,
+            )
+        )
+
+        del dVa
+
+    fThr = np.mean(lfThr)
     print("Using threshold: {:.3f}".format(fThr))
 
 
     ### --- Testing
 
     # - Test signal
-    # Generate test data and time trace
-    tsInTe, tsTgtTe = ts_ecg_target(nTrialsTe, **kwSignal)
+    tsInTe, tsTgtTe, mfTgtTe = ts_ecg_target(nTrialsTe, **kwSignal)
 
     # - Run test
     dTe = net.evolve(tsInTe)
@@ -209,22 +220,71 @@ if __name__ == '__main__':
     # - Output TimeSeries
     tsOutTe = dTe[flOut.strName]
 
-    # Plot input, target and output
-    # plt.figure()
-    # plt.plot(tsOutTe.vtTimeTrace, tsOutTe.mfSamples)
-    # plt.plot(tsTgtTe.vtTimeTrace, tsTgtTe.mfSamples)
-    # plt.plot([tsResTe.vtTimeTrace[0], tsResTe.vtTimeTrace[1]], [fThr, fThr], 'k--', zorder=0, lw=2)
-    # plt.plot(tsInTe.vtTimeTrace, 0.2*tsInTe.mfSamples, color='k', alpha=0.3, zorder=-1)
-
     # - Analysis and plotting
     print("\nAnalysis of test run:")
     vOutTe = dTe[flOut.strName].mfSamples
     dAnalysis = an.analyze(
-                           vOutTe,
-                           tsTgtTe.mfSamples.flatten(),
-                           tsInTe.mfSamples,
-                           fThr,
-                           nWindow=int(fHeartRate / tDt),
-                           nClose=int(fHeartRate / tDt),
-                           bPlot=True,
-                          )
+        vOutTe,
+        tsTgtTe.mfSamples.flatten(),
+        tsInTe.mfSamples,
+        fThr,
+        nWindow=int(fHeartRate / tDt),
+        nClose=int(fHeartRate / tDt),
+        bPlot=True,
+        mfTarget=mfTgtTe,
+    )
+
+
+    # - Show statistics for individual symptom types
+
+    lstrSymptomFullNames = [
+        "Inverted P-wave",
+        "Missing P-wave",
+        "Missing QRS-complex",
+        "Inverted T-wave",
+        "Elevated ST-segment",
+        "Depressed ST-segment",
+    ]
+
+    for iSymptom in dAnalysis["dSymptoms"].keys():
+        if iSymptom < len(lstrSymptomFullNames):
+            print(
+                lstrSymptomFullNames[iSymptom]
+                + ":" 
+                + str(dAnalysis["dSymptoms"][iSymptom]["fSensitivity"])
+            )
+
+
+    """
+    ### --- Test anomaly-wise
+
+    # dicts with data and results
+    ddTSAnom = dict()
+    ddResultsAnom = dict()
+
+    for strAnomaly in (k for k in dProbs.keys() if k != "complete_normal"):
+        print("\nTesting " + strAnomaly)
+
+        # Probabilites
+        kwSignalAn = kwSignal.copy()
+        kwSignalAn["bVerbose"] = False
+        kwSignalAn["dProbs"] = {"complete_normal": pNormal, strAnomaly: 1. - pNormal}
+        # Signal generation
+        tsInAn, tsTgtAn, __ = ts_ecg_target(nTrialsTe, **kwSignalAn)
+
+        # Run test
+        ddTSAnom[strAnomaly] = net.evolve(tsInAn, bVerbose=False)
+        net.reset_all()
+
+        # Analysis
+        vOutAn = ddTSAnom[strAnomaly][flOut.strName].mfSamples
+        ddResultsAnom[strAnomaly] = an.analyze(
+            vOutAn,
+            tsTgtAn.mfSamples.flatten(),
+            tsInAn.mfSamples,
+            fThr,
+            nWindow=int(fHeartRate / tDt),
+            nClose=int(fHeartRate / tDt),
+            bPlot=False,
+        )
+        """
