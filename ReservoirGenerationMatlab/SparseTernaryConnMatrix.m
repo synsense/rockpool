6be15ac7ee3b871b03be392292c00d0c1385ba98
Dylan Richard Muir <dylan.richard.muir@gmail.com>
2018-07-05 10:59:33 +0200

    Moved reports and old projects

--- ReservoirGenerationMatlab/SparseTernaryConnMatrix.m
+++
@@ -1,9 +0,0 @@
-function mfWeights = SparseTernaryConnMatrix(nNumNeurons, nNumExcConn, nNumInhConn)
-
-mfWeights = zeros(nNumNeurons);
-
-for nSource = 1:nNumNeurons
-    vnDest = randsample(nNumNeurons, nNumExcConn + nNumInhConn);
-    mfWeights(vnDest(1:nNumExcConn), nSource) = 1;
-    mfWeights(vnDest((1:nNumInhConn) + nNumExcConn), nSource) = -1;
-end
