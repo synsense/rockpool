496ed2ba9e75ecfc1880d6641ffc659c104d5e89
Dylan Richard Muir <dylan.richard.muir@gmail.com>
2018-07-06 18:04:23 +0200

    * `Network.stream` now checks that all layers implement `stream` * Re-ported some bug fixes to `TSContinuous` * `TSContinuous` now implements `find` * Added comments to `Network.stream()`

--- NetworksPython/timeseries.py
+++ NetworksPython/timeseries.py
@@ -935,7 +935,10 @@ class TimeSeries:
 
 
 class TSContinuous(TimeSeries):
-    pass
+    def find(self, ttTimes):
+        vtTime = list(ttTimes)
+        mfSamples = self(vtTime)
+        return vtTime, mfSamples
 
 
 ### --- Event time series
@@ -966,11 +969,11 @@ class TSEvent(TimeSeries):
 
         # - Only 1D samples and channels are supported
         assert (np.ndim(vfSamples) <= 1) or (
-            np.sum(vfSamples.shape > 1) == 1
+            np.sum(np.array(vfSamples.shape) > 1) == 1
         ), "`vfSamples` must be 1-dimensional"
 
         assert (np.ndim(vnChannels) <= 1) or (
-            np.sum(vnChannels.shape > 1) == 1
+            np.sum(np.array(vnChannels.shape) > 1) == 1
         ), "`vnChannels` must be 1-dimensional"
 
         # - Provide default inputs
