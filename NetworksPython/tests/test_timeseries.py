e00700ff4e8e8b87942e1f4cbbe103de7d873e5a
Sadique Sheik <sadique.sheik@ai-ctx.com>
2018-07-10 13:42:04 +0200

    added another test for issue #33

--- NetworksPython/tests/test_timeseries.py
+++ NetworksPython/tests/test_timeseries.py
@@ -8,11 +8,28 @@ sys.path.insert(1, strNetworkPath)
 
 
 def test_TSEvent_raster():
+    '''
+    Test TSEvent raster function on merging other time series events
+    '''
     from NetworksPython import TSEvent
 
     testTSEvent = TSEvent([0, 30], 0)
     for i in range(1, 4):
         testTSEvent.merge(TSEvent(None, i))
 
-    raster = testTSEvent.raster(tDt=1)[1]
+    raster = testTSEvent.raster(tDt=1)[2]
+    assert raster.shape == (31, 5)
+
+
+def test_TSEvent_raster_explicit_nNumChannels():
+    '''
+    Test TSEvent raster method when the function is initialized with explicit number of Channels
+    '''
+    from NetworksPython import TSEvent
+
+    testTSEvent = TSEvent([0, 30], 0, nNumChannels=5)
+    for i in range(1, 4):
+        testTSEvent.merge(TSEvent(None, i))
+
+    raster = testTSEvent.raster(tDt=1)[2]
     assert raster.shape == (31, 5)
