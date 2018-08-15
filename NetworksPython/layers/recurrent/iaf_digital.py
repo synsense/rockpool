1385330ee6dc2c6a35527d7cdede6f2097a272d6
Felix Bauer <felix.bauer@ai-ctx.com>
2018-08-15 15:42:52 +0200

    Continued work on digital neuron

---
+++ NetworksPython/layers/recurrent/iaf_digital.py
@@ -0,0 +1,329 @@
+###
+# digital_brian.py - Class implementing a recurrent layer consisting of
+#                    digital neurons with constant leak in Brian
+###
+
+# - Imports
+import sys
+strNetworkPath = sys.path[0] + '../../..'
+sys.path.insert(1, strNetworkPath)
+
+from NetworksPython.timeseries import TSEvent
+
+from NetworksPython.layers.layer import Layer
+
+from NetworksPython.layers.recurrent.timedarray_shift import TimedArray as TAShift
+
+
+# - Configure exports
+__all__ = ['RecDIAF']
+
+
+# - Absolute tolerance, e.g. for comparing float values
+fTolAbs = 1e-9
+
+
+## - RecDIAFBrian - Class: define a spiking recurrent layer based on digital IAF neurons
+class RecDIAF(Layer):
+    """ RecDIAFBrian - Class: define a spiking recurrent layer based on digital IAF neurons
+    """
+
+    ## - Constructor
+    def __init__(self,
+
+                 mfWRec: np.ndarray = None,
+                 mfWIn: np.ndarray = None,
+
+                 tDt: float = 0.0001,
+
+                 tSpikeDelay = 1e-8,
+                 tTauLeak = 1e-3,
+
+                 vfVThresh: np.ndarray = 100,
+                 vfVReset: np.ndarray = 0,
+                 vfCleak: np.ndarray = 1,
+
+                 strDtypeState: str = "int8",
+
+                 strName: str = 'unnamed'
+                 ):
+        """
+        RecDIAFBrian - Construct a spiking recurrent layer with digital IAF neurons, with a Brian2 back-end
+
+        :param mfWRec:          np.array NxN weight matrix
+        :param mfWIn:           np.array nDimInxN input weight matrix.
+
+        :param tDt:             float Length of single time step
+
+        :param tSpikeDelay:     float Time after which a spike within the
+                                      layer arrives at the recurrent
+                                      synapses of the receiving neurons
+                                      within the network.
+        :param tTauLeak:        float Period for applying leak
+
+        :param vfVThresh:       np.array Nx1 vector of neuron thresholds.
+        :param vfVReset:        np.array Nx1 vector of neuron reset potentials.
+        :param vfCleak:         np.array Nx1 vector of leak values.
+
+        :param strDtypeState:   str data type for the membrane potential
+
+        :param strName:         str Name for the layer. Default: 'unnamed'
+        """
+
+        # - Call super constructor
+        super().__init__(mfW = mfWIn,
+                         tDt = np.asarray(tDt),
+                         strName = strName)
+
+        # - Input weights must be provided
+        assert vfWRec is not None, 'Recurrent weights mfWRec must be provided.'
+
+        # - Channel for leak
+        self._nLeakChannel = self.nDimIn + self.nSize
+        
+        # - One large weight matrix to process input and recurrent connections
+        #   as well as leak
+        self._mfWTotal = np.zeros((self._nLeakChannel + 1, self.nSize))
+        
+        # - Set neuron parameters
+        self.mfW = mfWRec
+        self.mfWIn = mfWIn
+        self.vfVThresh = vfVThresh
+        self.vfVReset = vfVReset
+        self.vfCleak = vfCleak
+        self.tSpikeDelay = tSpikeDelay
+        self.tTauLeak = tTauLeak
+        self.strDtypeState = strDtypeState
+
+        # - Initialize heap for events that are to be processed in future evolution
+        self._heapRemainingSpikes = []
+
+
+    def reset_state(self):
+        """ .reset_state() - Method: reset the internal state of the layer
+            Usage: .reset_state()
+        """
+        self.vfState = self.vfVReset
+        self._heapRemainingSpikes = []
+
+    def reset_time(self):
+        """
+        reset_time - Reset the internal clock of this layer
+        """
+
+        # - Reset heap for remaining spikes
+        self._heapRemainingSpikes = []
+        super().reset_time()
+
+
+    ### --- State evolution
+
+    def evolve(self,
+               tsInput: TSEvent = None,
+               tDuration: float = None,
+               bVerbose: bool = False,
+    ) -> TSEvent:
+        """
+        evolve - Evolve the state of this layer
+
+        :param tsInput:     TimeSeries TxM or Tx1 input to this layer
+        :param tDuration:   float Duration of evolution, in seconds
+        :param bVerbose:    bool Currently no effect, just for conformity
+
+        :return: TimeSeries Output of this layer during evolution period
+        """
+
+        # - Infer real duration of evolution
+        *__, tDuration = self._prepare_input(tsInput, tDuration)
+        tFinal = self.t + tDuration
+
+        # - Extract spike timings and channels
+        if tsInput is not None:
+            vtEventTimes, vnEventChannels, __ = tsInput.find([self.t, tFinal+self.tDt])
+            if np.size(vnEventChannels) > 0:
+                # - Make sure channels are within range
+                assert np.amax(vnEventChannels) < self.nDimIn, /
+                "Only channels between 0 and {} are allowed".format(self.nDimIn-1)
+        else:
+            vtEventTimes, vnEventChannels = [], []
+
+        ## -- Consider leak as periodic input spike with fixed weight
+        
+        # - Leak timings
+        # First leak is at multiple of self.tTauLeak
+        tFirstLeak = b2.ceil(self.t / self.tTauLeak) * self.tTauLeak
+        # Maximum possible number of leak steps in evolution period
+        nMaxNumLeaks = b2.ceil(tDuration / self.tTauLeak)
+        vtLeak = np.arange(nMaxNumLeaks) * self.tTauLeak + tFirstLeak
+        vtLeak = vtLeak[vtLeak <= tFinal + fTolAbs*second]
+
+        # - Include leaks in event trace, assign channel self.LeakChannel to leak
+        vnEventChannels = np.r_[vnEventChannels, np.ones_like(vtLeak) * self._nLeakChannel]
+        vtEventTimes = np.r_[vtEventTimes, vtLeak]
+
+        # - Push spike timings and IDs to a heap, ordered by spike time
+        # - Include spikes from previous evolution that might fall into this time interval
+        heapSpikes = self._heapRemainingSpikes + list(zip(vtEventTimes, vnEventChannels))
+        heapq.heapify(heapSpikes)
+
+        # - Store layer spike times and IDs in lists
+        ltSpikeTimes = []
+        liSpikeIDs = []
+        
+        tTime = self.t
+        # - Iterate over spike times. Stop when tFinal is exceeded.
+        while tTime <= tFinal:
+            try:
+                # - Iterate over spikes in temporal order
+                tTime, nChannel = heapq.heappop(heapSpikes)                       
+            except IndexError:
+                # - Stop if there are no spikes left
+                break
+            else:
+                # - State updates after incoming spike
+                self._vfState = np.clip(
+                    self._vfState + self._mfWTotal[nChannel],
+                    self._nStateMin,
+                    self._nStateMax
+                    ).astype(self.strDtypeState)
+
+                # - Neurons above threshold
+                vbAboveThresh = (self._vfState >= self._vfVThresh)
+                
+                # - Set states to reset potential
+                self._vfState[vbAboveThresh] = self._vfVReset[vbAboveThresh].astype(self._strDtypeState)
+
+                # # - Use this if fixed value should be subtracted from state instead of resetting:
+                # self._vfState[vbAboveThresh] = np.clip(
+                #     self._vfState[vbAboveThresh]-self.vfVReset[vbAboveThresh],
+                #     self._nStateMin,
+                #     None
+                # )
+                
+                # - IDs of spiking neurons
+                viSpikeIDs = np.where(vbAboveThresh)[0]
+                # - Append spike events to lists
+                ltSpikeTimes += [tTime] * np.sum(vbAboveThresh)
+                ltSpikeIDs += list(viSpikeIDs)
+
+                # - Append new spikes to heap
+                for nID in viSpikeIDs:
+                    # - Delay spikes by self.tSpikeDelay. Set IDs off by self.nDimIn in order
+                    #   to distinguish them from spikes coming from the input
+                    heapq.heappush(heapSpikes, (tTime + self._tSpikeDelay, nID + self._nDimIn))
+
+        # - Store remaining spikes (happening after tFinal) for next call of evolution
+        self._heapRemainingSpikes = heapSpikes
+
+        # - Update time
+        self.t += tDuration
+
+        # - Output time series
+        return TSEvent(ltSpikeTimes, liSpikeIDs)
+
+    ### --- Properties
+
+    @property
+    def cOutput(self):
+        return TSEvent
+
+    @property
+    def cInput(self):
+        return TSEvent
+
+    @property
+    def mfW(self):
+        return self.mfWRec
+
+    @mfW.setter
+    def mfW(self, mfNewW):
+        self.mfWRec = mfNewW
+
+    @property
+    def mfWRec(self):
+        return self._mfWTotal[self.nDimIn : self._nLeakChannel, : ]
+
+    @mfW.setter
+    def mfWRec(self, mfNewW):
+
+        self._mfWTotal[self.nDimIn : self._nLeakChannel, : ] = self._expand_to_weight_size(mfNewW, "mfWRec")
+
+    @property
+    def mfWIn(self):
+        return self._mfWTotal[: self.nDimIn, : ]
+
+    @mfWIn.setter
+    def mfWIn(self, mfNewW):
+        assert np.size(vfNewW) == self.nDimIn * self.nSize, \
+            '`mfNewW` must have [{}] elements.'.format(self.nDimIn * self.nSize)
+
+        self._mfWTotal[: self.nDimIn, : ] = np.array(mfWInNew)
+
+    @property
+    def vState(self):
+        return self._vState
+
+    @vState.setter
+    def vState(self, vNewState):
+        self._vState = self._expand_to_net_size(vNewState, "vState")
+
+    @property
+    def vfVThresh(self):
+        return self._vfVThresh
+
+    @vfVThresh.setter
+    def vfVThresh(self, vfNewThresh):
+        self._vfVThresh = self._expand_to_net_size(vfNewThresh, "vfVThresh")
+
+    @property
+    def vfVReset(self):
+        return self._vfVReset
+
+    @vfVReset.setter
+    def vfVReset(self, vfNewReset):
+        self._vfVReset = self._expand_to_net_size(vfNewReset, "vfVReset")
+
+    @property
+    def vfCleak(self):
+        return self._mfWTotal[-1, : ]
+
+    @vfCleak.setter
+    def vfCleak(self, vfNewLeak):
+
+        self._mfWTotal[-1, : ] = self._expand_to_net_size(vfNewLeak, 'vfCleak')
+
+    @Layer.tDt.setter
+    def tDt(self, _):
+        raise ValueError('The `tDt` property cannot be set for this layer')
+
+    @property
+    def tTauLeak(self):
+        return self._tTauLeak
+
+    @tTauLeak.setter
+    def tTauLeak(self, tNewTauLeak):
+        assert np.isscalar(tNewTauLeak) and tNewTauLeak > 0, \
+        "`tNewTauLeak` must be a scalar greater than 0."
+
+        self._tTauLeak = tNewTauLeak
+
+    @property
+    def tSpikeDelay(self):
+        return self._tSpikeDelay
+
+    @tSpikeDelay.setter
+    def tSpikeDelay(self, tNewSpikeDelay):
+        assert np.isscalar(tNewSpikeDelay) and tNewSpikeDelay > 0, \
+        "`tNewSpikeDelay` must be a scalar greater than 0."
+
+        self._tSpikeDelay = tNewSpikeDelay
+
+    @property
+    def strDtypeState(self):
+        return self._strDtypeState
+
+    @strDtypeState.setter
+    def strDtypeState(self, strNewDtype):
+        self._nStateMin = np.iinfo(strNewDtype).min
+        self._nStateMax = np.iinfo(strNewDtype).max
+        self._strDtypeState = strNewDtype
