fcc020ec5e1172529aa948ed52d9a3b3f9c3871b
Felix Bauer <felix.bauer@ai-ctx.com>
2018-08-20 16:34:59 +0200

    Compatibility adaptations to EventDrivenSpikingLayer

--- NetworksPython/layers/feedforward/evSpikeLayer.py
+++ NetworksPython/layers/feedforward/evSpikeLayer.py
@@ -9,19 +9,19 @@ class EventDrivenSpikingLayer(FFCLIAF):
     EventCNNLayer: Event driven 2D convolution layer
     '''
     def __init__(self, mfW: np.ndarray = None,
-                 fVth: float = 8,
+                 vfVThresh: float = 8,
                  tDt: float = 1,
                  strName: str = 'unnamed'):
         """
         EventCNLayer - Implements a 2D convolutional layer of spiking neurons
 
         :param nfW:        np.ndarray Weight matrix
-        :param fVth: float      Spiking threshold
+        :param vfVThresh: float      Spiking threshold
         :param tDt:  float  Time step
         :param strName:    str        Name of this layer.
         """
         # Call parent constructor
-        FFCLIAF.__init__(self, mfW, fVth=fVth, tDt=tDt, strName=strName)
+        FFCLIAF.__init__(self, mfW, vfVThresh=vfVThresh, tDt=tDt, strName=strName)
 
     def evolve(self,
                tsInput: TSEvent = None,
@@ -51,7 +51,7 @@ class EventDrivenSpikingLayer(FFCLIAF):
 
         # Local variables
         vState = self.vState
-        fVth = self.fVth
+        vfVThresh = self.vfVThresh
         mfW = self.mfW
 
         # Iterate over all input spikes
@@ -61,23 +61,23 @@ class EventDrivenSpikingLayer(FFCLIAF):
             nInputId = vIdInput[nSpikeIndx].astype(int)
 
             # Add input to neurons
-            vW = mfW[nInputId]
+            vfUpdate = mfW[nInputId]
 
-            # State update
-            vState[:] += vW  # Membrane state update
+            # State update (avoiding type cast errors)
+            vState = vState + vfUpdate
 
-            self.addToRecord(aStateTimeSeries, tCurrentTime, nIdOut=self.__nIdMonitor__)
+            self.addToRecord(aStateTimeSeries, tCurrentTime, vnIdOut=self.vnIdMonitor, vState=vState)
 
             # Check threshold and reset
-            mbSpike = vState >= fVth
-            if mbSpike.any():
-                vnSpike, = np.nonzero(mbSpike)
+            vbSpike = (vState >= vfVThresh)
+            if vbSpike.any():
+                vnSpike, = np.nonzero(vbSpike)
 
                 # Reset membrane state
-                vState[mbSpike] -= fVth
+                vState[vbSpike] -= vfVThresh[vbSpike]
 
                 # TODO: The above code has a bug
-                # If the threshold goes over 2*fVth this spike will not be detected till the next update.
+                # If the threshold goes over 2*vfVThresh this spike will not be detected till the next update.
 
                 # Record spikes
                 aSpk.append(
@@ -85,7 +85,7 @@ class EventDrivenSpikingLayer(FFCLIAF):
                                      vnSpike)))
 
                 # Record state after reset
-                self.addToRecord(aStateTimeSeries, tCurrentTime, nIdOut=self.__nIdMonitor__)
+                self.addToRecord(aStateTimeSeries, tCurrentTime, vnIdOut=self.vnIdMonitor, vState=vState)
 
         # Convert arrays to TimeSeries objects
         mfSpk = np.row_stack(aSpk)
