Fixed noise scaling. In FFExpSynBrian fixed reset methods

NetworksPython/layers/feedforward/exp_synapses_manual.py
@@ -133,8 +133,9 @@ class FFExpSyn(Layer):
         mWeightedSpikeTrains[0, :] += self.vState
 
         # - Add a noise trace
-        mfNoise = np.random.randn(*mWeightedSpikeTrains.shape) * self.fNoiseStd * np.sqrt(self.tDt) / self.tTauSyn
-        mfNoise[0,:] = 0 # Assure that noise trace starts with 0
+        # - Noise correction is slightly different than in other layers
+        mfNoise = np.random.randn(*mWeightedSpikeTrains.shape) * self.fNoiseStd * np.sqrt(2 * self.tDt / self.tTauSyn)
+        mfNoise[0,:] = 0 # Make sure that noise trace starts with 0
         #mfNoise = np.zeros_like(mWeightedSpikeTrains)
         #mfNoise[0,:] = self.fNoiseStd
 

NetworksPython/layers/feedforward/rate.py
@@ -180,7 +180,9 @@ class FFRateEuler(Layer):
                                         vfGain=self._vfGain,
                                         vfBias=self._vfBias,
                                         vfAlpha=self._vfAlpha,
-                                        fNoiseStd=self._fNoiseStd/np.sqrt(self._tDt))
+                                        # Without correction, standard deviation after some time will be
+                                        # self._fNoiseStd * sqrt(self._vfAlpha/2)
+                                        fNoiseStd=self._fNoiseStd * np.sqrt(2./self._vfAlpha))
 
         # - Increment internal time representation
         self._t += tTrueDuration
@@ -238,7 +240,9 @@ class FFRateEuler(Layer):
                                   vfGain = self._vfGain,
                                   vfBias = self._vfBias,
                                   vfAlpha = self._vfAlpha,
-                                  fNoiseStd = self._fNoiseStd / np.sqrt(self._tDt))
+                                  # Without correction, standard deviation after some time will be
+                                  # self._fNoiseStd * sqrt(self._vfAlpha/2)
+                                  fNoiseStd=self._fNoiseStd * np.sqrt(2./self._vfAlpha))
 
             # - Increment time
             self._t += tDt

NetworksPython/layers/recurrent/rate.py
@@ -151,7 +151,11 @@ class RecRateEuler(Layer):
         vtTimeBase, mfInputStep, tDuration = self._prepare_input(tsInput, tDuration)
 
         # - Generate a noise trace
-        mfNoiseStep = np.random.randn(np.size(vtTimeBase), self.nSize) * self.fNoiseStd * np.sqrt(self.tDt)
+        # Noise correction: Standard deviation after some time would be fNoiseStd * sqrt(0.5*tDt/vtTau)
+        mfNoiseStep = (
+            np.random.randn(np.size(vtTimeBase), self.nSize) 
+            * self.fNoiseStd * np.sqrt(2.*self._vtTau/self._tDt)
+        )
 
         # - Call Euler method integrator
         #   Note: Bypass setter method for .vState
@@ -187,7 +191,10 @@ class RecRateEuler(Layer):
         nEulerStepsPerDt = int(tDt / self._tDt)
 
         # - Generate a noise trace
-        mfNoiseStep = np.random.randn(nNumSteps, self.nSize) * self.fNoiseStd * np.sqrt(self.tDt)
+        mfNoiseStep = (
+            np.random.randn(np.size(vtTimeBase), self.nSize) 
+            * self.fNoiseStd * np.sqrt(2.*self._vtTau/self._tDt)
+        )
 
         if bVerbose: print("Layer: Prepared")
 