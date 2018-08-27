9229c453dbcb8a8aa8220385e776b53a379b8a63
Sadique Sheik <sadique.sheik@ai-ctx.com>
2018-08-27 18:16:41 +0200

    implemented strides #42

--- NetworksPython/tests/test_cnn_weightaccess.py
+++ NetworksPython/tests/test_cnn_weightaccess.py
@@ -1,6 +1,6 @@
-'''
+"""
 Test weigh access and indexing for CNNWeight class
-'''
+"""
 import sys
 import pytest
 import numpy as np
@@ -10,16 +10,16 @@ sys.path.insert(1, strNetworkPath)
 
 
 def test_import():
-    '''
+    """
     Test import of the class
-    '''
+    """
     from NetworksPython.layers.cnnweights import CNNWeight
 
 
 def test_raise_exception_on_incorrect_shape():
-    '''
+    """
     Test exception on size incompatibility
-    '''
+    """
     from NetworksPython.layers.cnnweights import CNNWeight
 
     W = CNNWeight(inShape=(200, 200))
@@ -33,9 +33,9 @@ def test_raise_exception_on_incorrect_shape():
 
 
 def test_raise_exception_on_undefined_shape():
-    '''
+    """
     Test exception on size incompatibility
-    '''
+    """
     from NetworksPython.layers.cnnweights import CNNWeight
 
     W = CNNWeight()
@@ -50,9 +50,9 @@ def test_raise_exception_on_undefined_shape():
 
 
 def test_convolution_full_image():
-    '''
+    """
     Test convolution of full image
-    '''
+    """
     from NetworksPython.layers.cnnweights import CNNWeight
 
     W = CNNWeight(inShape=(400, 400))
@@ -66,9 +66,9 @@ def test_convolution_full_image():
 
 
 def test_convolutionl_nonzero_index():
-    '''
+    """
     Test convolution when the indexing is done by non-zero pixels
-    '''
+    """
     from NetworksPython.layers.cnnweights import CNNWeight
 
     W = CNNWeight(inShape=(400, 400))
@@ -83,16 +83,18 @@ def test_convolutionl_nonzero_index():
 
 
 def test_data_format_channels_last():
-    '''
+    """
     Test indexing and output dimensions with channels last data format
-    '''
+    """
     from NetworksPython.layers.cnnweights import CNNWeight
 
-    W = CNNWeight(inShape=(400, 400, 1),
-                  nKernels=3,
-                  kernel_size=(1, 1),
-                  mode='same',
-                  img_data_format='channels_last')
+    W = CNNWeight(
+        inShape=(400, 400, 1),
+        nKernels=3,
+        kernel_size=(1, 1),
+        mode="same",
+        img_data_format="channels_last",
+    )
 
     # Create an image
     myImg = np.zeros((400, 400, 1))
@@ -102,7 +104,7 @@ def test_data_format_channels_last():
     # Test indexing with entire image
     outConv = W[myImgIndex]
     # Ensure size of output is as expected
-    assert myImg.size*3 == outConv.size
+    assert myImg.size * 3 == outConv.size
     # Ensure image dimensions are understood and maintained
     assert myImg.shape[:2] == W.outShape[:2]
     # Ensure convolution data is accurate
@@ -112,16 +114,18 @@ def test_data_format_channels_last():
 
 
 def test_data_format_channels_first():
-    '''
+    """
     Test indexing and output dimensions with channels last data format
-    '''
+    """
     from NetworksPython.layers.cnnweights import CNNWeight
 
-    W = CNNWeight(inShape=(1, 400, 400),
-                  nKernels=3,
-                  kernel_size=(1, 1),
-                  mode='same',
-                  img_data_format='channels_first')
+    W = CNNWeight(
+        inShape=(1, 400, 400),
+        nKernels=3,
+        kernel_size=(1, 1),
+        mode="same",
+        img_data_format="channels_first",
+    )
 
     # Create an image
     myImg = np.zeros((1, 400, 400))
@@ -133,8 +137,26 @@ def test_data_format_channels_first():
     # Ensure image dimensions are understood and maintained
     assert myImg.shape[-2:] == W.outShape[-2:]
     # Ensure size of output is as expected
-    assert myImg.size*3 == outConv.size
+    assert myImg.size * 3 == outConv.size
     # Ensure convolution data is accurate
     outConv = outConv.reshape((3, 400, 400))
     assert outConv[0, 5, 0] != 0
     assert outConv[0, 0, 5] == 0
+
+
+def test_strides_on_convolution():
+    """
+    Test the convolution upon a custom stride specified by user
+    """
+    from NetworksPython.layers.cnnweights import CNNWeight
+
+    W = CNNWeight(
+        inShape=(1, 10, 10),
+        nKernels=3,
+        kernel_size=(2, 2),
+        strides=(2, 5),
+        mode="valid",
+        img_data_format="channels_first",
+    )
+
+    assert W.outShape == (3, 5, 2)
