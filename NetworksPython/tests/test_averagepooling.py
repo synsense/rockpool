07445f95c8e69181c4033ca2286450c122c7d2bd
Sadique Sheik <sadique.sheik@ai-ctx.com>
2018-08-31 18:56:16 +0200

    all classes accessible at layer module

--- NetworksPython/tests/test_averagepooling.py
+++ NetworksPython/tests/test_averagepooling.py
@@ -12,14 +12,14 @@ def test_import():
     """
     Test import of the class
     """
-    from NetworksPython.layers.internal import AveragePooling2D
+    from NetworksPython.layers import AveragePooling2D
 
 
 def test_averagepooling():
     """
     Test import of the class
     """
-    from NetworksPython.layers.internal import AveragePooling2D
+    from NetworksPython.layers import AveragePooling2D
     from NetworksPython.timeseries import TSEvent
 
     lyrAverage = AveragePooling2D(
