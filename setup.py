7b28ad417d083212efaf400dc405e4113f63a3cb
Sadique Sheik <sadique.sheik@ai-ctx.com>
2018-09-03 16:31:41 +0200

    updated setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='NetworksPython',
-    version='0.1dev',
-    packages=['NetworksPython',],
-    license='All rights reserved aiCTX AG',
+    name="NetworksPython",
+    version="0.1dev",
+    packages=[
+        "NetworksPython",
+        "NetworksPython.layers",
+        "NetworksPython.layers.internal",
+        "NetworksPython.layers.gpl",
+    ],
+    license="All rights reserved aiCTX AG",
     # long_description=open('README.txt').read(),
 )
