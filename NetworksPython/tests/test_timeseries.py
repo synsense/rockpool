9fb8466fe3e7f39c7c78fd1f2755aeda4f98fd05
Dylan RIchard Muir <dylan.muir@ai-ctx.com>
2018-09-11 12:07:02 +0200

    * Fixed bug in `iaf_brian`, where unitless `tDt` would cause an error on instantiation * Added tests for `TimeSeries` classes * Added type hints for `TimeSeries`

--- NetworksPython/tests/test_timeseries.py
+++ NetworksPython/tests/test_timeseries.py
@@ -2,9 +2,86 @@
 Test TimeSeries methods
 '''
 import sys
+import pytest
 
 strNetworkPath = sys.path[0] + "/../.."
 sys.path.insert(1, strNetworkPath)
+
+def test_imports():
+    '''
+    Test importing TimeSeries classes
+    '''
+    from NetworksPython import TimeSeries
+    from NetworksPython import TSContinuous
+    from NetworksPython import TSEvent
+    from NetworksPython.timeseries import TimeSeries
+    from NetworksPython.timeseries import TSContinuous
+    from NetworksPython.timeseries import TSEvent
+    from NetworksPython.timeseries import GetPlottingBackend
+    from NetworksPython.timeseries import SetPlottingBackend
+    import NetworksPython.timeseries as ts
+
+
+def test_backends():
+    '''
+    Test using the plotting backend setting functions
+    '''
+    from NetworksPython.timeseries import GetPlottingBackend, SetPlottingBackend
+    bUseMPL, bUseHV = GetPlottingBackend()
+    SetPlottingBackend('matplotlib')
+    SetPlottingBackend('holoviews')
+
+
+def test_continuous_operators():
+    '''
+    Test creation and manipulation of a continuous time series
+    '''
+    from NetworksPython import TSContinuous
+
+    # - Creation
+    ts = TSContinuous([0], [0])
+    ts = TSContinuous([0, 1, 2, 3], [1, 2, 3, 4])
+    ts2 = TSContinuous([1, 2, 3, 4], [5, 6, 7, 8])
+
+    # - Samples don't match time
+    with pytest.raises(AssertionError):
+        TSContinuous([0, 1, 2], [0])
+
+    # - Addition
+    ts = ts + 1
+    ts += 5
+    ts = ts + ts2
+    ts += ts2
+
+    # - Subtraction
+    ts = ts - 3
+    ts -= 2
+    ts = ts - ts2
+    ts -= ts2
+
+    # - Multiplication
+    ts = ts * .9
+    ts *= .2
+    ts = ts * ts2
+    ts *= ts2
+
+    # - Division
+    ts = ts / 2.
+    ts /= 1.
+    ts = ts / ts2
+    ts /= ts2
+
+    # - Floor division
+    ts = ts // 1.
+    ts //= 1.
+    ts = ts // ts2
+    ts //= ts2
+
+
+def test_merge():
+    from NetworksPython import TSContinuous, TSEvent
+    ts1 = TSContinuous([0, 1, 2], [0, 1, 2])
+    ts1 = TSContinuous([0, 1, 2], [0, 1, 2])
 
 
 def test_TSEvent_raster():
@@ -43,5 +120,5 @@ def test_TSEvent_empty():
     testTSEvent = TSEvent([], [])
     assert testTSEvent.nNumChannels == 0
 
-    tsetTSEvent = TSEvent(None, None)
+    testTSEvent = TSEvent(None, None)
     assert testTSEvent.nNumChannels == 0
