226fe866b580bf90772be0acf97634b5a50da343
Sadique Sheik <sadique.sheik@ai-ctx.com>
2018-09-24 11:03:49 +0200

    moved torch implementations to torch submodule

--- NetworksPython/layers/internal/averagepooling.py
+++ NetworksPython/layers/internal/averagepooling.py
@@ -164,26 +164,3 @@ class AveragePooling2D(CLIAF):
     @mfW.setter
     def mfW(self, mfNewW):
         self.mfWIn = mfNewW
-
-
-class TorchSumPooling2dLayer(nn.Module):
-    """
-    Torch implementation of SumPooling2d for spiking neurons
-    """
-
-    def __init__(
-        self,
-        kernel_size: ArrayLike = (1, 1),
-        strides: Optional[ArrayLike] = None,
-        padding: ArrayLike = (0, 0, 0, 0),
-    ):
-        """
-        Torch implementation of SumPooling using the LPPool2d module
-        """
-        super(TorchSumPooling2dLayer, self).__init__()  # Init nn.Module
-        self.pad = nn.ZeroPad2d(padding)
-        self.pool = nn.LPPool2d(1, kernel_size=kernel_size, stride=strides)
-
-    def forward(self, tsrBinaryInput):
-        tsrPoolOut = self.pool(self.pad(tsrBinaryInput))
-        return tsrPoolOut
