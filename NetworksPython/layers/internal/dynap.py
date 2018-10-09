01be9cc682f894cbf8983df680eb490ed32b8418
Dylan Richard Muir <dylan.muir@ai-ctx.com>
2018-10-09 17:13:24 +0200

    * Implementing allocation of neurons * Refactoring to have super-class that can switches automatically between HW and Brian backends

--- NetworksPython/layers/internal/dynap.py
+++ NetworksPython/layers/internal/dynap.py
@@ -1,78 +1,14 @@
 ###
-# dynapse.py - Classes implementing layers via ctxctl for controlling the DynapSE
+# dynap.py - Classes implementing layers for simulating on the DynapSE
 ###
 
-from ..layer import Layer
-from ...timeseries import TSEvent
+from warnings import warn
 
-import numpy as np
+# -- Attempt to import required CtxCTL modules
+try:
+    from .dynap_hw import RecDynapSE
 
-import CtxDynapse
-import NeuronNeuronConnector
-from CtxDynapse import DynapseCamType as SynapseTypes
-
-class RecDynapSE(Layer):
-    """
-    RecDynapSE - Recurrent layer implemented on DynapSE
-    """
-    def __init__(self,
-                 mfW: np.ndarray,
-                 ):
-        # - Convert weight matrix to connectivity list (excitatory)
-        vnPreSynE, vnPostSynE = connectivity_matrix_to_prepost_lists(mfW > 0)
-
-        # - Convert weight matrix to connectivity list (inhibitory)
-        vnPreSynI, vnPostSynI = connectivity_matrix_to_prepost_lists(mfW < 0)
-
-        # - Get a list of HW neurons on the DynapSE
-        lHWNeurons = CtxDynapse.model.get_neurons()
-
-        # - Get a connector object
-        connector = NeuronNeuronConnector.DynapseConnector()
-
-        # - Map neuron indices to neurons
-        # lPreSynE = lHWNeurons[vnPreSynE]
-        # lPostSynE = lHWNeurons[vnPostSynE]
-        # lPreSynI = lHWNeurons[vnPreSynI]
-        # lPostSynI = lHWNeurons[vnPostSynI]
-
-        # - Connect the excitatory neurons
-        connector.add_connection_from_list(lHWNeurons[vnPreSynE],
-                                           lHWNeurons[vnPostSynE],
-                                           [SynapseTypes.SLOW_EXC]
-                                           )
-
-        # - Connect the inhibitory neurons
-        connector.add_connection_from_list(lHWNeurons[vnPreSynI],
-                                           lHWNeurons[vnPostSynI],
-                                           [SynapseTypes.SLOW_EXC]
-                                           )
-
-    def evolve(self,
-               tsInput: TSEvent = None,
-               tDuration: float = None,
-               ):
-        """
-        evolve - Evolve the layer by queueing spikes, stimulating and recording
-
-        :param tsInput:
-        :param tDuration:
-        :return:
-        """
-        # - Get input events from tsInput
-
-
-        pass
-
-
-def connectivity_matrix_to_prepost_lists(mfW: np.ndarray):
-    return np.nonzero(mfW)
-
-def TSEvent_to_spike_list(tsSeries: TSEvent):
-    """
-    TSEvent_to_spike_list - Convert a TSEvent object to a ctxctl spike list
-
-    :param tsSeries:
-    :return:
-    """
-    pass
+except ModuleNotFoundError:
+    # - Let's try to use a spike-based simulation instead
+    warn('A connection to the required DynapSE hardware was not found. Will try to use a spiking simulation instead.')
+    from .dynapse_brian import RecDynapseBrian as RecDynapSE
---
+++ NetworksPython/layers/internal/dynap_hw.py
@@ -0,0 +1,142 @@
+from ..layer import Layer
+from ...timeseries import TSEvent
+
+import numpy as np
+from warnings import warn
+
+
+import CtxDynapse
+import NeuronNeuronConnector
+from CtxDynapse import DynapseCamType as SynapseTypes
+from CtxDynapse import DynapseFpgaSpikeGen, DynapseNeuron
+
+def init_dynapse():
+    # - Initialise HW dictionary
+    dDynapse = {}
+
+    dDynapse['model'] = CtxDynapse.model()
+    lFPGAModules = dDynapse['model'].get_fpga_modules()
+
+    # - Find a spike generator module
+    vbIsSpikeGenModule = [isinstance(m, DynapseFpgaSpikeGen) for m in lFPGAModules]
+    if not np.any(vbIsSpikeGenModule):
+        # - There is no spike generator, so we can't use this Python layer on the HW
+        assert ModuleNotFoundError
+
+    else:
+        # - Get first spike generator module
+        dDynapse['fpgaSpikeGen'] = lFPGAModules[np.argwhere(vbIsSpikeGenModule)[0][0]]
+
+    # - Get all neurons
+    dDynapse['lAllNeurons'] = dDynapse['model'].get_neurons()
+
+    # - Initialise neuron allocation
+    dDynapse['vbAllocatedNeurons'] = np.array(False * len(dDynapse['lAllNeurons']))
+
+    # - Wipe configuration
+    warn('DynapSE configuration is not wiped -- IMPLEMENT ME --')
+
+    # - Return dictionary
+    return dDynapse
+
+# - Create global, if required
+global DHW_dDynapse
+if 'DHW_dDynapse' not in dir():
+    DHW_dDynapse = init_dynapse()
+
+
+def allocate_neurons(nNumNeurons: int) -> DynapseNeuron:
+    """
+    allocate_neurons - Return a list of neurons that may be used. These are guaranteed not to already be assigned.
+
+    :param nNumNeurons: int     The number of neurons requested
+    :return:            list    A list of neurons that may be used
+    """
+    # - Are there sufficient unallocated neurons?
+    if np.sum(DHW_dDynapse['vbAllocatedNeurons']) < nNumNeurons:
+        raise MemoryError('Insufficient unallocated neurons available. {}'.format(nNumNeurons) + ' requested.')
+
+    # - Pick the first available neurons
+    vnNeuronsToAllocate = np.nonzero(DHW_dDynapse['vbAllocatedNeurons'])[:nNumNeurons]
+
+    # - Mark these as allocated
+    DHW_dDynapse['vbAllocatedNeurons'][vnNeuronsToAllocate] = False
+
+    # - Return these neurons
+    return DHW_dDynapse['lAllNeurons'][vnNeuronsToAllocate]
+
+
+# -- Define the HW layer class for recurrent networks
+class RecDynapSE(Layer):
+    """
+    RecDynapSE - Recurrent layer implemented on DynapSE
+    """
+    def __init__(self,
+                 mfW: np.ndarray,
+                 strName: str = 'unnamed',
+                 tDt: float = 1e-6,
+                 ):
+        """
+        RecDynapSE - Recurrent layer implemented on DynapSE
+
+
+        :param mfW:     ndarray NxN matrix of recurrent weights
+        :param strName: str     Layer name
+        :param tDt:     float   Dummy time step. Not used in layer evolution
+        """
+
+        # - Initialise superclass
+        super().__init__(mfW, tDt, strName=strName)
+
+        # - Convert weight matrix to connectivity list (excitatory)
+        vnPreSynE, vnPostSynE = connectivity_matrix_to_prepost_lists(mfW > 0)
+
+        # - Convert weight matrix to connectivity list (inhibitory)
+        vnPreSynI, vnPostSynI = connectivity_matrix_to_prepost_lists(mfW < 0)
+
+        # - Get a connector object
+        connector = NeuronNeuronConnector.DynapseConnector()
+
+        # - Map neuron indices to neurons
+        lHWNeurons = allocate_neurons(self.nSize)
+
+        # - Connect the excitatory neurons
+        connector.add_connection_from_list(lHWNeurons[vnPreSynE],
+                                           lHWNeurons[vnPostSynE],
+                                           [SynapseTypes.SLOW_EXC]
+                                           )
+
+        # - Connect the inhibitory neurons
+        connector.add_connection_from_list(lHWNeurons[vnPreSynI],
+                                           lHWNeurons[vnPostSynI],
+                                           [SynapseTypes.SLOW_EXC]
+                                           )
+
+    def evolve(self,
+               tsInput: TSEvent = None,
+               tDuration: float = None,
+               ):
+        """
+        evolve - Evolve the layer by queueing spikes, stimulating and recording
+
+        :param tsInput:
+        :param tDuration:
+        :return:
+        """
+        # - Get input events from tsInput
+        pass
+
+
+def connectivity_matrix_to_prepost_lists(mfW: np.ndarray):
+    return np.nonzero(mfW)
+
+
+def TSEvent_to_spike_list(tsSeries: TSEvent):
+    """
+    TSEvent_to_spike_list - Convert a TSEvent object to a ctxctl spike list
+
+    :param tsSeries:
+    :return:
+    """
+    pass
+
