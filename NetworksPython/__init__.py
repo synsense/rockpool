d40eee6e0016fb91d3f8b2a65e89fd673737c939
Felix Bauer <felix.bauer@aictx.ai>
2019-01-18 19:07:50 +0100

    Include dynapse_contrl in init file

--- NetworksPython/__init__.py
+++ NetworksPython/__init__.py
@@ -1,4 +1,5 @@
 from .networks.network import Network
 from .timeseries import TimeSeries, TSContinuous, TSEvent
+from .dynapse_control import DynapseControl
 
-__all__ = ['Network', 'TimeSeries', 'TSContinuous', 'TSEvent']
+__all__ = ['Network', 'TimeSeries', 'TSContinuous', 'TSEvent', 'DynapseControl']
