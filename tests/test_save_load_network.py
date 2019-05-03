16865d1695586642ade123116b72a62a7ad56b66
christos.sourmpis <christos.sourmpis@ai-ctx.com>
2019-05-03 10:36:50 +0200

    test for save load of network

---
+++ tests/test_save_load_network.py
@@ -0,0 +1,27 @@
+"""
+Test the saving and loading of network model
+"""
+
+def test_load_save_net():
+    from NetworksPython.layers import FFIAFTorch, FFIAFNest, RecIAFSpkInTorch, RecIAFSpkInNest
+    from NetworksPython import networks as nw
+
+    lfftorch = FFIAFTorch([1], strName="torchff")
+    lrectorch = RecIAFSpkInTorch([1],[1], strName="torchrec")
+    lffnest = FFIAFNest([[1]], strName="nestff")
+    lrecnest = RecIAFSpkInNest([[1]],[[1]], strName="nestrec")
+
+    net = nw.Network(lffnest, lrecnest)
+
+    net.save("test_net_torch_config")
+
+    net1 = nw.Network.load("test_net_torch_config")
+
+    assert net.dLayers.keys() == net1.dLayers.keys()
+    net = nw.Network(lfftorch, lrectorch)
+
+    net.save("test_net_nest_config")
+
+    net1 = nw.Network.load("test_net_nest_config")
+
+    assert net.dLayers.keys() == net1.dLayers.keys()
