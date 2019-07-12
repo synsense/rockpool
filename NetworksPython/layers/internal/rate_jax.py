* Fixed a bug in `RecRateEulerJax`, where layer parameters were not forced to be `float`s * Fixed a bug in documentation notebook for gradient descent jax layer, where an `int` parameter would cause `grad` to fail

NetworksPython/layers/internal/rate_jax.py
@@ -380,7 +380,7 @@ class RecRateEulerJax(Layer):
         config["noise_std"] = self.noise_std.tolist()
         config["dt"] = self.dt
         config["name"] = self.name
-        config["rng_key"] = self.rng_key
+        config["rng_key"] = self._rng_key
         warn(
             f"RecRateEulerJax `{self.name}`: `activation_func` can not be stored with this "
             + "method. When creating a new instance from this dict, it will use the "
@@ -400,7 +400,7 @@ class RecRateEulerJax(Layer):
             self._size,
         ), "`win` must be [{:d}, {:d}]".format(self._num_inputs, self._size)
 
-        self._weights = np.array(value)
+        self._weights = np.array(value).astype("float")
 
     @property
     def w_recurrent(self) -> np.ndarray:
@@ -415,7 +415,7 @@ class RecRateEulerJax(Layer):
             self._size,
         ), "`w_recurrent` must be [{:d}, {:d}]".format(self._size, self._size)
 
-        self._w_recurrent = np.array(value)
+        self._w_recurrent = np.array(value).astype("float")
 
     @property
     def w_out(self) -> np.ndarray:
@@ -430,7 +430,7 @@ class RecRateEulerJax(Layer):
             self._num_outputs,
         ), "`w_out` must be [{:d}, {:d}]".format(self._size, self._num_outputs)
 
-        self._w_out = np.array(value)
+        self._w_out = np.array(value).astype("float")
 
     @property
     def tau(self) -> np.ndarray:
@@ -446,7 +446,7 @@ class RecRateEulerJax(Layer):
             np.size(value) == self._size
         ), "`tau` must have {:d} elements or be a scalar".format(self._size)
 
-        self._tau = np.reshape(value, self._size)
+        self._tau = np.reshape(value, self._size).astype("float")
 
     @property
     def bias(self) -> np.ndarray:
@@ -462,7 +462,7 @@ class RecRateEulerJax(Layer):
             np.size(value) == self._size
         ), "`bias` must have {:d} elements or be a scalar".format(self._size)
 
-        self._bias = np.reshape(value, self._size)
+        self._bias = np.reshape(value, self._size).astype("float")
 
     @property
     def dt(self) -> float:
@@ -477,7 +477,7 @@ class RecRateEulerJax(Layer):
 
         assert value >= tau_min, "`tau` must be at least {:.2e}".format(tau_min)
 
-        self._dt = np.array(value)
+        self._dt = np.array(value).astype("float")
 
 
 class ForceRateEulerJax(RecRateEulerJax):