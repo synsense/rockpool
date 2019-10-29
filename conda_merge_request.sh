6fd36af18cc70acb8c72e5c900a8e72e0771bdc2
Dacian Herbei <dacian.herbei@aictx.ai>
2019-10-29 13:55:06 +0100

    extract file name and sha256

--- conda_merge_request.sh
+++ conda_merge_request.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-ls -a ./dist/
+array=($(find ./dist/ -name "rockpool*" ))
+
+rezult_pip=($(/usr/bin/pip hash "${array[@]}"))
+
+file_name=${array[@]}
+file_name=${file_name#*dist/}
+
+str=${rezult_pip[@]}
+sha_with_tag=${str#*=}
+sha=${sha_with_tag#*:}
+
+echo ${file_name}
+echo ${sha}
