13cdcde69c30322c9dc55aaa2d2b7e34b4b84aa8
Dylan Richard Muir <dylan.muir@aictx.ai>
2019-11-05 13:57:11 +0100

    Fixes in response to conda-forge deployment review

--- conda_merge_request.sh
+++ conda_merge_request.sh
@@ -48,7 +48,6 @@ echo "  number: 0" >> meta.yaml
 echo "  script: \"{{ PYTHON }} -m pip install . -vv\"" >> meta.yaml
 echo "" >> meta.yaml
 echo "requirements:" >> meta.yaml
-echo "  build:" >> meta.yaml
 echo "  host:" >> meta.yaml
 echo "    - python >=3.6" >> meta.yaml
 echo "    - pip" >> meta.yaml
@@ -59,11 +58,6 @@ echo "    - numba" >> meta.yaml
 echo "    - python >=3.6" >> meta.yaml
 echo "" >> meta.yaml
 echo "test:" >> meta.yaml
-echo "  requires:" >> meta.yaml
-echo "    - numpy" >> meta.yaml
-echo "    - scipy" >> meta.yaml
-echo "    - numba" >> meta.yaml
-echo "    - pytest" >> meta.yaml
 echo "  imports:" >> meta.yaml
 echo "    - rockpool" >> meta.yaml
 echo "" >> meta.yaml
