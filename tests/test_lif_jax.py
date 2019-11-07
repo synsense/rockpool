2eabf7a9439b404cc0cc06c1e05690e356dd2be7
Dylan Richard Muir <dylan.muir@aictx.ai>
2019-11-07 16:18:01 +0100

    Initial commit of new LIF Jax model, plus test

---
+++ tests/test_lif_jax.py
@@ -0,0 +1,138 @@
+"""
+Test rate-based Euler models in iaf_jax.py
+"""
+
+import numpy as np
+import pytest
+
+
+def test_imports():
+    from rockpool.layers import RecLIFJax
+    from rockpool.layers import RecLIFCurrentInJax
+
+
+def test_RecLIFJax():
+    """ Test RecIAFExpJax """
+    from rockpool import TSContinuous, TSEvent
+    from rockpool.layers import RecLIFJax
+
+    # - Generic parameters
+    net_size = 2
+    dt = 10e-3
+
+    w_recurrent = 2 * np.random.rand(net_size, net_size) - 1
+    bias = 2 * np.random.rand(net_size) - 1
+    tau_m = 20e-3 * np.ones(net_size)
+    tau_s = 20e-3 * np.ones(net_size)
+
+    # - Layer generation
+    fl0 = RecLIFJax(
+        w_recurrent=w_recurrent,
+        bias=bias,
+        noise_std=0.1,
+        tau_mem=tau_m,
+        tau_syn=tau_s,
+        dt=dt,
+    )
+
+    # - Input signal
+    tsInSp = TSEvent(times=np.arange(15) * dt, channels=np.ones(15) * net_size)
+
+    # - Compare states and time before and after
+    vStateBefore = np.copy(fl0.state)
+    ts_output = fl0.evolve(tsInSp, duration=0.1)
+    assert fl0.t == 0.1
+    assert (vStateBefore != fl0.state).any()
+
+    fl0.reset_all()
+    assert fl0.t == 0
+    assert (vStateBefore == fl0.state).all()
+
+    # - Test that some errors are caught
+    with pytest.raises(AssertionError):
+        fl1 = RecLIFJax(
+            w_recurrent=np.zeros((3, 2)),
+            tau_mem=np.zeros(3),
+            tau_syn=np.zeros(3),
+            bias=np.zeros(3),
+        )
+
+    with pytest.raises(AssertionError):
+        fl1 = RecLIFJax(
+            w_recurrent=np.zeros((2, 2)),
+            tau_mem=np.zeros(3),
+            tau_syn=np.zeros(3),
+            bias=np.zeros(3),
+        )
+
+    with pytest.raises(AssertionError):
+        fl1 = RecLIFJax(
+            w_recurrent=np.zeros((2, 2)),
+            tau_mem=np.zeros(2),
+            tau_syn=np.zeros(3),
+            bias=np.zeros(3),
+        )
+
+
+def test_RecLIFCurrentInJax():
+    """ Test RecLIFCurrentInJax """
+    from rockpool import TSContinuous
+    from rockpool.layers import RecLIFCurrentInJax
+
+    # - Generic parameters
+    net_size = 2
+    dt = 10e-3
+
+    w_recurrent = 2 * np.random.rand(net_size, net_size) - 1
+    bias = 2 * np.random.rand(net_size) - 1
+    tau_m = 20e-3 * np.ones(net_size)
+    tau_s = 20e-3 * np.ones(net_size)
+
+    # - Layer generation
+    fl0 = RecLIFCurrentInJax(
+        w_recurrent=w_recurrent,
+        bias=bias,
+        noise_std=0.1,
+        tau_mem=tau_m,
+        tau_syn=tau_s,
+        dt=dt,
+    )
+
+    # - Input signal
+    tsInCont = TSContinuous(times=np.arange(15) * dt, samples=np.ones((15, net_size)))
+
+    # - Compare states and time before and after
+    vStateBefore = np.copy(fl0.state)
+    ts_output = fl0.evolve(tsInCont, duration=0.1)
+    assert fl0.t == 0.1
+    assert (vStateBefore != fl0.state).any()
+
+    fl0.reset_all()
+    assert fl0.t == 0
+    assert (vStateBefore == fl0.state).all()
+
+    # - Test that some errors are caught
+    with pytest.raises(AssertionError):
+        fl1 = RecLIFCurrentInJax(
+            w_recurrent=np.zeros((3, 2)),
+            tau_mem=np.zeros(3),
+            tau_syn=np.zeros(3),
+            bias=np.zeros(3),
+        )
+
+    with pytest.raises(AssertionError):
+        fl1 = RecLIFCurrentInJax(
+            w_recurrent=np.zeros((2, 2)),
+            tau_mem=np.zeros(3),
+            tau_syn=np.zeros(3),
+            bias=np.zeros(3),
+        )
+
+    with pytest.raises(AssertionError):
+        fl1 = RecLIFCurrentInJax(
+            w_recurrent=np.zeros((2, 2)),
+            tau_mem=np.zeros(2),
+            tau_syn=np.zeros(3),
+            bias=np.zeros(3),
+        )
+
