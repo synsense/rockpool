1e59def2d17fb68eaed6021386553741c386e059
Dylan Muir <dylan.muir@ai-ctx.com>
2020-03-03 10:37:53 +0100

    * rate_jax layers use a property for setting the activation function * Integrated testing of save / load in rate_jax layers * `FFRateEulerJax` now uses `w_in` instead of `weights` * Removed unneeded file

--- tests/test_rate_jax.py
+++ tests/test_rate_jax.py
@@ -45,6 +45,22 @@ def test_RecRateEulerJax():
     # - Test storage
     fl0.to_dict()
 
+    # - Test save and load
+    import tempfile
+    with tempfile.NamedTemporaryFile() as path_to_save:
+        # - Save the layer
+        fl0.save_layer(path_to_save.name)
+
+        # - Load the layer
+        lyr_loaded = RecRateEulerJax.load_from_file(path_to_save.name)
+
+        # - Compare properties
+        props_to_test = ['weights', 'tau', 'bias', 'dt', '_H', ]
+        assert all([
+            np.array([getattr(fl0, prop_name) == getattr(lyr_loaded, prop_name)]).all()
+            for prop_name in props_to_test
+        ])
+
     # - Test that some errors are caught
     with pytest.raises(AssertionError):
         fl1 = RecRateEulerJax(
@@ -66,6 +82,15 @@ def test_RecRateEulerJax():
             tau=np.zeros(2),
             bias=np.zeros(3),
         )
+
+    with pytest.raises(ValueError):
+        fl1 = RecRateEulerJax(
+                weights = np.zeros((2, 2)),
+                tau = np.zeros(2),
+                bias = np.zeros(2),
+                activation_func = "blah",
+                )
+
 
 def test_RecRateEulerJax_IO():
     """ Test RecRateEulerJax_IO """
@@ -106,6 +131,22 @@ def test_RecRateEulerJax_IO():
     # - Test storage
     fl0.to_dict()
 
+    # - Test save and load
+    import tempfile
+    with tempfile.NamedTemporaryFile() as path_to_save:
+        # - Save the layer
+        fl0.save_layer(path_to_save.name)
+
+        # - Load the layer
+        lyr_loaded = RecRateEulerJax_IO.load_from_file(path_to_save.name)
+
+        # - Compare properties
+        props_to_test = ['w_in', 'w_recurrent', 'w_out', 'tau', 'bias', 'dt', '_H', ]
+        assert all([
+            np.array([getattr(fl0, prop_name) == getattr(lyr_loaded, prop_name)]).all()
+            for prop_name in props_to_test
+        ])
+
     # - Test that some errors are caught
     with pytest.raises(AssertionError):
         fl1 = RecRateEulerJax_IO(
@@ -143,6 +184,17 @@ def test_RecRateEulerJax_IO():
             bias=np.zeros(3),
         )
 
+    with pytest.raises(ValueError):
+        fl1 = RecRateEulerJax_IO(
+                w_in = np.zeros((1, 2)),
+                w_recurrent = np.zeros((2, 2)),
+                w_out = np.zeros((2, 1)),
+                tau = np.zeros(2),
+                bias = np.zeros(2),
+                activation_func = "blah",
+                )
+
+
 def test_ForceRateEulerJax_IO():
     """ Test ForceRateEulerJax """
     from rockpool import TSContinuous
@@ -177,6 +229,22 @@ def test_ForceRateEulerJax_IO():
     # - Test storage
     fl0.to_dict()
 
+    # - Test save and load
+    import tempfile
+    with tempfile.NamedTemporaryFile() as path_to_save:
+        # - Save the layer
+        fl0.save_layer(path_to_save.name)
+
+        # - Load the layer
+        lyr_loaded = ForceRateEulerJax_IO.load_from_file(path_to_save.name)
+
+        # - Compare properties
+        props_to_test = ['w_in', 'w_out', 'tau', 'bias', 'dt', '_H', ]
+        assert all([
+            np.array([getattr(fl0, prop_name) == getattr(lyr_loaded, prop_name)]).all()
+            for prop_name in props_to_test
+        ])
+
     # - Test that some errors are caught
     with pytest.raises(AssertionError):
         fl1 = ForceRateEulerJax_IO(
@@ -201,3 +269,83 @@ def test_ForceRateEulerJax_IO():
             tau=np.zeros(2),
             bias=np.zeros(3),
         )
+
+    with pytest.raises(ValueError):
+        fl1 = ForceRateEulerJax_IO(
+                w_in = np.zeros((1, 2)),
+                w_out = np.zeros((2, 1)),
+                tau = np.zeros(2),
+                bias = np.zeros(2),
+                activation_func = "blah",
+                )
+
+
+def test_FFRateEulerJax():
+    """ Test FFRateEulerJax """
+    from rockpool import TSContinuous
+    from rockpool.layers import FFRateEulerJax
+
+    # - Generic parameters
+    weights = 2 * np.random.rand(1, 2) - 1
+    bias = 2 * np.random.rand(2) - 1
+    tau = 20e-3 * np.ones(2)
+
+    # - Layer generation
+    fl0 = FFRateEulerJax(
+        w_in=weights, bias=bias, noise_std=0.1, tau=tau, dt=0.01
+    )
+
+    # - Input signal
+    tsInCont = TSContinuous(times=np.arange(15) * 0.01, samples=np.ones((15, 1)))
+
+    # - Compare states and time before and after
+    vStateBefore = np.copy(fl0.state)
+    ts_output = fl0.evolve(tsInCont, duration=0.1)
+    assert fl0.t == 0.1
+    assert (vStateBefore != fl0.state).any()
+
+    fl0.reset_all()
+    assert fl0.t == 0
+    assert (vStateBefore == fl0.state).all()
+
+    # - Test storage
+    fl0.to_dict()
+
+    # - Test save and load
+    import tempfile
+    with tempfile.NamedTemporaryFile() as path_to_save:
+        # - Save the layer
+        fl0.save_layer(path_to_save.name)
+
+        # - Load the layer
+        lyr_loaded = FFRateEulerJax.load_from_file(path_to_save.name)
+
+        # - Compare properties
+        props_to_test = ['w_in', 'tau', 'bias', 'dt', '_H', ]
+        assert all([
+            np.array([getattr(fl0, prop_name) == getattr(lyr_loaded, prop_name)]).all()
+            for prop_name in props_to_test
+        ])
+
+    # - Test that some errors are caught
+    with pytest.raises(AssertionError):
+        fl1 = FFRateEulerJax(
+            w_in=np.zeros((1, 2)),
+            tau=np.zeros(3),
+            bias=np.zeros(3),
+        )
+
+    with pytest.raises(AssertionError):
+        fl1 = FFRateEulerJax(
+            w_in=np.zeros((1, 2)),
+            tau=np.zeros(2),
+            bias=np.zeros(3),
+        )
+
+    with pytest.raises(ValueError):
+        fl1 = FFRateEulerJax(
+                w_in = np.zeros((1, 2)),
+                tau = np.zeros(2),
+                bias = np.zeros(2),
+                activation_func = "blah",
+                )
