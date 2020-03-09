8bc292106f08edd38fc0b8040cc748308e9edcfb
Dylan Muir <dylan.muir@ai-ctx.com>
2020-03-09 11:43:51 +0100

    Added training function to Jax trained layer base class

--- rockpool/layers/training/gpl/jax_trained_layer.py
+++ rockpool/layers/training/gpl/jax_trained_layer.py
@@ -4,18 +4,26 @@
 
 # - Import base classes
 from rockpool.layers.layer import Layer
+from rockpool.timeseries import TimeSeries
+
+from jax import jit, grad
+from jax.experimental.optimizers import adam
+
+import itertools
+
 from abc import abstractmethod, ABC
 
 # - Import and define types
-from typing import Dict, Tuple, Any, Callable, Union, List
+from typing import Dict, Tuple, Any, Callable, Union, List, Optional
 
 State = Any
 Params = Union[Dict, Tuple, List]
 
-__all__ = ['JaxTrainedLayer']
+__all__ = ["JaxTrainedLayer"]
 
 # - Import jax elements
 from jax import numpy as np
+
 
 class JaxTrainedLayer(Layer, ABC):
     """
@@ -40,7 +48,7 @@ class JaxTrainedLayer(Layer, ABC):
                 ...
             }
 
-    The ``_pack()`` method must return a dictionary or tuple or other collection of strictly jax-compatible types, that completely define the modifiable parameters for this layer. For example: weights; biases; time constants. Included should be all parameters that one might want to perform gradient descent on. *Excluded* should be parameters that are fixed: for example ``dt``, and ``noise_std``.
+    The :py:meth:`~.JaxTrainedLayer._pack()` method must return a dictionary or tuple or other collection of strictly jax-compatible types, that completely define the modifiable parameters for this layer. For example: weights; biases; time constants. Included should be all parameters that one might want to perform gradient descent on. *Excluded* should be parameters that are fixed: for example ``dt``, and ``noise_std``.
 
     ::
 
@@ -55,7 +63,7 @@ class JaxTrainedLayer(Layer, ABC):
                 ...
             )
 
-    The ``_unpack()`` method must accept a parameters definition as returned by ``_pack()``, and apply those parameters to the layer.
+    The :py:meth:`~.JaxTrainedLayer._unpack()` method must accept a parameters definition as returned by :py:meth:`~.JaxTrainedLayer._pack()`, and apply those parameters to the layer.
 
     ::
 
@@ -63,7 +71,7 @@ class JaxTrainedLayer(Layer, ABC):
         def _evolve_functional(self) -> Callable[]:
             return evol_func
 
-    The property ``_evolve_functional`` must return a *function* ``evol_func()`` with the following calling signature. This function must evolve the state of the layer, given an initial state, set of parameters and raw inputs, with *no side effects*. That means the function must not update the internal state of the layer, or update the `._t` attribute, etc. The function ``evol_func()`` must be compilable with `jax.jit`. An example property and function are shown here::
+    The property :py:attr:`~.JaxTrainedLayer._evolve_functional` must return a *function* ``evol_func()`` with the following calling signature. This function must evolve the state of the layer, given an initial state, set of parameters and raw inputs, with *no side effects*. That means the function must not update the internal state of the layer, or update the `._t` attribute, etc. The function ``evol_func()`` must be compilable with `jax.jit`. An example property and function are shown here::
 
         @property
         def _evolve_functional(self) -> Callable[[Params, State, np.ndarray], Tuple[np.ndarray, State]]:
@@ -85,6 +93,7 @@ class JaxTrainedLayer(Layer, ABC):
 
             return evol_func
     """
+
     @abstractmethod
     def _pack(self) -> Params:
         """
@@ -107,7 +116,9 @@ class JaxTrainedLayer(Layer, ABC):
 
     @property
     @abstractmethod
-    def _evolve_functional(self) -> Callable[[Params, State, np.ndarray], Tuple[np.ndarray, State]]:
+    def _evolve_functional(
+        self,
+    ) -> Callable[[Params, State, np.ndarray], Tuple[np.ndarray, State]]:
         """
         Functional form of evolution for this layer
 
@@ -121,8 +132,211 @@ class JaxTrainedLayer(Layer, ABC):
         :return Callable[[Params, State, np.ndarray)], Tuple[np.ndarray, State]]: evol_func: Evolution function
         """
 
-        def evol_func(params: Params, state: State, input: np.ndarray) -> Tuple[np.ndarray, State]:
+        def evol_func(
+            params: Params, state: State, input: np.ndarray
+        ) -> Tuple[np.ndarray, State]:
             pass
 
         return evol_func
 
+    def train_output_target(
+        self,
+        ts_input: Union[TimeSeries, np.ndarray],
+        ts_target: Union[TimeSeries, np.ndarray],
+        min_tau: Optional[float] = None,
+        is_first: bool = False,
+        is_last: bool = False,
+        loss_fcn: Callable[[Dict, Tuple], float] = None,
+        loss_params: Dict = {},
+        optimizer: Callable = adam,
+        opt_params: Dict = {"step_size": 1e-4},
+    ) -> Tuple[Callable[[], float], Callable[[], float]]:
+        """
+        Perform one trial of Adam stochastic gradient descent to train the reservoir
+
+        :param TimeSeries ts_input:     TimeSeries (or raw sampled signal) to use as input for this trial [TxI]
+        :param TimeSeries ts_target:    TimeSeries (or raw sampled signal) to use as target for this trial [TxO]
+        :param Optional[float] min_tau: Minimum time constant to permit
+        :param bool is_first:           Flag to indicate this is the first trial. Resets learning and causes initialisation.
+        :param bool is_last:            Flag to indicate this is the last trial. Performs clean-up (not essential)
+        :param Callable loss_fcn:       Function that computes the loss for the currently configured layer. Default: :py:func:`loss_mse_reg`
+        :param Dict loss_params:        A dictionary of loss function parameters to pass to the loss function. Must be configured on the very first call to `.train_output_target`; subsequent changes will be ignored. Default: Appropriate parameters for :py:func:`loss_mse_reg`.
+        :param Callable optimizer:      A JAX-style optimizer function. See the JAX docs for details. Default: `jax.experimental.optimizers.adam`
+        :param Dict opt_params:         A dictionary of parameters passed to `optimizer`. Default: {"step_size": 1e-4}
+
+        Use this function to train the output of the reservoir to match a target, given an input stimulus. This function can
+        be called in a loop, passing in randomly-chosen training examples on each call. Parameters of the layer are updated
+        on each call of `.train_adam`, but the layer time and state are *not* updated.
+
+        :return:            (loss_fcn, grad_fcn):
+                                loss_fcn:   Callable[[], float] Function that returns the current loss
+                                grad_fcn:   Callable[[], float] Function that returns the gradient for the current batch
+        """
+
+        # - Set a minimum tau, if not provided
+        if min_tau is None:
+            min_tau = self._dt * 10.0
+
+        # - Get static arguments
+        dt = self._dt
+        params = self._pack()
+        evol_func = self._evolve_functional
+
+        # - Define loss function
+        @jit
+        def loss_mse_reg(
+            params: Params,
+            output_batch_t: np.ndarray,
+            target_batch_t: np.ndarray,
+            min_tau: float,
+            lambda_mse: float = 1.0,
+            reg_tau: float = 10000.0,
+            reg_l2_rec: float = 1.0,
+        ) -> float:
+            """
+            loss_mse_reg() - Loss function for target versus output
+
+            :param Params params:               Set of packed parameters
+            :param np.ndarray output_batch_t:   Output rasterised time series [TxO
+            :param np.ndarray target_batch_t:   Target rasterised time series [TxO]
+            :param float min_tau:               Minimum time constant
+            :param float lambda_mse:            Factor when combining loss, on mean-squared error term. Default: 1.0
+            :param float reg_tau:               Factor when combining loss, on minimum time constant limit. Default: 1e5
+            :param float reg_l2_rec:            Factor when combining loss, on L2-norm term of recurrent weights. Default: 1.
+
+            :return float: Current loss value
+            """
+            # - Measure output-target loss
+            mse = lambda_mse * np.mean((output_batch_t - target_batch_t) ** 2)
+
+            # - Get loss for tau parameter constraints
+            tau_loss = reg_tau * np.mean(
+                np.where(params["tau"] < min_tau, np.exp(-(params["tau"] - min_tau)), 0)
+            )
+
+            # - Measure recurrent L2 norm
+            w_res_norm = reg_l2_rec * np.mean(params["w_recurrent"] ** 2)
+
+            # - Loss: target/output squared error, time constant constraint, recurrent weights norm, activation penalty
+            fLoss = mse + tau_loss + w_res_norm
+
+            # - Return loss
+            return fLoss
+
+        # - Initialise training
+        initialise = is_first or not hasattr(self, "__in_training_sgd_adam")
+
+        if initialise:
+            # - Get optimiser
+            (opt_init, opt_update, get_params) = optimizer(**opt_params.copy())
+            self.__get_params = get_params
+
+            # - Make update function
+            @jit
+            def update_fcn(
+                i: int,
+                opt_state: Any,
+                input_batch_t: np.ndarray,
+                target_batch_t: np.ndarray,
+            ) -> Any:
+                """
+                Perform one round of optimizer update
+
+                :param int i:                       Current optimization iteration
+                :param Any opt_state:               Current optimizer state
+                :param np.ndarray input_batch_t:    Input signal for this batch. Rasterized time series [TxI]
+                :param np.ndarray target_batch_t:   Target signal for this batch. Rasterized time series [TxO]
+
+                :return Any: new_opt_state
+                """
+                # - Get layer parameters
+                opt_params = get_params(opt_state)
+
+                # - Call the layer evolution function
+                output_batch_t, _ = evol_func(opt_params, self._state, input_batch_t)
+
+                # - Get the loss function gradients
+                g = self.__grad_fcn(opt_params, output_batch_t, target_batch_t)
+
+                # - Call optimiser update function
+                return opt_update(i, g, opt_state)
+
+            # - If using default loss, set up parameters
+            if loss_fcn is None:
+                loss_fcn = loss_mse_reg
+                default_loss_params = {
+                    "lambda_mse": 1.0,
+                    "reg_tau": 10000.0,
+                    "reg_l2_rec": 1.0,
+                    "min_tau": self._dt * 10.0,
+                }
+                default_loss_params.update(loss_params)
+                loss_params = default_loss_params
+
+            # - Make a curried loss function, incorporating static loss parameters
+            def loss_curried(
+                opt_params: Params,
+                output_batch_t: np.ndarray,
+                target_batch_t: np.ndarray,
+            ) -> float:
+                """
+                Curried loss function; absorbs loss parameters
+
+                :param Params opt_params:           Current values of the layer parameters, modified by optimization
+                :param np.ndarray output_batch_t:   Output rasterized time series for this batch [TxO]
+                :param np.ndarray target_batch_t:   Target rasterized time series for this batch [TxO]
+
+                :return float:                      Loss value for the parameters in `opt_params`, for the current batch
+                """
+                return loss_fcn(
+                    opt_params, output_batch_t, target_batch_t, **loss_params
+                )
+
+            # - Assign update, loss and gradient functions
+            self.__update_fcn = update_fcn
+            self.__loss_fcn = jit(loss_curried)
+            self.__grad_fcn = jit(grad(loss_curried))
+
+            # - Initialise optimizer
+            self.__opt_state = opt_init(self._pack())
+            self.__itercount = itertools.count()
+
+            # - Assign "in training" flag
+            self.__in_training_sgd_adam = True
+
+        # - Prepare time base and inputs
+        if isinstance(ts_input, TimeSeries):
+            # - Check that `ts_target` is also a time series
+            assert isinstance(
+                ts_target, TimeSeries
+            ), "If `ts_input` is provided as a `TimeSeries` object, then `ts_target` must also be a `TimeSeries`."
+
+            # - Rasterize input and target time series
+            time_base, inps, num_timesteps = self._prepare_input(ts_input, None, None)
+            target = ts_target(time_base)
+        else:
+            # - Use pre-rasterized time series
+            inps = ts_input
+            target = ts_target
+
+        # - Perform one step of optimisation
+        self.__opt_state = self.__update_fcn(
+            next(self.__itercount), self.__opt_state, (inps, target)
+        )
+
+        # - Apply the parameter updates
+        self._unpack(self.__get_params(self.__opt_state))
+
+        # - Reset status, on "is_last" flag
+        if is_last:
+            del self.__in_training_sgd_adam
+
+        # - Return lambdas that evaluate the loss and the gradient
+        return (
+            lambda: self.__loss_fcn(
+                self.__get_params(self.__opt_state), inps, target, self._state
+            ),
+            lambda: self.__grad_fcn(
+                self.__get_params(self.__opt_state), (inps, target), self._state
+            ),
+        )
