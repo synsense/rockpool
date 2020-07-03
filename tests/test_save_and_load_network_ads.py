11019d34dd4529899ce382d599178bbcf0004952
Julian Buechel <julian.buechell@aictx.ai>
2020-07-03 08:47:13 +0200

    Changed .gitignore, updated test for ADS Net

--- tests/test_save_and_load_network_ads.py
+++ tests/test_save_and_load_network_ads.py
@@ -10,7 +10,7 @@ import sys
 
 def test_it():
 
-    def test_network_ads(nInSize, nResSize, nOutSize, activation_func):
+    def test_network_ads(nInSize, nResSize, nOutSize):
 
         w_in = 2*npr.rand(nInSize, nResSize)-1
         w_rec_fast = npr.randn(nResSize, nResSize) / np.sqrt(nResSize)
@@ -19,8 +19,6 @@ def test_it():
         tDt = 0.0001
         noise_std = 0.01
         Nb = nResSize
-        M = np.random.randn(Nb,nResSize)
-        theta = 0.1*np.random.randn(Nb)
         eta = 0.001
         k=0.1
         v_thresh = np.random.randn(nResSize)
@@ -38,8 +36,6 @@ def test_it():
                                 weights_out= w_out,
                                 weights_fast= w_rec_fast,
                                 weights_slow=w_rec_slow,
-                                M=M,
-                                theta=theta,
                                 eta=eta,
                                 k=k,
                                 noise_std=noise_std,
@@ -51,7 +47,7 @@ def test_it():
                                 tau_syn_r_fast=tau_syn_r_fast,
                                 tau_syn_r_slow=tau_syn_r_slow,
                                 tau_syn_r_out=tau_syn_out,
-                                phi=activation_func,
+                                discretize_dynapse = True,
                                 record=True)
 
         # Store the network in a temporary file
@@ -67,8 +63,6 @@ def test_it():
             assert(np.array_equal(net.lyrRes.weights_out, network_loaded.lyrRes.weights_out)), "weights_out are not the same"
             assert(np.array_equal(net.lyrRes.weights_fast, network_loaded.lyrRes.weights_fast)), "weights_fast are not the same"
             assert(np.array_equal(net.lyrRes.weights_slow, network_loaded.lyrRes.weights_slow)), "weights_slow are not the same"
-            assert(np.array_equal(net.lyrRes.M, network_loaded.lyrRes.M)), "M matrix is not the same"
-            assert(np.array_equal(net.lyrRes.theta, network_loaded.lyrRes.theta)), "theta is not the same"
             assert(np.array_equal(net.lyrRes.eta, network_loaded.lyrRes.eta)), "eta not the same"
             assert(np.array_equal(net.lyrRes.k, network_loaded.lyrRes.k)), "k not the same"
             assert(np.array_equal(net.lyrRes.noise_std, network_loaded.lyrRes.noise_std)), "noise_std are not the same"
@@ -79,11 +73,11 @@ def test_it():
             assert(np.array_equal(net.lyrRes.tau_mem, network_loaded.lyrRes.tau_mem)), "tau_mem are not the same"
             assert(np.array_equal(net.lyrRes.tau_syn_r_fast, network_loaded.lyrRes.tau_syn_r_fast)), "tau_syn_r_fast are not the same"
             assert(np.array_equal(net.lyrRes.tau_syn_r_slow, network_loaded.lyrRes.tau_syn_r_slow)), "tau_syn_r_slow are not the same"
-            assert(np.array_equal(net.lyrRes.phi_name, network_loaded.lyrRes.phi_name)), "phi are not the same"
-
+            assert(net.lyrRes.discretize_dynapse == True), "discretize_dynapse is not equal"
+            assert(net.lyrRes.num_synapse_states == -1), "num_synapse_states is not as expected"
             assert(np.array_equal(net.output_layer.tau_syn, network_loaded.output_layer.tau_syn)), "tau_syn_out of output does not match"
 
-    test_network_ads(2,100,2,"tanh")
+    test_network_ads(2,100,2)
     
 
 test_it()
