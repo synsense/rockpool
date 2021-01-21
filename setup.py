853f98d2182cc8ce5ac89ea902b8c10e87346793
Philpp Weidel <philipp.weidel@aictx.ai>
2021-01-21 16:45:54 +0100

    not requireing torch in setup

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
+import logging
 from distutils.core import setup
 import setuptools
 from setuptools import Extension
-from torch.utils import cpp_extension
 
 # - Read version
 exec(open("rockpool/version.py").read())
 
-setup(
-    name="rockpool",
-    author="SynSense",
-    author_email="dylan.muir@synsense.ai",
-    version=__version__,
-    packages=setuptools.find_packages(),
-    install_requires=["numba", "numpy", "scipy"],
-    extras_require={
+setup_args = {
+    "name": "rockpool",
+    "author": "SynSense",
+    "author_email": "dylan.muir@synsense.ai",
+    "version": __version__,
+    "packages": setuptools.find_packages(),
+    "install_requires": ["numba", "numpy", "scipy"],
+    "extras_require": {
         "all": [
             "tqdm",
             "brian2",
@@ -33,21 +33,26 @@ setup(
             "pandoc",
         ]
     },
-    description="A Python package for developing, simulating and training spiking neural networks, and deploying on neuromorphic hardware",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    classifiers=[
+    "description": "A Python package for developing, simulating and training spiking neural networks, and deploying on neuromorphic hardware",
+    "long_description": open("README.md").read(),
+    "long_description_content_type": "text/markdown",
+    "classifiers": [
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
     ],
-    keywords="spiking neural network SNN neuromorphic",
-    python_requires=">=3.6",
-    project_urls={
+    "keywords": "spiking neural network SNN neuromorphic",
+    "python_requires": ">=3.6",
+    "project_urls": {
         "Source Code": "https://gitlab.com/SynSense/rockpool",
         "Documentation": "https://rockpool.ai",
         "Bug Tracker": "https://gitlab.com/SynSense/rockpool/-/issues",
     },
+}
+
+try:
+    from torch.utils import cpp_extension
+
     # cpp extensions
     ext_modules=[
         cpp_extension.CppExtension(
@@ -57,9 +62,13 @@ setup(
                 "rockpool/nn/modules/torch/cpp/threshold.cpp",
                 "rockpool/nn/modules/torch/cpp/bitshift.cpp",
             ],
-            extra_compile_args=['-O3'],
-            optional=True,
-        )
-    ],
-    cmdclass={"build_ext": cpp_extension.BuildExtension},
-)
+            extra_compile_args=['-O3']),
+        ]
+    
+    cmdclass = {"build_ext": cpp_extension.BuildExtension}
+
+    setup(ext_modules=ext_modules, cmdclass=cmdclass, **setup_args)
+except:
+    log.warn("The Torch C++ extension could not be compiled")
+    setup(**setup_args)
+
