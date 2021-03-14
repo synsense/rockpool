e93e574506d114576f1cd0da4bcaa8d76b08251c
Dylan Richard Muir <dylan.muir@aictx.ai>
2021-03-14 21:45:01 +0100

    * Moved and renamed docker image * Updated CI

--- make_docker.sh
+++
@@ -1,4 +0,0 @@
-cp requirements.yml docker
-docker login spinystellate.office.synsense.ai:5051
-docker build -t spinystellate.office.synsense.ai:5051/research/rockpool .docker
-docker push spinystellate.office.synsense.ai:5051/research/rockpool
