fa16f3457ef8b8fe9dac3f6380a03500aa09533f
Dylan Muir <dylan.richard.muir@gmail.com>
2021-03-25 15:28:45 +0100

    * Docker image now pushed to public gitlab * Improvement to CI on public gitlab

--- images/make_docker.sh
+++ images/make_docker.sh
@@ -3,11 +3,19 @@
 #
 
 # - Login to container repository
-docker login spinystellate.office.synsense.ai:5051
+# docker login spinystellate.office.synsense.ai:5051
+docker login registry.gitlab.com
 
 # - Build and push the test container
-docker build -t spinystellate.office.synsense.ai:5051/research/rockpool/test:latest test
-docker push spinystellate.office.synsense.ai:5051/research/rockpool/test
+# docker build -t spinystellate.office.synsense.ai:5051/research/rockpool/test:latest test
+# docker push spinystellate.office.synsense.ai:5051/research/rockpool/test
+
+# - Get the requirements file
+cp ../requirements.yml test
+#cp ../requirements.yml deploy
+
+docker build -t registry.gitlab.com/synsense/rockpool/test:latest test
+docker push registry.gitlab.com/synsense/rockpool/test
 
 # - Build and push the deployed container
 # docker build -t spinystellate.office.synsense.ai:5051/research/rockpool/deploy:latest deploy
