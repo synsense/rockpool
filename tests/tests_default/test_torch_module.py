8dd40bc7adacee001cecfe11d29d15b87277c228
Michel Perez <michel.perez@gmx.ch>
2021-06-09 14:45:57 +0200

    add test for `LIFNeuronTorch`

--- tests/tests_default/test_torch_module.py
+++ tests/tests_default/test_torch_module.py
@@ -230,6 +230,47 @@ def test_LIFTorch():
     for _, obj in rd.items():
         assert obj.shape == input_data.shape
 
+def test_LIFNeuronTorch():
+    from rockpool.nn.modules.torch.lif_neuron_torch import LIFNeuronTorch
+    import torch
+
+    n_neurons = 10
+    n_batches = 3
+    T = 20
+    tau_mem = torch.rand(1, n_neurons)
+    tau_syn = torch.rand(1, n_neurons)
+    bias = torch.rand(1, n_neurons)
+    w_rec = torch.rand(n_neurons, n_neurons)
+
+    mod = LIFNeuronTorch(
+        shape=(n_neurons,),
+        tau_mem=tau_mem,
+        bias=bias,
+        has_bias=True,
+        dt=1e-3,
+        noise_std=0.1,
+        device=None,
+        dtype=None,
+        record=True
+    )
+
+    # - Generate some data
+    input_data = torch.rand(n_batches, T, n_neurons, requires_grad=True)
+
+    # - Test torch interface
+    out = mod.forward(input_data)
+
+    out.sum().backward()
+
+    # - Test Rockpool interface
+    out, ns, rd = mod.evolve(input_data)
+
+    assert out.shape == input_data.shape
+    for _, obj in ns.items():
+        assert obj.shape == (1, n_neurons)
+    for _, obj in rd.items():
+        assert obj.shape == input_data.shape
+
 def test_single_neuron():
     from rockpool.nn.modules.torch.lif_bitshift_torch import LIFBitshiftTorch
     import torch
