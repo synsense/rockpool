1121d24c9634058f974f9f87497a95e2ab5b7c3e
Michel Perez <michel.perez@gmx.ch>
2021-07-01 13:49:41 +0200

    add test for up down ADM Torch module

---
+++ tests/tests_default/test_updown_torch.py
@@ -0,0 +1,41 @@
+import pytest
+
+
+def test_FFUpDownTorch():
+    from rockpool.nn.modules.torch.updown_torch import FFUpDownTorch
+    import torch
+
+    n_channels = 10
+    n_batches = 3
+    T = 20
+    thr_up = torch.rand(n_channels)
+    thr_down = torch.rand(n_channels)
+    models = []
+    # - Test minimal initialisation
+    models.append(FFUpDownTorch((n_channels,)))
+
+    # - Test maximal initialisation
+    models.append(FFUpDownTorch(
+        shape=(n_channels,),
+        thr_up=thr_up,
+        thr_down=thr_down,
+        repeat_output=1,
+        dt=1e-3,
+        device=None,
+        dtype=None,
+    ))
+
+    # - Generate some data
+    input_data = torch.rand(n_batches, T, n_channels, requires_grad=True)
+
+    # - Test the different initialisations
+    for mod in models:
+        # - Test torch interface
+        out = mod.forward(input_data)
+
+        out.sum().backward()
+
+        # - Test Rockpool interface
+        out, ns, rd = mod(input_data, record=True)
+
+        assert out.shape == (n_batches, T, 2*n_channels)
