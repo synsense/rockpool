e659c8a2ed8cd38afdca0dbfb187d1d07033720f
ugurcancakal <ugurcan.cakal@gmail.com>
2021-11-11 12:05:34 +0300

    BUG FIX : Index map dict * Extract the dict  from the sim_config object in default construction

--- rockpool/devices/dynapse/dynapse1_simulator.py
+++ rockpool/devices/dynapse/dynapse1_simulator.py
@@ -159,6 +159,8 @@ class DynapSE1Jax(DynapSE1NeuronSynapseJax):
                 sim_config = DynapSE1SimBoard.from_config(None, idx_map_dict["w_rec"])
             else:
                 sim_config = DynapSE1SimBoard(shape[-1])
+                idx_map_dict = {}
+                idx_map_dict["w_rec"] = sim_config.idx_map
 
         super().__init__(
             shape,
