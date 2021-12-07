887e2a1f2cc92536499cc6966c6cd8ed0c6191fd
Philipp Weidel <philipp.weidel@synsense.ai>
2021-12-07 10:54:44 +0100

    use neuron model in block

--- rockpool/nn/networks/wavesense.py
+++ rockpool/nn/networks/wavesense.py
@@ -333,6 +333,7 @@ class WaveSenseNet(TorchModule):
                 tau_mem=tau_mem,
                 base_tau_syn=base_tau_syn,
                 threshold=threshold,
+                neuron_model=neuron_model,
                 dt=dt,
                 device=device,
             )
