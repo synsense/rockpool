4e34b20b3d2794df03e3168bfffeb984d6621b71
Philipp Weidel <philipp.weidel@synsense.ai>
2021-12-09 10:39:02 +0100

    minor cleanup

--- rockpool/nn/modules/torch/lif_slayer.py
+++ rockpool/nn/modules/torch/lif_slayer.py
@@ -78,10 +78,6 @@ class LIFSlayer(LIFBaseTorch):
 
     
     def forward_leak(self, inp: torch.Tensor, alpha, state):
-
-        out_state = LeakyIntegrator.apply(
-            inp, state.flatten().contiguous(), alpha
-        )
     
         return out_state 
 
@@ -109,72 +105,62 @@ class LIFSlayer(LIFBaseTorch):
                 )
             )
 
+        # Bring input to expected format for rockpool
+        data = data.reshape(n_batches, time_steps, self.n_neurons, self.n_synapses)
 
-        # - Replicate states out by batches
+        # Replicate states out by batches
         vmem = torch.ones(n_batches, self.n_neurons).to(data.device) * self.vmem
-        vmem = vmem.squeeze().repeat(1, time_steps)
-
         isyn = (
             torch.ones(n_batches, self.n_neurons, self.n_synapses).to(data.device)
             * self.isyn
         )
-
-        bias = torch.ones(n_batches, self.n_neurons).to(data.device) * self.bias
         spikes = torch.zeros(n_batches, self.n_neurons).to(data.device) * self.spikes
-        spikes = spikes.squeeze()
 
-        # - Set up state record and output
-        if self._record:
-            self._record_Vmem = torch.zeros(n_batches, time_steps, self.n_neurons)
-            self._record_Isyn = torch.zeros(
-                n_batches, time_steps, self.n_neurons, self.n_synapses
-            )
-
-        self._record_spikes = torch.zeros(
-            n_batches, time_steps, self.n_neurons, device=data.device
-        )
-
-
-        # leak
-        data = data.reshape(n_batches, time_steps, self.n_neurons, self.n_synapses)
-#        .movedim(-1, 1).reshape(n_batches * n_connections, time_steps)
-        inp = torch.zeros(n_batches * self.n_neurons, self.n_synapses, time_steps).to(data.device)
+        # Exponential leak
+        # Generate buffer for synaptic current
+        isyn_slayer = torch.zeros(n_batches * self.n_neurons, self.n_synapses, time_steps).to(data.device)
 
         for syn in range(self.n_synapses):
-            inp[:, syn] = self.forward_leak(data[:, :, :, syn].movedim(1, -1).reshape(n_batches * self.n_neurons, time_steps), 
-                                            self.beta[0, syn], 
-                                            isyn[:, :, syn])
+            # bring data into format expected by slayer
+            inp = data[:, :, :, syn].movedim(1, -1).reshape(n_batches * self.n_neurons, time_steps)
 
-        output_spikes, vmem = SpikeFunctionIterForward.apply(
-                inp.sum(1), # input
+
+            isyn_slayer[:, syn] = LeakyIntegrator.apply(inp, 
+                                                        isyn[:, :, syn].flatten().contiguous(),
+                                                        self.beta[0, syn])
+
+
+
+        spikes, vmem_slayer = SpikeFunctionIterForward.apply(
+                isyn_slayer.sum(1), # input
                 self.threshold[0], #membrane subtract
                 self.alpha[0].item(), # alpha
-                vmem, # init state
-                spikes, # last activations
+                vmem.squeeze(), # init state
+                spikes.squeeze(), # last activations
                 self.threshold[0], # threshold
                 None, # threshold low
                 self.learning_window, #learning window
                 1.0, #scale grads
                 )
         
-        vmem = vmem.reshape(n_batches, self.n_neurons, time_steps).movedim(1, -1)
-        spikes = output_spikes.reshape(n_batches, self.n_neurons, time_steps).movedim(1, -1)
+        # Bring states to rockpool dimensions
+        isyn_slayer = isyn_slayer.reshape(n_batches, self.n_neurons, self.n_synapses, time_steps).movedim(-1, 1)
+        vmem_slayer = vmem_slayer.reshape(n_batches, self.n_neurons, time_steps).movedim(-1, 1)
+        spikes = spikes.reshape(n_batches, self.n_neurons, time_steps).movedim(-1, 1)
 
-        vmem = vmem - spikes * self.threshold[0]
-        inp = inp.reshape(n_batches, self.n_neurons, self.n_synapses, time_steps).movedim(-1, 1)
+        vmem_slayer = vmem_slayer - spikes * self.threshold[0]
 
+        # recording
         if self._record:
             # recording
-            self._record_Vmem = vmem.detach()
-            self._record_Isyn = inp.detach()
+            self._record_Vmem = vmem_slayer.detach()
+            self._record_Isyn = isyn_slayer.detach()
 
         self._record_spikes = spikes
 
-        self.vmem = vmem[0, -1].detach()
-        self.isyn = inp[0, -1].detach()
+        self.vmem = vmem_slayer[0, -1].detach()
+        self.isyn = isyn_slayer[0, -1].detach()
         self.spikes = spikes[0, -1].detach()
-
-        self._record_spikes
 
         return self._record_spikes
 
