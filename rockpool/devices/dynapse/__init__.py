22781b4f4d6752a1acf59cf4bedb4234aff80e7f
ugurcancakal <ugurcan.cakal@gmail.com>
2021-12-13 18:58:18 +0300

    Import submodules in __init__

--- rockpool/devices/dynapse/__init__.py
+++ rockpool/devices/dynapse/__init__.py
@@ -6,6 +6,46 @@ DynapSE-family device simulations, deployment and HDK support
 from warnings import warn
 
 try:
-    from .virtual_dynapse import VirtualDynapse
+    from .adexplif_jax import *
+except (ModuleNotFoundError, ImportError) as err:
+    warn("Could not load package:" + str(err))
+
+try:
+    from .biasgen import *
+except (ModuleNotFoundError, ImportError) as err:
+    warn("Could not load package:" + str(err))
+
+try:
+    from .dpi_gui import *
+except (ModuleNotFoundError, ImportError) as err:
+    warn("Could not load package:" + str(err))
+
+try:
+    from .dynapse1_jax import *
+except (ModuleNotFoundError, ImportError) as err:
+    warn("Could not load package:" + str(err))
+
+try:
+    from .figure import *
+except (ModuleNotFoundError, ImportError) as err:
+    warn("Could not load package:" + str(err))
+
+try:
+    from .fpga_jax import *
+except (ModuleNotFoundError, ImportError) as err:
+    warn("Could not load package:" + str(err))
+
+try:
+    from .router import *
+except (ModuleNotFoundError, ImportError) as err:
+    warn("Could not load package:" + str(err))
+
+try:
+    from .simconfig import *
+except (ModuleNotFoundError, ImportError) as err:
+    warn("Could not load package:" + str(err))
+
+try:
+    from .utils import *
 except (ModuleNotFoundError, ImportError) as err:
     warn("Could not load package:" + str(err))
