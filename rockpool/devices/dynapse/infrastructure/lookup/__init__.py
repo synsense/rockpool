41f26fd78d9e79913cc485ad2640705b2d6bba0c
ugurcancakal <ugurcan.cakal@gmail.com>
2022-01-14 19:41:38 +0300

    transistor lookup tables for bias generators * as python code to avoid changing behavior across operating systems

---
+++ rockpool/devices/dynapse/infrastructure/lookup/__init__.py
@@ -0,0 +1,5 @@
+"""
+    Lookup tables obtained from 
+    https://hardware.basket.office.synsense.ai/documentation/dynapse2docs/Sections/input_interface_config.html
+    for more accurate simulation of DynapSE-2 bias generator
+"""
