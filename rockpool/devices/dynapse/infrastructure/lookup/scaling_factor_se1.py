e5c462023afec73ec76a4ddd594abfb73d7d5fda
ugurcancakal <ugurcan.cakal@gmail.com>
2022-01-17 16:57:51 +0300

    empty spaces in keys fixex

---
+++ rockpool/devices/dynapse/infrastructure/lookup/scaling_factor_se1.py
@@ -0,0 +1,37 @@
+"""
+Dynap-SE1 circuit bias generator transistor current response scaling factors
+Neither experimental nor theoretical. Waiting for tests to be verified!
+
+Project Owner : Dylan Muir, SynSense AG
+Author : Ugurcan Cakal
+E-mail : ugurcan.cakal@gmail.com
+17/01/2022
+"""
+
+table = {
+    "IF_AHTAU_N": 1.0,
+    "IF_AHTHR_N": 1.0,
+    "IF_AHW_P": 1.0,
+    "IF_BUF_P": 1.0,
+    "IF_CASC_N": 1.0,
+    "IF_DC_P": 1.0,
+    "IF_NMDA_N": 1.0,
+    "IF_RFR_N": 1.0,
+    "IF_TAU1_N": 1e-4,
+    "IF_TAU2_N": 1.0,
+    "IF_THR_N": 1.0,
+    "NPDPIE_TAU_F_P": 1.0,
+    "NPDPIE_TAU_S_P": 1.0,
+    "NPDPIE_THR_F_P": 1.0,
+    "NPDPIE_THR_S_P": 1.0,
+    "NPDPII_TAU_F_P": 1.0,
+    "NPDPII_TAU_S_P": 1.0,
+    "NPDPII_THR_F_P": 1.0,
+    "NPDPII_THR_S_P": 1.0,
+    "PS_WEIGHT_EXC_F_N": 1.0,
+    "PS_WEIGHT_EXC_S_N": 1.0,
+    "PS_WEIGHT_INH_F_N": 1.0,
+    "PS_WEIGHT_INH_S_N": 1.0,
+    "PULSE_PWLK_P": 1.0,
+    "R2R_P": 1.0,
+}
