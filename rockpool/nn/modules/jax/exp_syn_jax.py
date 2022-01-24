* Updated ExpSyn to precisely numerically match Jax and Torch implementations * Fixed bug in ExpSynJax, where isyn would not be de-batched * ExpSynTorch no longer records the output * Updated unit test to compare ExpSyn defaults and dynamics

rockpool/nn/modules/jax/exp_syn_jax.py
@@ -51,7 +51,7 @@ class ExpSynJax(JaxModule):
 
         Args:
             shape (Optional[tuple]): The number of units in this module ``(N,)``.
-            tau (Optional[np.ndarray]): Concrete initialisation data to use for the time constants of the synapses. Default: shared 100 ms for all synapses.
+            tau (Optional[np.ndarray]): Concrete initialisation data to use for the time constants of the synapses. Default: trainable individual 10 ms for all synapses.
             dt (float): The time step for simulation, in seconds. Default: 1 ms
         """
         # - Call super-class initialisation
@@ -87,7 +87,7 @@ class ExpSynJax(JaxModule):
         """ (np.ndarray) Time constant of each synapse """
 
         # - Initialise noise std dev
-        self.noise_std: rt.P_tensor = SimulationParameter(noise_std, cast_fn=np.array)
+        self.noise_std: rt.P_float = SimulationParameter(noise_std, cast_fn=np.array)
         """ (float) Noise std. dev after 1 second """
 
         self.isyn: Union[np.array, State] = State(
@@ -131,4 +131,4 @@ class ExpSynJax(JaxModule):
         isyn, output = scan_time(isyn, input_data + noise_ts)
 
         # - Return output data and state
-        return output, {"isyn": isyn}, {}
+        return output, {"isyn": isyn[0]}, {}

rockpool/nn/modules/native/exp_syn.py
@@ -5,12 +5,13 @@ Implement a fast exponential synapse layer using convolution
 # - Rockpool imports
 from rockpool.nn.modules import Module
 from rockpool.parameters import Parameter, State, SimulationParameter
+from rockpool.typehints import P_float
 
 # - Other imports
 import numpy as np
 import scipy.signal as sig
 
-from typing import Union
+from typing import Union, Optional
 
 __all__ = ["ExpSyn"]
 
@@ -24,8 +25,9 @@ class ExpSyn(Module):
 
     def __init__(
         self,
-        shape: tuple = None,
-        tau: np.array = None,
+        shape: Union[int, tuple],
+        tau: Optional[np.array] = None,
+        noise_std: float = 0.0,
         dt: float = 1e-3,
         max_window_length: int = 1e6,
         *args,
@@ -36,20 +38,15 @@ class ExpSyn(Module):
 
         Args:
             shape (Optional[tuple]): The number of synapses in this module ``(N,)``. If not provided, the shape will be extracted from ``tau``.
-            tau (Optional[np.ndarray]): Concrete initialisation data for the time constants of the synapses, in seconds. Default: 100 ms for all synapses.
+            tau (Optional[np.ndarray]): Concrete initialisation data for the time constants of the synapses, in seconds. Default: 10 ms individual for all synapses.
             dt (float): The timestep of this module, in seconds. Default: 1 ms.
-            max_window_length (int):
+            max_window_length (int): The largest window to use when pre-generating synaptic kernels. Default: 1e6.
         """
         # - Work out the shape of this module
-        if shape is None:
-            assert (
-                tau is not None
-            ), "You must provide either `shape` or else specify concrete parameters."
-            shape = np.array(tau).shape
-
-        # - Check that the shape is reasonable
         if np.size(shape) > 1:
-            raise ValueError("The `shape` argument must be one-dimensional.")
+            raise ValueError(
+                "The `shape` argument must be one-dimensional for an ExpSyn module."
+            )
 
         # - Call super-class initialisation
         super().__init__(shape=shape, *args, **kwargs)
@@ -61,17 +58,21 @@ class ExpSyn(Module):
         self.max_window_length: Union[int, SimulationParameter] = SimulationParameter(
             max_window_length
         )
-        """ Maximum window length for convolution """
+        """ (int) Maximum window length for convolution """
+
+        # - Initialise noise std dev
+        self.noise_std: P_float = SimulationParameter(noise_std, cast_fn=np.array)
+        """ (float) Noise std. dev after 1 second """
 
         self.tau: Union[np.array, Parameter] = Parameter(
-            shape=self.shape,
             data=tau,
             family="taus",
-            init_func=lambda s: 100e-3 * np.ones(s),
+            shape=[(self.size_in,), ()],
+            init_func=lambda s: 10e-3 * np.ones(s),
         )
-        """ Time constant of each synapse """
+        """ (np.ndarray) Time constant of each synapse ``(Nin,)`` or ``()`` """
 
-        self.Isyn: Union[np.array, State] = State(
+        self.isyn: Union[np.array, State] = State(
             shape=self.shape, init_func=np.zeros,
         )
 
@@ -82,7 +83,7 @@ class ExpSyn(Module):
         )
 
         # - Compute window normalised time base
-        time_base = [-np.arange(0, window_length) * self.dt] * self.size_out
+        time_base = [-np.arange(1, window_length + 1) * self.dt] * self.size_out
         time_base = np.array(time_base) / np.atleast_2d(self.tau).T
 
         # - Compute exponentials
@@ -91,29 +92,42 @@ class ExpSyn(Module):
     def evolve(
         self, input_data: np.array, *args, **kwargs,
     ) -> (np.ndarray, dict, dict):
+        # - Expand states and data over batches
+        input_data, (isyn, window) = self._auto_batch(
+            input_data, (self.isyn, self._window)
+        )
+        n_batches, n_timesteps, _ = input_data.shape
+        window = np.broadcast_to(
+            self._window, (n_batches, self._window.shape[0], self.size_in)
+        )
+
         # - Compute roll-over decay from last evolution
         rollover = np.zeros(input_data.shape)
-        rollover[0, :] = self.Isyn
+        rollover[:, 0, :] = isyn
         rollover = sig.fftconvolve(
-            rollover, self._window[: input_data.shape[0]], axes=0, mode="full"
+            rollover, window[:, :n_timesteps, :], axes=1, mode="full",
         )
 
         # - Perform temporal convolution on input
         output_data = (
-            sig.fftconvolve(
-                input_data, self._window[: input_data.shape[0]], axes=0, mode="full"
-            )
+            sig.fftconvolve(input_data, window[:, :n_timesteps, :], axes=1, mode="full")
             + rollover
         )
 
-        # - Record final state for use in next evolution
-        self.Isyn = output_data[input_data.shape[0], :]
-
         # - Trim output to input shape
-        output_data = output_data[: input_data.shape[0]]
+        output_data = output_data[:, :n_timesteps, :]
+
+        # - Add noise
+        if self.noise_std > 0.0:
+            output_data += (
+                self.noise_std * np.sqrt(self.dt) * np.random.randn(*output_data.shape)
+            )
+
+        # - Record final state for use in next evolution
+        self.isyn = output_data[0, -1, :]
 
         # - Return output along with new state
-        return output_data, {"Isyn": self.Isyn}, {}
+        return output_data, self.state(), {}
 
     @property
     def tau(self) -> float:

rockpool/nn/modules/torch/exp_syn_torch.py
@@ -61,7 +61,7 @@ class ExpSynTorch(TorchModule):
         to_float_tensor = lambda x: torch.tensor(x).float()
 
         # - Initialise tau
-        self.tau_syn: rt.P_tensor = rp.Parameter(
+        self.tau: rt.P_tensor = rp.Parameter(
             tau,
             shape=[(self.size_out,), ()],
             family="taus",
@@ -91,13 +91,10 @@ class ExpSynTorch(TorchModule):
     ) -> Tuple[Any, Any, Any]:
 
         # - Evolve the module
-        output_data, states, _ = super().evolve(input_data, record)
-
-        # - Build a record dictionary
-        record_dict = {"isyn": self._isyn_rec} if record else {}
+        output_data, _, _ = super().evolve(input_data, record)
 
         # - Return the result of evolution
-        return output_data, states, record_dict
+        return output_data, self.state(), {}
 
     def forward(self, data: torch.Tensor) -> torch.Tensor:
         """
@@ -119,10 +116,10 @@ class ExpSynTorch(TorchModule):
         n_batches, time_steps, _ = data.shape
 
         # - Build a tensor to compute and return internal state
-        self._isyn_rec = torch.zeros(data.shape, device=data.device)
+        isyn_ts = torch.zeros(data.shape, device=data.device)
 
         # - Compute decay factor
-        beta = torch.exp(-self.dt / self.tau_syn)
+        beta = torch.exp(-self.dt / self.tau)
         noise_zeta = self.noise_std * torch.sqrt(torch.tensor(self.dt))
 
         # - Loop over time
@@ -131,10 +128,10 @@ class ExpSynTorch(TorchModule):
                 isyn.shape, device=isyn.device
             )
             isyn *= beta
-            self._isyn_rec[:, t, :] = isyn[:, 0, :]
+            isyn_ts[:, t, :] = isyn[:, 0, :]
 
         # - Store the final state
         self.isyn = isyn[0].detach()
 
         # - Return the evolved synaptic current
-        return self._isyn_rec
+        return isyn_ts

tests/tests_default/test_standard_dynamics.py
@@ -8,6 +8,10 @@ from typing import List
 from rockpool.typehints import Tree
 
 
+class MismatchError(ValueError):
+    pass
+
+
 def compare_value_tree(results: List[Tree], Classes: List[type], atol: float = 1e-4):
     def to_numpy(x):
         if isinstance(x, torch.Tensor):
@@ -29,9 +33,12 @@ def compare_value_tree(results: List[Tree], Classes: List[type], atol: float = 1
             mismatch = tree_find(mismatch_params)
 
             if len(mismatch) > 0:
-                raise ValueError(
-                    f"Comparing {Classes[0].__name__} and {Classes[class_index].__name__}\nSome elements of default parameters do not match:\n{mismatch}"
+                raise MismatchError(
+                    f"Comparing {Classes[0].__name__} and {Classes[class_index].__name__}\nSome elements of comaprison values do not match:\n{mismatch}"
                 )
+        except MismatchError:
+            raise
+
         except Exception as e:
             raise ValueError(
                 f"Error comparing classes {Classes[0].__name__} and {Classes[class_index].__name__}:\n"
@@ -157,6 +164,59 @@ def test_rate_dynamics():
             "rec_output": out_rec,
             "rec_states": dict(ns_rec),
             "rec_record": dict(r_d_rec),
+        }
+
+    # - Get dynamics for each module class
+    results = [get_dynamics(M) for M in Module_classes]
+
+    compare_value_tree(results, Module_classes)
+
+
+def test_expsyn_defaults():
+    from rockpool.nn.modules import ExpSyn, ExpSynJax, ExpSynTorch
+
+    Module_classes = [ExpSyn, ExpSynJax, ExpSynTorch]
+
+    N = 2
+
+    def get_defaults(Mod_class: type) -> dict:
+        mod = Mod_class(N)
+        params = mod.parameters()
+        simparams = mod.simulation_parameters()
+        simparams.pop("max_window_length", None)
+
+        return {"params": dict(params), "simparams": dict(simparams)}
+
+    # - Get default parameters for each module class
+    results = [get_defaults(M) for M in Module_classes]
+
+    compare_value_tree(results, Module_classes)
+
+
+def test_expsyn_dynamics():
+    from rockpool.nn.modules import ExpSyn, ExpSynJax, ExpSynTorch, TorchModule
+    import numpy as np
+    import torch
+
+    Module_classes = [ExpSyn, ExpSynJax, ExpSynTorch]
+
+    Nin = 2
+    batches = 3
+    T = 10
+    input_data = np.random.rand(batches, T, Nin)
+
+    def get_dynamics(Mod_class: type):
+        is_torch = issubclass(Mod_class, TorchModule)
+        this_data = torch.from_numpy(input_data).float() if is_torch else input_data
+
+        mod = Mod_class((Nin,))
+        out, ns, r_d = mod(this_data, record=True)
+        ns.pop("rng_key", None)
+
+        return {
+            "output": out,
+            "states": dict(ns),
+            "record": dict(r_d),
         }
 
     # - Get dynamics for each module class