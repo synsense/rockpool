* Updated random initialisation to align between Linear, LinearJax, LinearTorch for proper Kaiming initialisation * Fixed error in torch PWL surrogate gradient * Sequential combinator implements _wrap_recorded_state() method * Fixed bug in Jax auto-batching for scalar input * LIFJax implements _wrap_recorded_state() method * LIF implements _wrap_recorded_state() method

rockpool/nn/modules/torch/lif_torch.py
@@ -49,7 +49,7 @@ class StepPWL(torch.autograd.Function):
     def backward(ctx, grad_output):
         (data,) = ctx.saved_tensors
         grad_input = grad_output.clone()
-        grad_input[data < -ctx.window] = 0
+        grad_input[(data - ctx.threshold) < -ctx.window] = 0
         return grad_input, None, None
 
 