* Fixed shape errors in LIF and LIFJax, and ExpSynTorch * Fixed surrogate gradient for LIFJax * Fixed ExpSynTorch unit test to reflect shape fix * Standardisation unit tests now check shapes as well * Added unit test for LIF gradients and surrogate gradients

rockpool/nn/modules/native/lif.py
@@ -85,7 +85,7 @@ class LIF(Module):
         w_rec: Optional[FloatVector] = None,
         has_rec: bool = False,
         weight_init_func: Optional[Callable[[Tuple], np.ndarray]] = kaiming,
-        threshold: FloatVector = 1.0,
+        threshold: Optional[FloatVector] = None,
         noise_std: float = 0.0,
         max_spikes_per_dt: P_int = np.inf,
         dt: float = 1e-3,
@@ -169,7 +169,11 @@ class LIF(Module):
         """ (np.ndarray) Neuron bias currents `(Nout,)` or `()` """
 
         self.threshold: P_ndarray = Parameter(
-            threshold, shape=[(self.size_out,), ()], cast_fn=np.array
+            threshold,
+            "threshold",
+            init_func=np.ones,
+            shape=[(self.size_out,), ()],
+            cast_fn=np.array,
         )
         """ (np.ndarray) Firing threshold for each neuron `(Nout,)` or `()`"""
 

rockpool/nn/modules/torch/exp_syn_torch.py
@@ -85,7 +85,7 @@ class ExpSynTorch(TorchModule):
 
         # - Initialise state
         self.isyn: rt.P_tensor = rp.State(
-            shape=(1, self.size_out,), init_func=lambda s: torch.zeros(*s),
+            shape=(self.size_out,), init_func=lambda s: torch.zeros(*s),
         )
         """ (torch.tensor) Synaptic current state for each synapse ``(1, N)`` """
 
@@ -129,13 +129,13 @@ class ExpSynTorch(TorchModule):
         beta = torch.exp(-self.dt / self.tau)
         noise_zeta = self.noise_std * torch.sqrt(torch.tensor(self.dt))
 
+        data = data + noise_zeta * torch.randn(data.shape)
+
         # - Loop over time
         for t in range(time_steps):
-            isyn += data[:, t : t + 1, :] + noise_zeta * torch.randn(
-                isyn.shape, device=isyn.device
-            )
+            isyn += data[:, t, :]
             isyn *= beta
-            isyn_ts[:, t, :] = isyn[:, 0, :]
+            isyn_ts[:, t, :] = isyn
 
         # - Store the final state
         self.isyn = isyn[0].detach()

rockpool/nn/modules/torch/linear_torch.py
@@ -87,7 +87,7 @@ class LinearTorch(TorchModule):
         if has_bias:
             self.bias: Union[torch.Tensor, rp.Parameter] = rp.Parameter(
                 bias,
-                shape=self.shape,
+                shape=[(self.size_out,), ()],
                 init_func=lambda s: init.uniform_(
                     torch.empty(s[-1]), -math.sqrt(1 / s[0]), math.sqrt(1 / s[0]),
                 ),