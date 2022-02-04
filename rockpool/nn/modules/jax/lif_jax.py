* Fixed errors in gradient functions for LIFJax and LIFTorch

rockpool/nn/modules/jax/lif_jax.py
@@ -59,13 +59,13 @@ def step_pwl(
         float: Number of output events for each input value
     """
     spikes = (x >= threshold) * np.floor(x / threshold)
-    spikes[spikes > max_spikes_per_dt] = max_spikes_per_dt
+    return np.clip(spikes, 0.0, max_spikes_per_dt)
 
 
 @step_pwl.defjvp
 def step_pwl_jvp(primals, tangents):
-    x, threshold, window = primals
-    (x_dot, threshold_dot, window_dot) = tangents
+    x, threshold, window, max_spikes_per_dt = primals
+    x_dot, threshold_dot, window_dot, max_spikes_per_dt_dot = tangents
     primal_out = step_pwl(*primals)
     tangent_out = (x >= (threshold - window)) * (
         x_dot / threshold - threshold_dot * x / (threshold ** 2)
@@ -200,7 +200,17 @@ class LIFJax(JaxModule):
             tau_syn,
             "taus",
             init_func=lambda s: np.ones(s) * 20e-3,
-            shape=[(self.size_out, self.n_synapses,), (1, self.n_synapses,), (),],
+            shape=[
+                (
+                    self.size_out,
+                    self.n_synapses,
+                ),
+                (
+                    1,
+                    self.n_synapses,
+                ),
+                (),
+            ],
             cast_fn=np.array,
         )
         """ (np.ndarray) Synaptic time constants `(Nout,)` or `()` """
@@ -251,7 +261,9 @@ class LIFJax(JaxModule):
         }
 
     def evolve(
-        self, input_data: np.ndarray, record: bool = False,
+        self,
+        input_data: np.ndarray,
+        record: bool = False,
     ) -> Tuple[np.ndarray, dict, dict]:
         """
 
@@ -267,7 +279,11 @@ class LIFJax(JaxModule):
         input_data, (vmem, spikes, isyn) = self._auto_batch(
             input_data,
             (self.vmem, self.spikes, self.isyn),
-            ((self.size_out,), (self.size_out,), (self.size_out, self.n_synapses),),
+            (
+                (self.size_out,),
+                (self.size_out,),
+                (self.size_out, self.n_synapses),
+            ),
         )
         batches, num_timesteps, _ = input_data.shape
 
@@ -327,7 +343,7 @@ class LIFJax(JaxModule):
             Vmem += Isyn.sum(1) + I_in_t + self.bias
 
             # - Detect next spikes (with custom gradient)
-            spikes = step_pwl(Vmem, self.threshold, None, self.max_spikes_per_dt)
+            spikes = step_pwl(Vmem, self.threshold, 0.5, self.max_spikes_per_dt)
 
             # - Apply subtractive membrane reset
             Vmem = Vmem - spikes * self.threshold

rockpool/nn/modules/native/lif.py
@@ -61,7 +61,7 @@ def spike_subtract_threshold(
     Returns:
         float: Number of output events for each input value
     """
-    return np.clamp((x >= threshold) * np.floor(x / threshold), None, max_spikes_per_dt)
+    return np.clip((x >= threshold) * np.floor(x / threshold), None, max_spikes_per_dt)
 
 
 class LIF(Module):
@@ -175,12 +175,25 @@ class LIF(Module):
             tau_syn,
             "taus",
             init_func=lambda s: np.ones(s) * 20e-3,
-            shape=[(self.size_out, self.n_synapses,), (1, self.n_synapses,), (),],
+            shape=[
+                (
+                    self.size_out,
+                    self.n_synapses,
+                ),
+                (
+                    1,
+                    self.n_synapses,
+                ),
+                (),
+            ],
         )
         """ (np.ndarray) Synaptic time constants `(Nout,)` or `()` """
 
         self.bias: P_ndarray = Parameter(
-            bias, "bias", init_func=lambda s: np.zeros(s), shape=[(self.size_out,), ()],
+            bias,
+            "bias",
+            init_func=lambda s: np.zeros(s),
+            shape=[(self.size_out,), ()],
         )
         """ (np.ndarray) Neuron bias currents `(Nout,)` or `()` """
 
@@ -214,9 +227,10 @@ class LIF(Module):
         self.max_spikes_per_dt: P_int = SimulationParameter(max_spikes_per_dt)
         """ (int) Maximum number of events that can be produced in each time-step """
 
-
     def evolve(
-        self, input_data: np.ndarray, record: bool = False,
+        self,
+        input_data: np.ndarray,
+        record: bool = False,
     ) -> Tuple[np.ndarray, dict, dict]:
         """
 
@@ -231,7 +245,11 @@ class LIF(Module):
         input_data, (vmem, spikes, isyn) = self._auto_batch(
             input_data,
             (self.vmem, self.spikes, self.isyn),
-            ((self.size_out,), (self.size_out,), (self.size_out, self.n_synapses),),
+            (
+                (self.size_out,),
+                (self.size_out,),
+                (self.size_out, self.n_synapses),
+            ),
         )
         batches, num_timesteps, _ = input_data.shape
 

rockpool/nn/modules/torch/exp_syn_torch.py
@@ -85,7 +85,8 @@ class ExpSynTorch(TorchModule):
 
         # - Initialise state
         self.isyn: rt.P_tensor = rp.State(
-            shape=(self.size_out,), init_func=lambda s: torch.zeros(*s),
+            shape=(self.size_out,),
+            init_func=lambda s: torch.zeros(*s),
         )
         """ (torch.tensor) Synaptic current state for each synapse ``(1, N)`` """
 

rockpool/nn/modules/torch/lif_torch.py
@@ -41,9 +41,14 @@ class StepPWL(torch.autograd.Function):
     """
 
     @staticmethod
-    def forward(ctx, x, threshold=1.0, window=0.5, max_spikes_per_dt=torch.inf):
-        ctx.save_for_backward(x.clone())
-        ctx.threshold = threshold
+    def forward(
+        ctx,
+        x,
+        threshold=torch.tensor(1.0),
+        window=torch.tensor(0.5),
+        max_spikes_per_dt=torch.tensor(float("inf")),
+    ):
+        ctx.save_for_backward(x, threshold)
         ctx.window = window
         nr_spikes = ((x >= threshold) * torch.floor(x / threshold)).float()
         nr_spikes[nr_spikes > max_spikes_per_dt] = max_spikes_per_dt
@@ -51,10 +56,10 @@ class StepPWL(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output):
-        (x, threshold, window) = ctx.saved_tensors
+        x, threshold = ctx.saved_tensors
         grad_x = grad_threshold = grad_window = grad_max_spikes_per_dt = None
 
-        mask = x >= (threshold - window)
+        mask = x >= (threshold - ctx.window)
         if ctx.needs_input_grad[0]:
             grad_x = grad_output / threshold * mask
 
@@ -159,7 +164,11 @@ class LIFBaseTorch(TorchModule):
 
         # - Initialise superclass
         super().__init__(
-            shape=shape, spiking_input=True, spiking_output=True, *args, **kwargs,
+            shape=shape,
+            spiking_input=True,
+            spiking_output=True,
+            *args,
+            **kwargs,
         )
 
         self.n_neurons = self.size_out
@@ -202,7 +211,17 @@ class LIFBaseTorch(TorchModule):
         self.tau_syn: P_tensor = rp.Parameter(
             tau_syn,
             family="taus",
-            shape=[(self.size_out, self.n_synapses,), (1, self.n_synapses,), (),],
+            shape=[
+                (
+                    self.size_out,
+                    self.n_synapses,
+                ),
+                (
+                    1,
+                    self.n_synapses,
+                ),
+                (),
+            ],
             init_func=lambda s: torch.ones(s) * 20e-3,
             cast_fn=to_float_tensor,
         )
@@ -227,7 +246,8 @@ class LIFBaseTorch(TorchModule):
         """ (Tensor) Firing threshold for each neuron `(Nout,)` """
 
         self.learning_window: P_tensor = rp.SimulationParameter(
-            learning_window, cast_fn=to_float_tensor,
+            learning_window,
+            cast_fn=to_float_tensor,
         )
         """ (float) Learning window cutoff for surrogate gradient function """
 
@@ -387,7 +407,11 @@ class LIFTorch(LIFBaseTorch):
         data, (vmem, spikes, isyn) = self._auto_batch(
             data,
             (self.vmem, self.spikes, self.isyn),
-            ((self.size_out,), (self.size_out,), (self.size_out, self.n_synapses),),
+            (
+                (self.size_out,),
+                (self.size_out,),
+                (self.size_out, self.n_synapses),
+            ),
         )
         n_batches, time_steps, _ = data.shape
 