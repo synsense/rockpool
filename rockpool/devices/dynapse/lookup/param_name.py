707b796132ac4abe42d7767a98e318ccb247bf6f
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-05-02 14:44:02 +0300

    Reciprocal se1 se2 parameter names updated

--- rockpool/devices/dynapse/lookup/param_name.py
+++ rockpool/devices/dynapse/lookup/param_name.py
@@ -13,28 +13,29 @@ E-mail : ugurcan.cakal@gmail.com
 
 
 table = {
-    "Itau_ahp": ["Itau_ahp", "IF_AHTAU_N", None],
-    "Ith_ahp": ["Ith_ahp", "IF_AHTHR_N", None],
-    "Iw_ahp": ["Iw_ahp", "IF_AHW_P", None],
-    "Idc": ["Idc", "IF_DC_P", None],
+    "Itau_ahp": ["Itau_ahp", "IF_AHTAU_N", "SOAD_TAU_P"],
+    "Ith_ahp": ["Ith_ahp", "IF_AHTHR_N", "SOAD_GAIN_P"],
+    "Iw_ahp": ["Iw_ahp", "IF_AHW_P", "SOAD_W_N"],
+    "Idc": ["Idc", "IF_DC_P", "SOIF_DC_P"],
     "If_nmda": ["If_nmda", "IF_NMDA_N", None],
-    "Iref": ["Iref", "IF_RFR_N", None],
-    "Itau_mem": ["Itau_mem", "IF_TAU1_N", None],
+    "Iref": ["Iref", "IF_RFR_N", "SOIF_REFR_N"],
+    "Itau_mem": ["Itau_mem", "IF_TAU1_N", "SOIF_LEAK_N"],
     "Itau2_mem": ["Itau2_mem", "IF_TAU2_N", None],
-    "Ith_mem": ["Ith_mem", "IF_THR_N", None],
-    "Itau_ampa": ["Itau_ampa", "NPDPIE_TAU_F_P", None],
-    "Itau_nmda": ["Itau_nmda", "NPDPIE_TAU_S_P", None],
-    "Ith_ampa": ["Ith_ampa", "NPDPIE_THR_F_P", None],
-    "Ith_nmda": ["Ith_nmda", "NPDPIE_THR_S_P", None],
-    "Itau_gaba_a": ["Itau_gaba_a", "NPDPII_TAU_F_P", None],
-    "Itau_gaba_b": ["Itau_gaba_b", "NPDPII_TAU_S_P", None],
-    "Ith_gaba_a": ["Ith_gaba_a", "NPDPII_THR_F_P", None],
-    "Ith_gaba_b": ["Ith_gaba_b", "NPDPII_THR_S_P", None],
+    "Ith_mem": ["Ith_mem", "IF_THR_N", "SOIF_GAIN_N"],
+    "Itau_ampa": ["Itau_ampa", "NPDPIE_TAU_F_P", "DEAM_ETAU_P"],
+    "Itau_nmda": ["Itau_nmda", "NPDPIE_TAU_S_P", "DENM_ETAU_P"],
+    "Ith_ampa": ["Ith_ampa", "NPDPIE_THR_F_P", "DEAM_EGAIN_P"],
+    "Ith_nmda": ["Ith_nmda", "NPDPIE_THR_S_P", "DENM_EGAIN_P"],
+    "Itau_gaba_a": ["Itau_gaba_a", "NPDPII_TAU_F_P", "DESC_ITAU_P"],
+    "Itau_gaba_b": ["Itau_gaba_b", "NPDPII_TAU_S_P", "DEGA_ITAU_P"],
+    "Ith_gaba_a": ["Ith_gaba_a", "NPDPII_THR_F_P", "DESC_IGAIN_P"],
+    "Ith_gaba_b": ["Ith_gaba_b", "NPDPII_THR_S_P", "DEGA_IGAIN_P"],
     "Iw_3": ["Iw_3", "PS_WEIGHT_EXC_F_N", "SYAM_W3_P"],  # AMPA SE1
     "Iw_2": ["Iw_2", "PS_WEIGHT_EXC_S_N", "SYAM_W2_P"],  # NMDA SE1
     "Iw_1": ["Iw_1", "PS_WEIGHT_INH_F_N", "SYAM_W1_P"],  # GABA_A SE1
     "Iw_0": ["Iw_0", "PS_WEIGHT_INH_S_N", "SYAM_W0_P"],  # GABA_B SE1
-    "Ipulse": ["Ipulse", "PULSE_PWLK_P", None],
+    "Ipulse": ["Ipulse", "PULSE_PWLK_P", "SYPD_EXT_N"],
+    "Ispkthr": ["Ispkthr", None, "SOIF_SPKTHR_P"],
 }
 
 # SE1 device to simulation map
@@ -49,3 +50,6 @@ se1 = dict(sorted(se1.items(), key=lambda x: x[0].upper()))
 
 # SE2 device to simulation map
 se2 = {v[2]: k for k, v in table.items() if v[2] is not None}
+
+if __name__ == "__main__":
+    print(se2)
