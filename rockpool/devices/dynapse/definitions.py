ae0f730c6c564e74284d841fd0f2acc94bee18ef
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-05-31 07:13:41 +0300

    base renamed as definitions

---
+++ rockpool/devices/dynapse/definitions.py
@@ -0,0 +1,58 @@
+"""
+Dynap-SE1 common definitions and methods
+
+Project Owner : Dylan Muir, SynSense AG
+Author : Ugurcan Cakal
+E-mail : ugurcan.cakal@gmail.com
+22/12/2021
+"""
+
+from typing import Dict, List, Tuple, Union
+import numpy as np
+
+from jax import numpy as jnp
+
+
+ArrayLike = Union[np.ndarray, List, Tuple]
+Numeric = Union[int, float, complex, np.number]
+NeuronKey = Tuple[np.uint8, np.uint8, np.uint16]
+CoreKey = Tuple[np.uint8, np.uint8]
+NeuronConnection = Tuple[np.uint16, np.uint16]
+NeuronConnectionSynType = Tuple[np.uint16, np.uint16, np.uint8]
+
+DynapSERecord = Tuple[
+    jnp.DeviceArray,  # iahp
+    jnp.DeviceArray,  # iampa
+    jnp.DeviceArray,  # igaba
+    jnp.DeviceArray,  # imem
+    jnp.DeviceArray,  # inmda
+    jnp.DeviceArray,  # ishunt
+    jnp.DeviceArray,  # spikes
+    jnp.DeviceArray,  # vmem
+]
+
+DynapSEState = Tuple[
+    jnp.DeviceArray,  # iahp
+    jnp.DeviceArray,  # iampa
+    jnp.DeviceArray,  # igaba
+    jnp.DeviceArray,  # imem
+    jnp.DeviceArray,  # inmda
+    jnp.DeviceArray,  # ishunt
+    jnp.DeviceArray,  # rng_key
+    jnp.DeviceArray,  # spikes
+    jnp.DeviceArray,  # timer_ref
+    jnp.DeviceArray,  # vmem
+]
+
+WeightRecord = Tuple[
+    jnp.DeviceArray,  # loss
+    jnp.DeviceArray,  # w_en
+    jnp.DeviceArray,  # w_dec
+]
+
+NUM_CHIPS = 4
+NUM_CORES = 4
+NUM_NEURONS = 256
+NUM_SYNAPSES = 64
+NUM_DESTINATION_TAGS = 4
+NUM_POISSON_SOURCES = 1024
