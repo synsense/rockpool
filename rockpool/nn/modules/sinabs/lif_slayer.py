* Modified torch layers to use `expand` rather than `broadcast_to` * Fixed bug in aLIFTorch where scalar synapses would fail `as_graph` method

rockpool/nn/modules/sinabs/lif_slayer.py
@@ -105,9 +105,10 @@ class LIFSlayer(LIFBaseTorch):
             n_batches * self.n_neurons, self.n_synapses, time_steps
         ).to(data.device)
 
-        beta = torch.broadcast_to(self.beta, (self.n_neurons, self.n_synapses))
-        threshold = torch.broadcast_to(self.threshold, (self.n_neurons,))
-        alpha = torch.broadcast_to(self.alpha, (self.n_neurons,))
+        # - Broadcast parameters to full size for this module
+        beta = self.beta.expand((self.n_neurons, self.n_synapses))
+        threshold = self.threshold.expand((self.n_neurons,))
+        alpha = self.alpha.expand((self.n_neurons,))
 
         for syn in range(self.n_synapses):
             # bring data into format expected by slayer

rockpool/nn/modules/torch/ahp_lif_torch.py
@@ -289,9 +289,10 @@ class aLIFTorch(LIFBaseTorch):
             Returns:
                 np.array: ``tau_syn``
             """
-            tau_syn = self.tau_syn.broadcast_to((self.size_out, self.n_synapses))
-            tau_syn = torch.cat((tau_syn, self.tau_ahp.reshape(self.size_out, 1)), 1)
-            return tau_syn.flatten().detach().numpy()
+            tau_syn = self.tau_syn.expand((self.size_out, self.n_synapses))
+            tau_ahp = self.tau_ahp.reshape((-1, 1)).expand((self.size_out, 1))
+            tau_syn_ahp = torch.cat((tau_syn, tau_ahp), 1)
+            return tau_syn_ahp.flatten().detach().numpy()
 
         def w_ahp_reshape(self):
             """
@@ -308,17 +309,15 @@ class aLIFTorch(LIFBaseTorch):
             return w_ahp
 
         # - Get tau_mem for export
-        tau_mem = self.tau_mem.broadcast_to((self.size_out,)).flatten().detach().numpy()
+        tau_mem = self.tau_mem.expand((self.size_out,)).flatten().detach().numpy()
 
         # - Get tau_syn and w_ahp for export
         tau_syn_ahp = syn_integration(self)
         w_ahp = w_ahp_reshape(self)
 
         # - Get threshold and bias parameters for export
-        threshold = (
-            self.threshold.broadcast_to((self.size_out,)).flatten().detach().numpy()
-        )
-        bias = self.bias.broadcast_to((self.size_out,)).flatten().detach().numpy()
+        threshold = self.threshold.expand((self.size_out,)).flatten().detach().numpy()
+        bias = self.bias.expand((self.size_out,)).flatten().detach().numpy()
 
         # - Generate a GraphModule for the neurons
         neurons = LIFNeuronWithSynsRealValue._factory(

rockpool/nn/modules/torch/torch_module.py
@@ -216,9 +216,7 @@ class TorchModule(Module, nn.Module):
             data = torch.unsqueeze(data, 0)
 
         if data.shape[-1] == 1:
-            data = torch.broadcast_to(
-                data, (data.shape[0], data.shape[1], self.size_in)
-            )
+            data = data.expand((data.shape[0], data.shape[1], self.size_in))
 
         # - Get shape of input
         (n_batches, time_steps, n_connections) = data.shape
@@ -458,7 +456,7 @@ class TorchModule(Module, nn.Module):
 
         obj.__class__ = TorchModulePatch
         obj.__old_class_name = old_class_name
-        
+
         obj._has_torch_api = retain_torch_api
 
         assert isinstance(obj, TorchModule)