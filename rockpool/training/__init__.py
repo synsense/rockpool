eaf3a2856283622d25a75fe905fc319ec014c697
Dylan Richard Muir <dylan.muir@synsense.ai>
2022-07-12 14:39:35 +0200

    Fixed error with import `__init__.py`

--- rockpool/training/__init__.py
+++ rockpool/training/__init__.py
@@ -1,14 +1,3 @@
 """
 Contains packages for assisting with NN training
-# """
-
-from .torch_loss import *
-
-# try:
-#     from .torch_loss import *
-# except:
-#     from rockpool.utilities.backend_management import (
-#         backend_available,
-#         missing_backend_shim,
-#     )
-
+"""
