1201868fa86d008c537bce2bd933b14161eddb47
Dylan Richard Muir <dylan.muir@synsense.ai>
2022-08-17 17:48:27 +0200

    Added some inline docs

--- rockpool/transform/torch_transform.py
+++ rockpool/transform/torch_transform.py
@@ -36,7 +36,7 @@ __all__ = [
 
 def make_backward_passthrough(function: Callable) -> Callable:
     """
-    Wrap a function to pass the gradient directly through in teh backward pass
+    Wrap a function to pass the gradient directly through in the backward pass
 
     Args:
         function (Callable): A function to wrap
@@ -46,6 +46,7 @@ def make_backward_passthrough(function: Callable) -> Callable:
     """
 
     class Wrapper(torch.autograd.Function):
+        """A torch.autograd.Function that wraps a function with a pass-through gradient."""
         @staticmethod
         def forward(self, x):
             self.save_for_backward(x)
@@ -71,6 +72,59 @@ def stochastic_rounding(
     num_levels: int = 2**8,
     maintain_zero: bool = True,
 ):
+    """
+    Perform floating-point stochastic rounding on a tensor, with detailed control over quantisation levels
+    
+    Stochastic rounding randomly pushes values up or down probabilistically, depending on the original value.
+    Values will round with greater probability to their nearest quantised level, and with lower probability to their next-nearest quantised level.
+    
+    For example, if we are rounding to integers, then a value of 0.1 will round down to 0.0 with 90% probability; it will round to 1.0 with 10% probability.
+    
+    If we are rounding to arbitrary floating point levels, then the same logic holds, but the quantised output values will not be round numbers.
+
+    :py:func:`stochastic_rounding` permits the input space to be re-scaled during rounding to an output space, which will be quantised over a specified number of quantisation levels (``2**8`` by default).
+    By default, the input and output space are defined to be the full input range from minimum to maximum.
+    
+    :py:func:`stochastic_rounding` permits careful handling of symmetric or asymmetric spaces.
+    By default, values of zero in the input space will map to zero in the output space (i.e. ``maintain_zero = True``).
+    In this case the output range is defined as ``max(abs(input_range)) * [-1, 1]``.
+    
+    Quantisation and rounding is always to equally-spaced levels.
+    
+    Examples
+    
+        >>> stochastic_rounding(torch.tensor([-1., -0.5, 0., 0.5, 1.]), num_levels = 3)
+        tensor([-1.,  0.,  0.,  1.,  1.])
+        
+        >>> stochastic_rounding(torch.tensor([-1., -0.5, 0., 0.5, 1.]), num_levels = 3)
+        tensor([-1., -1.,  0.,  0.,  1.])
+        
+        Quantise to round integers over a defined space, changing the input scale (0..1) to (0..10).
+        
+        >>> stochastic_rounding(torch.rand(10), input_range = [0., 1.], output_range = [0., 10.], num_levels = 11)
+        tensor([1., 9., 2., 2., 7., 1., 7., 9., 6., 1.])
+        
+        Quantise to floating point levels, without changing the scale of the values.
+        >>> stochastic_rounding(torch.rand(10)-.5, num_levels = 3)
+        tensor([ 0.0000,  0.0000,  0.0000, -0.4701, -0.4701,  0.4701, -0.4701, -0.4701,
+         0.0000,  0.4701])
+         
+        >>> stochastic_rounding(torch.rand(10)-.5, num_levels = 3)
+        tensor([ 0.0000,  0.0000, -0.4316,  0.0000,  0.0000,  0.4316, -0.4316,  0.0000,
+         0.4316,  0.4316])
+         
+         Note that the scale is defined by the observed range of the random values, in this case.
+    
+    Args:
+        value (torch.Tensor): A Tensor of values to round 
+        input_range (Optional[List]): If defined, a specific input range to use (``[min_value, max_value]``), as floating point numbers. If ``None`` (default), use the range of input values to define the input range.
+        output_range (Optional[List]): If defined, a specific output range to use (``[min_value, max_value]``), as floating point numbers. If ``None`` (default), use the range of input values to define the input range.
+        num_levels (int): The number of output levels to quantise to (Default: ``2**8``)  
+        maintain_zero (bool): Iff ``True``, ensure that input values of zero map to zero in the output space (Default: ``True``). If ``False``, the output range may shift zero w.r.t. the input range.
+
+    Returns:
+        torch.Tensor: Floating point stochastically rounded values
+    """
     if maintain_zero:
         # - By default, input range is whatever the current data range is
         max_range = torch.max(torch.abs(value))
@@ -110,10 +164,24 @@ def stochastic_rounding(
 
 def stochastic_channel_rounding(
     value: Tensor,
-    output_range: Optional[List] = None,
+    output_range: List[float],
     num_levels: int = 2**8,
     maintain_zero: bool = True,
 ):
+    """
+    Perform stochastic rounding of a matrix, but with the input range defined automatically for each column independently
+    
+    This function performs the same quantisation approach as :py:func:`stochastic_rounding`, but considering each column of a matrix independently. ie. per-channel.
+    
+    Args:
+        value (torch.Tensor): A tensor of values to quantise 
+        output_range (List[float]): Defines the destination quantisation space ``[min_value, max_value]``. 
+        num_levels (int): The number of quantisation values to round to (Default: ``2**8``) 
+        maintain_zero (bool): Iff ``True`` (default), input values of zero map to zero in the output space. If ``False``, the output space may shift w.r.t. the input space. Note that the output space must be symmetric for the zero mapping to work as expected.
+
+    Returns:
+        torch.Tensor: The rounded values
+    """
     def round_vector(vector: Tensor):
         if maintain_zero:
             max_range = torch.max(torch.abs(vector))
@@ -148,6 +216,50 @@ def deterministic_rounding(
     num_levels: int = 2**8,
     maintain_zero: bool = True,
 ):
+    """
+    Quantise values by shifting them to the closest quantisation level
+    
+    This is a floating-point equivalent to standard integer rounding (e.g. using ``torch.round()``).
+    :py:func:`deterministic_rounding` provides fine control over input and output spaces, as well as numbers of levels to quantise to, and can round to floating point levels instead of round numbers.
+    :py:func:`deterministic_rounding` always leaves values as floating point.
+    
+    For example, if we are rounding to integers, then a value of ``0.1`` will round down to ``0.``.
+    A value of ``0.5`` will round up to ``1.``.
+    A value of ``0.9`` will round up to ``1.``.
+    
+    If we are rounding to arbitrary floating point levels, then the same logic holds, but the quantised output values will not be round numbers, but will be the nearest floating point quantisation level.
+
+    :py:func:`deterministic_rounding` permits the input space to be re-scaled during rounding to an output space, which will be quantised over a specified number of quantisation levels (``2**8`` by default).
+    By default, the input and output space are defined to be the full input range from minimum to maximum.
+    
+    :py:func:`deterministic_rounding` permits careful handling of symmetric or asymmetric spaces.
+    By default, values of zero in the input space will map to zero in the output space (i.e. ``maintain_zero = True``).
+    In this case the output range is defined as ``max(abs(input_range)) * [-1, 1]``.
+    
+    Quantisation and rounding is always to equally-spaced levels.
+    
+    Examples
+    
+        >>> deterministic_rounding(torch.tensor([-1., -0.5, 0., 0.5, 1.]), num_levels = 3)
+        tensor([-1.,  -1.,  0.,  1.,  1.])
+        
+        >>> deterministic_rounding(torch.tensor([-1., -0.5, 0., 0.5, 1.]), num_levels = 3)
+        tensor([-1., -1.,  0.,  0.,  1.])
+        
+        Round to integer values (-10..10)
+        
+        >>> deterministic_rounding(torch.rand(10)-.5, output_range=[-10., 10.], num_levels = 21)
+        tensor([ 10.,  -3.,   3., -10.,   5.,   0.,   9.,  -5.,   7.,   8.])
+        
+        value (torch.Tensor): A Tensor of values to round 
+        input_range (Optional[List]): If defined, a specific input range to use (``[min_value, max_value]``), as floating point numbers. If ``None`` (default), use the range of input values to define the input range.
+        output_range (Optional[List]): If defined, a specific output range to use (``[min_value, max_value]``), as floating point numbers. If ``None`` (default), use the range of input values to define the input range.
+        num_levels (int): The number of output levels to quantise to (Default: ``2**8``)  
+        maintain_zero (bool): Iff ``True``, ensure that input values of zero map to zero in the output space (Default: ``True``). If ``False``, the output range may shift zero w.r.t. the input range.
+
+    Returns:
+        torch.Tensor: Floating point rounded values
+    """
     if maintain_zero:
         # - By default, input range is whatever the current data range is
         max_range = torch.max(torch.abs(value))
@@ -181,14 +293,50 @@ def deterministic_rounding(
 
 
 def dropout(param: Tensor, dropout_prob: float = 0.5):
+    """
+    Randomly set values of a tensor to ``0.``, with a defined probability
+    
+    Dropout is used to improve the robustness of a network, by reducing the dependency of a network on any given parameter value.
+    This is accomplished by randomly setting parameters (usually weights) to zero during training, such that the parameters are ignored.
+    
+    For a ``dropout_prob = 0.8``, each parameters is randomly set to zero with 80\% probability.
+    
+    Examples
+    
+        >>> dropout(torch.ones(10))
+        tensor([0., 0., 0., 1., 0., 1., 1., 1., 1., 0.])
+    
+        >>> dropout(torch.ones(10), dropout_prob = 0.8)
+        tensor([1., 0., 0., 0., 0., 0., 0., 1., 0., 0.])
+    
+    Args:
+        param (torch.Tensor): A tensor of values to dropout
+        dropout_prob (float): The probability of zeroing each parameter value (Default: ``0.5``, 50\%)
+
+    Returns:
+        torch.Tensor: The tensor of values 
+    """
     mask = torch.rand(param.shape, device=param.device) > dropout_prob
     return param * mask
 
 
 class TWrapper(TorchModule):
+    """
+    A wrapper for a Rockpool TorchModule, implementing a parameter transformation in the forward pass 
+    """
     def __init__(
         self, mod: TorchModule, T_config: Optional[Tree] = None, *args, **kwargs
     ):
+        """
+        Initialise a parameter transformer wrapper module
+        
+        
+        Args:
+            mod: 
+            T_config: 
+            *args: 
+            **kwargs: 
+        """
         # - Initialise Module superclass
         super().__init__(*args, shape=mod.shape, **kwargs)
 
