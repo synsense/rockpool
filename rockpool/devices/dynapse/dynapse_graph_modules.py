c8ba8237130068afc48ac45796be93f070e8ff8b
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-09-15 16:47:02 +0200

    DynapSE graph modules initiated

---
+++ rockpool/devices/dynapse/dynapse_graph_modules.py
@@ -0,0 +1,23 @@
+"""
+Dynap-SE graph modules implementing conversion and translation methods
+
+Note : Existing modules are reconstructed considering consistency with Xylo support.
+
+Project Owner : Dylan Muir, SynSense AG
+Author : Ugurcan Cakal
+E-mail : ugurcan.cakal@gmail.com
+
+15/09/2022
+"""
+from typing import Optional
+from rockpool.graph import GenericNeurons
+
+from dataclasses import dataclass, field
+
+__all__ = ["XyloNeurons", "XyloHiddenNeurons", "XyloOutputNeurons"]
+
+
+@dataclass(eq=False, repr=False)
+class DynapseNeurons(GenericNeurons):
+    dt: Optional[float] = None
+    """ float: The ``dt`` time step used for this neuron module """
