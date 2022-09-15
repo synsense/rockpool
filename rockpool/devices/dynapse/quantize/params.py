84277b859ce12655546b41831e9e96ab10438a18
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-09-15 17:10:50 +0200

    weight and parameter quantization packages

---
+++ rockpool/devices/dynapse/quantize/params.py
@@ -0,0 +1,11 @@
+"""
+Dynap-SE parameter quantization package provides easy to use support
+
+Note : Existing modules are reconstructed considering consistency with Xylo support.
+
+Project Owner : Dylan Muir, SynSense AG
+Author : Ugurcan Cakal
+E-mail : ugurcan.cakal@gmail.com
+
+15/09/2022
+"""
