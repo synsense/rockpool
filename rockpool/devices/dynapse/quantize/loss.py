cbd3fc0e5244fcb4b8ac903caa003a6428a6ef40
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-09-21 14:22:37 +0200

    merged to quantization/autoencoder.py

--- rockpool/devices/dynapse/quantize/loss.py
+++
@@ -1,9 +0,0 @@
-"""
-Dynap-SE weight quantization custom loss function implementations
-
-Project Owner : Dylan Muir, SynSense AG
-Author : Ugurcan Cakal
-E-mail : ugurcan.cakal@gmail.com
-
-20/09/2022
-"""
