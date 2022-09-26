d07b43fe248b65b2473167ad4ea0681ffd5c23a1
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-09-26 11:09:35 +0200

    file split

---
+++ rockpool/devices/dynapse/interface/config.py
@@ -0,0 +1,34 @@
+"""
+Dynap-SE samna config helper functions
+Handles the low-level hardware configuration under the hood and provide easy-to-use access to the user
+
+Note : Existing modules are reconstructed considering consistency with Xylo support.
+
+Project Owner : Dylan Muir, SynSense AG
+Author : Ugurcan Cakal
+E-mail : ugurcan.cakal@gmail.com
+
+15/09/2022
+"""
+
+from __future__ import annotations
+
+from typing import Any
+
+# - Configure exports
+__all__ = ["config_from_specification", "save_config", "load_config"]
+
+
+def config_from_specification(*args, **kwargs) -> Any:
+    None
+
+    # Bias values -> coarse,fine
+    # Weight matrices -> CAM & SRAM
+
+
+def save_config(*args, **kwargs) -> Any:
+    None
+
+
+def load_config(*args, **kwargs) -> Any:
+    None
