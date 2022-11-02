9e60cd36f7732a7c3b2a8cddddfe796eef94862f
Dylan Richard Muir <dylan.muir@synsense.ai>
2022-11-02 09:04:14 +0100

    Testing HW detection API

--- rockpool/devices/xylo/__init__.py
+++ rockpool/devices/xylo/__init__.py
@@ -9,42 +9,44 @@ See Also:
     Defines the subpackages :py:mod:`.syns61201`, :py:mod:`.syns61300`, :py:mod:`.syns65300`.
 """
 
-from rockpool.utilities.backend_management import (
-    backend_available,
-    missing_backend_shim,
-)
+from .helper import find_xylo_hdks
+
+# from rockpool.utilities.backend_management import (
+#     backend_available,
+#     missing_backend_shim,
+# )
 
 
-try:
-    from .syns61300.xylo_sim import *
-except (ImportError, ModuleNotFoundError) as err:
-    if not backend_available("xylosim", "samna"):
-        XyloSim = missing_backend_shim("XyloSim", "xylosim, samna")
-    else:
-        raise
+# try:
+#     from .syns61300.xylo_sim import *
+# except (ImportError, ModuleNotFoundError) as err:
+#     if not backend_available("xylosim", "samna"):
+#         XyloSim = missing_backend_shim("XyloSim", "xylosim, samna")
+#     else:
+#         raise
 
-try:
-    from .syns61300.xylo_samna import *
-except (ImportError, ModuleNotFoundError) as err:
-    if not backend_available("samna"):
-        XyloSamna = missing_backend_shim("XyloSamna", "samna")
-        config_from_specification = missing_backend_shim(
-            "config_from_specification", "samna"
-        )
-        save_config = missing_backend_shim("save_config", "samna")
-        load_config = missing_backend_shim("load_config", "samna")
-    else:
-        raise
+# try:
+#     from .syns61300.xylo_samna import *
+# except (ImportError, ModuleNotFoundError) as err:
+#     if not backend_available("samna"):
+#         XyloSamna = missing_backend_shim("XyloSamna", "samna")
+#         config_from_specification = missing_backend_shim(
+#             "config_from_specification", "samna"
+#         )
+#         save_config = missing_backend_shim("save_config", "samna")
+#         load_config = missing_backend_shim("load_config", "samna")
+#     else:
+#         raise
 
-from .syns65300.afe_sim import *
+# from .syns65300.afe_sim import *
 
-from .syns61201.afe_samna import *
-from .syns61201.xylo_divisive_normalisation import *
+# from .syns61201.afe_samna import *
+# from .syns61201.xylo_divisive_normalisation import *
 
-from .syns61300.xylo_graph_modules import *
-from .syns61300.xylo_mapper import *
+# from .syns61300.xylo_graph_modules import *
+# from .syns61300.xylo_mapper import *
 
 
-import rockpool.devices.xylo.syns61300 as pollen
-import rockpool.devices.xylo.syns65300 as vA1
-import rockpool.devices.xylo.syns61201 as vA2
+# import rockpool.devices.xylo.syns61300 as pollen
+# import rockpool.devices.xylo.syns65300 as vA1
+# import rockpool.devices.xylo.syns61201 as vA2
