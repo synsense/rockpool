df6bc7f6d59035d9e4c66e80cc4700bf067be3a6
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-11-18 10:53:43 +0100

    DEF default core-map

--- rockpool/devices/dynapse/default.py
+++ rockpool/devices/dynapse/default.py
@@ -9,7 +9,7 @@ E-mail : ugurcan.cakal@gmail.com
 [] TODO : merge with definitions
 """
 
-from rockpool.devices.dynapse.definitions import NUM_CORES, NUM_CHIPS
+from rockpool.devices.dynapse.definitions import NUM_NEURONS, NUM_CORES, NUM_CHIPS
 
 Itau_lambda = (
     lambda name: (
@@ -90,5 +90,6 @@ dcurrents = {
     "Iw_ahp": 1e-9,
 }
 
+CORE_MAP = {i: i // NUM_NEURONS for i in range(NUM_NEURONS * NUM_CORES * NUM_CHIPS)}
 CHIP_MAP = {i: i // NUM_CORES for i in range(-NUM_CORES, NUM_CORES * NUM_CHIPS)}
 CHIP_POS = {-1: (0, 0), 0: (1, 0), 1: (2, 0), 3: (3, 0), 4: (4, 0)}
