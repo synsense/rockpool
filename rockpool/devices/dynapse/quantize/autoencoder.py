ef0f050f33b3c5effb11d4e7fd1f9d79d2407ec6
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-11-23 17:05:59 +0100

    AUTOENC : enable multi cluster autoencoding

--- rockpool/devices/dynapse/quantize/autoencoder.py
+++ rockpool/devices/dynapse/quantize/autoencoder.py
@@ -12,7 +12,7 @@ Previous : config/autoencoder.py @ 220127
 """
 
 from __future__ import annotations
-from typing import Any, Callable, Dict, Optional, Tuple, Union
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 import numpy as np
 from dataclasses import dataclass
@@ -41,6 +41,70 @@ __all__ = ["autoencoder_quantization"]
 
 
 def autoencoder_quantization(
+    ## cluster
+    n_cluster: int,
+    core_map: List[int],
+    ## Input
+    weights_in: np.ndarray,
+    weights_rec: np.ndarray,
+    Iscale: float,
+    n_bits: Optional[int] = 4,
+    ## Optimization
+    fixed_epoch: bool = False,
+    num_epoch: int = int(1e7),
+    num_epoch_checkpoint: int = int(1e3),
+    eps: float = 1e-6,
+    record_loss: bool = True,
+    optimizer: str = "adam",
+    step_size: Union[float, Callable[[int], float]] = 1e-4,
+    opt_params: Optional[Dict[str, Any]] = {},
+    *args,
+    **kwargs,
+) -> Dict[str, Union[np.ndarray, float]]:
+
+    spec = {
+        "weights_in": [],
+        "sign_in": [],
+        "weights_rec": [],
+        "sign_rec": [],
+        "Iw_0": [],
+        "Iw_1": [],
+        "Iw_2": [],
+        "Iw_3": [],
+        "quantization_loss": [],
+    }
+    n_neuron = weights_rec.shape[1]
+
+    for n in range(n_cluster):
+
+        w_in = np.zeros_like(weights_in)
+        w_rec = np.zeros_like(weights_rec)
+
+        w_in[:, core_map == n] = weights_in[:, core_map == n]
+        w_rec[:, core_map == n] = weights_rec[:, core_map == n]
+
+        __temp = single_autoencoder(
+            w_in,
+            w_rec,
+            Iscale[n],
+            n_bits,
+            fixed_epoch,
+            num_epoch,
+            num_epoch_checkpoint,
+            eps,
+            record_loss,
+            optimizer,
+            step_size,
+            opt_params,
+        )
+
+        for key in spec:
+            spec[key].append(__temp[key])
+
+    return spec
+
+
+def single_autoencoder(
     ## Input
     weights_in: np.ndarray,
     weights_rec: np.ndarray,
