79e62134e8cb8a7cdc352909913b9f8a98042b62
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-11-25 13:50:32 +0100

    REF old utils removed, backup in dropbox

--- rockpool/devices/dynapse/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-DynapSE-family device simulation supporting utilities
-
-Supporting operations:
-
-* Investigating the imported simulation configuration
-* Producing informative figures using the simulation module outputs
-* Creating random or custom defined spike trains
-"""
