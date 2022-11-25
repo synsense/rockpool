6f8491f54dad0d1c82d1efdb9e3db67e6dfff133
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-11-25 17:03:12 +0100

    lookup table imports

--- rockpool/devices/dynapse/lookup/__init__.py
+++ rockpool/devices/dynapse/lookup/__init__.py
@@ -1,5 +1,10 @@
 """
-    Lookup tables obtained from 
-    https://hardware.basket.office.synsense.ai/documentation/dynapse2docs/Sections/input_interface_config.html
-    for more accurate simulation of DynapSE-2 bias generator
+Lookup tables obtained from 
+https://hardware.basket.office.synsense.ai/documentation/dynapse2docs/Sections/input_interface_config.html
+for more accurate simulation of DynapSE-2 bias generator
 """
+
+from .parameter_name import *
+from .paramgen import *
+from .scaling import *
+from .transistor import *
