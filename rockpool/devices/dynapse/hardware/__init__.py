90f1dbdcec8fcc449b9dea421ce39821bf319438
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-11-28 16:41:17 +0100

    hardware init explanation

--- rockpool/devices/dynapse/hardware/__init__.py
+++ rockpool/devices/dynapse/hardware/__init__.py
@@ -1,2 +1,12 @@
+"""
+DynapSE-family hardware configuration package
+
+Supporting operations:
+
+* Creating a custom simulation object
+* Importing a simulation configuration from a device configuration
+* Exporting to a device configuration from a simulation configuration
+"""
+
 from .config import config_from_specification
 from .interface import DynapseSamna, find_dynapse_boards
