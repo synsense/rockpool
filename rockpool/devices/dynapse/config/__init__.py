9b6e4771c08403c1085e8d59f07e58bd1dfae0ba
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-11-28 15:12:51 +0100

    all config module relocated

--- rockpool/devices/dynapse/config/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-DynapSE-family simulation configuration package
-
-Supporting operations:
-
-* Creating a custom simulation object
-* Importing a simulation configuration from a device configuration
-* Exporting to a device configuration from a simulation configuration
-"""
