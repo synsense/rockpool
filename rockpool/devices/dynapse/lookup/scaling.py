b5d8382eed25f695e87c20a2aa5025f92f5479ca
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-12-07 12:23:27 +0100

    we do not need se1 parameter scaling

--- rockpool/devices/dynapse/lookup/scaling.py
+++ rockpool/devices/dynapse/lookup/scaling.py
@@ -1,8 +1,5 @@
 """
-Both Dynap-SE1 and Dynap-SE2 scalig factor tables
-
-Dynap-SE1 circuit bias generator transistor current response scaling factors
-Neither experimental nor theoretical. Waiting for tests to be verified!
+Both Dynap-SE2 scalig factor tables
 
 Dynap-SE2 circuit bias generator transistor current response scaling factors
 Multiply the theoretical value obtained with the table values considering the parameter name
@@ -10,43 +7,13 @@ Obtained from
 https://hardware.basket.office.synsense.ai/documentation/dynapse2docs/Sections/input_interface_config.html
 for more accurate simulation of DynapSE-2 bias generator
 
-merged : scaling_factor_se1.py -> scaling_factor.py @ 220529
-
 Project Owner : Dylan Muir, SynSense AG
 Author : Ugurcan Cakal
 E-mail : ugurcan.cakal@gmail.com
 14/01/2022
 """
 
-__all__ = ["scale_factor_se1", "scale_factor_se2"]
-
-scale_factor_se1 = {
-    "IF_AHTAU_N": 1.0,
-    "IF_AHTHR_N": 1.0,
-    "IF_AHW_P": 1.0,
-    "IF_BUF_P": 1.0,
-    "IF_CASC_N": 1.0,
-    "IF_DC_P": 1.0,
-    "IF_NMDA_N": 1.0,
-    "IF_RFR_N": 1.0,
-    "IF_TAU1_N": 1e-4,
-    "IF_TAU2_N": 1.0,
-    "IF_THR_N": 1.0,
-    "NPDPIE_TAU_F_P": 1.0,
-    "NPDPIE_TAU_S_P": 1.0,
-    "NPDPIE_THR_F_P": 1.0,
-    "NPDPIE_THR_S_P": 1.0,
-    "NPDPII_TAU_F_P": 1.0,
-    "NPDPII_TAU_S_P": 1.0,
-    "NPDPII_THR_F_P": 1.0,
-    "NPDPII_THR_S_P": 1.0,
-    "PS_WEIGHT_EXC_F_N": 1.0,
-    "PS_WEIGHT_EXC_S_N": 1.0,
-    "PS_WEIGHT_INH_F_N": 1.0,
-    "PS_WEIGHT_INH_S_N": 1.0,
-    "PULSE_PWLK_P": 1.0,
-    "R2R_P": 1.0,
-}
+__all__ = ["scale_factor_se2"]
 
 scale_factor_se2 = {
     "LBWR_VB_P": 0.57,
