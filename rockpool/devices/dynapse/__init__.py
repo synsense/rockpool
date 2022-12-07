fccbef656ded83285f58944e2a4ef2ee7b4c5cc0
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-12-07 12:21:56 +0100

    remove SE1 name from header

--- rockpool/devices/dynapse/__init__.py
+++ rockpool/devices/dynapse/__init__.py
@@ -1,13 +1,13 @@
 """
 DynapSE-family device simulations, deployment and HDK support
 
-JAX-backend Dynap-SE1/SE2 simulator support which allows people to 
+JAX-backend Dynap-SE2 simulator support which allows people to 
 
-* Configure their own networks either custom configuration or importing from existing device configuration 
-* Run a simulation with any spiking input
-* Observe the current changes through time
-* Optimize the circuit level bias parameters using standard gradient-based optimization techniques
-* Export the simulation configuration in the form of a device configuraiton for deployment
+    * Configure their own networks either custom configuration or importing from existing device configuration 
+    * Run a simulation with any spiking input
+    * Observe the current changes through time
+    * Optimize the circuit level bias parameters using standard gradient-based optimization techniques
+    * Export the simulation configuration in the form of a device configuraiton for deployment
 
 """
 
