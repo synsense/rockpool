9a6585423d12c8841d9ab43d82bfee92e924c7d1
Felix Bauer <felix.bauer@aictx.ai>
2022-12-13 11:57:16 +0100

    Correctly test input gradients of lif models. Streamline lif tests

--- tests/tests_default/test_lif_sinabs.py
+++ tests/tests_default/test_lif_sinabs.py
@@ -56,8 +56,8 @@ pytest.importorskip("torch")
 pytest.importorskip("sinabs")
 pytest.importorskip("sinabs.exodus")
 
-
-def test_FF_equality_exodus():
+@pytest.mark.parametrize("n_synapses", (1, 2))
+def test_FF_equality_exodus(n_synapses):
     import torch
     import numpy as np
 
@@ -72,7 +72,7 @@ def test_FF_equality_exodus():
     tau_mem = 0.01
     tau_syn = 0.05
     threshold = 1.0
-    bias = 0.3
+    bias = torch.arange(n_neurons) * 0.05
 
     # - init LIFTorch
     from rockpool.nn.modules.torch.lif_torch import LIFTorch
@@ -104,12 +104,13 @@ def test_FF_equality_exodus():
     ).to("cuda")
 
     # - Generate some data
-    input_data_torch = (
-        torch.rand(n_batches, T, n_synapses * n_neurons, requires_grad=True).cuda()
+    input_data = (
+        torch.rand(n_batches, T, n_synapses * n_neurons).cuda()
         * 0.01
     )
     # Separate (but equal) input tensor for exodus, to compare gradients
-    input_data_sinabs = input_data_torch.clone().detach()
+    input_data_torch = input_data.clone()
+    input_data_sinabs = input_data.clone()
     input_data_torch.requires_grad_(True)
     input_data_sinabs.requires_grad_(True)
 
@@ -143,82 +144,6 @@ def test_FF_equality_exodus():
 
     assert torch.allclose(lif_torch.bias.grad, lif_sinabs.bias.grad)
     assert torch.allclose(input_data_torch.grad, input_data_sinabs.grad)
-
-
-def test_FF_multisyn_equality_exodus():
-    import torch
-    import numpy as np
-
-    from rockpool.parameters import Constant
-
-    if not torch.cuda.is_available():
-        pytest.skip("CUDA is required for Exodus tests")
-
-    # - parameter
-    n_synapses = 2
-    n_neurons = 10
-    n_batches = 3
-    T = 20
-    tau_mem = 0.01
-    tau_syn = torch.Tensor([[0.02, 0.03]]).repeat(n_neurons, 1)
-    threshold = 0.1
-
-    # - init LIFTorch
-    from rockpool.nn.modules.torch.lif_torch import LIFTorch
-
-    lif_torch = LIFTorch(
-        shape=(n_synapses * n_neurons, n_neurons),
-        tau_mem=tau_mem,
-        tau_syn=tau_syn,
-        bias=Constant(0.0),
-        has_rec=False,
-        dt=1e-3,
-        noise_std=0.0,
-        threshold=threshold,
-    ).to("cuda")
-
-    # - init LIFSlayer
-    from rockpool.nn.modules.sinabs.lif_exodus import LIFExodus
-
-    lif_sinabs = LIFExodus(
-        shape=(n_synapses * n_neurons, n_neurons),
-        tau_mem=tau_mem,
-        tau_syn=tau_syn,
-        has_rec=False,
-        dt=1e-3,
-        noise_std=0.0,
-        threshold=threshold,
-    ).to("cuda")
-
-    # - Generate some data
-    input_data = (
-        torch.rand(n_batches, T, n_synapses * n_neurons, requires_grad=True).cuda()
-        * 0.1
-    )
-
-    # - run LIFTorch and LIFSlayer
-    out_torch, ns_torch, rd_torch = lif_torch(input_data, record=True)
-    out_sinabs, ns_sinabs, rd_sinabs = lif_sinabs(input_data, record=True)
-
-    assert np.allclose(out_torch.detach().cpu(), out_sinabs.detach().cpu())
-
-    for key in ns_sinabs.keys():
-        if not ns_torch[key] == None and not ns_sinabs[key] == None:
-            assert np.allclose(
-                ns_torch[key].detach().cpu(),
-                ns_sinabs[key].detach().cpu(),
-                atol=1e-5,
-                rtol=1e-5,
-            )
-
-    for key in rd_sinabs.keys():
-        if not rd_torch[key] == None and not rd_sinabs[key] == None:
-            assert np.allclose(
-                rd_torch[key].detach().cpu(),
-                rd_sinabs[key].detach().cpu(),
-                atol=1e-5,
-                rtol=1e-5,
-            )
 
 
 def test_lif_slayer():
