2a0d1078bd60965e02622e803389d8fc813d7b0c
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-12-21 13:30:08 +0300

    top level init file header

--- rockpool/devices/dynapse/__init__.py
+++ rockpool/devices/dynapse/__init__.py
@@ -1,17 +1,51 @@
 """
-DynapSE-family device simulations, deployment and HDK support
+Dynap-SE2 Application Programming Interface (API)
 
-JAX-backend Dynap-SE2 simulator support which allows people to 
+This package provides an abstract Dynap-SE2 machine that operates in the same parameter space as Dynap-SE2 processors. 
+More, all the required tools to convert a simulation setting to an hardware configuration and a hardware configuration to a simulation setting.
 
-    * Configure their own networks either custom configuration or importing from existing device configuration 
-    * Run a simulation with any spiking input
-    * Observe the current changes through time
-    * Optimize the circuit level bias parameters using standard gradient-based optimization techniques
-    * Export the simulation configuration in the form of a device configuraiton for deployment
+JAX-backend Dynap-SE2 simulator named `DynapSim` does not simulate the hardware numerically precisely but executes a fast and an approximate simulation.
+It uses forward Euler updates to predict the time-dependent dynamics and solves the characteristic circuit transfer functions in time.
+In principle, the device and the simulator do not react exactly the same to the same input.
+Since two physical chips would not react the same as well, it should not create a problem in application development.
+The networks to be deployed to a chip should be robust against parameter variations.
+To stress this, `DynapSim` provides a mismatch simulation feature that can alter the parameter projection.
 
+A user can 
+
+* define a rockpool network 
+* map this network to a hardware specification
+* quantize the specified paramters
+* obtain a samna configuration
+* connect and configure a Dynap-SE2 chip 
+* run a real-time, hardware simulation
+
+```python
+net = Sequential(
+    LinearJax(shape=(Nin, Nrec), has_bias=False),
+    DynapSim((Nrec, Nrec), has_rec=True, percent_mismatch=0.05),
+)
+
+spec = mapper(net.as_graph())
+spec.update(autoencoder_quantization(**spec))
+config = config_from_specification(**spec)
+
+# Connect to device
+se2_devices = find_dynapse_boards()
+se2 = DynapseSamna(se2_devices[0], **config)
+out, state, rec = se2(raster, record=True)
+```
+
+Or 
+* resolve a samna configuration to obtain a rockpool network
+
+```python
+model : JaxModule = dynapsim_net_from_config(**config)
+out, state, rec = model(raster, record=True)
+```
+
+Please check the tutorials provided in :ref:`/docs/devices/DynapSE` for more.
 """
-
-from . import samna_alias as sa
 
 from .simulation import DynapSim, frozen_mismatch_prototype, dynamic_mismatch_prototype
 from .mapping import DynapseNeurons, mapper
