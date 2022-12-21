1789b8a65b325d4b136dd7d6f6122cc9040d6393
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-12-21 17:46:46 +0300

    see also block fixed

--- rockpool/devices/dynapse/simulation/__init__.py
+++ rockpool/devices/dynapse/simulation/__init__.py
@@ -6,7 +6,9 @@ JAX-backend Dynap-SE2 simulator named `DynapSim` does not simulate the hardware 
 It uses forward Euler updates to predict the time-dependent dynamics and solves the characteristic circuit transfer functions in time.
 
 This module also provides a surrogate function implementation to support gradient based optimization and a mismatch prototype to support analog device mismatch simulation.
-See the neuron model tutorial provided in :ref:`docs/devices/DynapSE/neuron-model.ipynb`
+
+See also:
+    The neuron model tutorial provided in :ref:`/devices/DynapSE/neuron-model.ipynb`
 """
 from .dynapsim import *
 from .surrogate import *
