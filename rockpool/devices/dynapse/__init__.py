effa77245c75bd1ccf5d4efdc19aa83f7d83b975
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-12-21 15:42:32 +0300

    DynapSE init file formatted for RST

--- rockpool/devices/dynapse/__init__.py
+++ rockpool/devices/dynapse/__init__.py
@@ -4,40 +4,48 @@ Dynap-SE2 Application Programming Interface (API)
 This package provides an abstract Dynap-SE2 machine that operates in the same parameter space as Dynap-SE2 processors. 
 More, all the required tools to convert a simulation setting to an hardware configuration and a hardware configuration to a simulation setting.
 
-A user can 
+It's possible to go from simulation to deployment:
 
-* define a rockpool network 
-* map this network to a hardware specification
-* quantize the specified paramters
-* obtain a samna configuration
-* connect and configure a Dynap-SE2 chip 
-* run a real-time, hardware simulation
+* Define a rockpool network 
+* Map this network to a hardware specification
+* Quantize the parameters
+* Obtain a samna configuration
+* Connect and configure a Dynap-SE2 chip 
+* Run a real-time, hardware simulation
 
-```python
-net = Sequential(
-    LinearJax(shape=(Nin, Nrec), has_bias=False),
-    DynapSim((Nrec, Nrec), has_rec=True, percent_mismatch=0.05),
-)
+..  code-block:: python
+    :caption: Simulation -> Device (pseudo-code)
 
-spec = mapper(net.as_graph())
-spec.update(autoencoder_quantization(**spec))
-config = config_from_specification(**spec)
+    # Define
+    net = Sequential(LinearJax((Nin, Nrec)), DynapSim((Nrec, Nrec)))
 
-# Connect to device
-se2_devices = find_dynapse_boards()
-se2 = DynapseSamna(se2_devices[0], **config)
-out, state, rec = se2(raster, record=True)
-```
+    # Map
+    spec = mapper(net.as_graph())
+    spec.update(autoencoder_quantization(**spec))
+    config = config_from_specification(**spec)
 
-Or 
-* resolve a samna configuration to obtain a rockpool network
+    # Connect 
+    se2_devices = find_dynapse_boards()
+    se2 = DynapseSamna(se2_devices[0], **config)
+    out, state, rec = se2(raster, record=True)
 
-```python
-model : JaxModule = dynapsim_net_from_config(**config)
-out, state, rec = model(raster, record=True)
-```
 
-See the tutorials provided in :ref:`/docs/devices/DynapSE`
+It's also possible to go from hardware configuration to simulation:
+
+..  code-block:: python
+    :caption: Device -> Simulation (pseudo-code)
+
+    net = dynapsim_net_from_config(**config)
+    out, state, rec = net(raster, record=True)
+
+
+See Also:
+    See the tutorials
+
+    * :ref:`/devices/DynapSE/dynapse-overview.ipynb`
+    * :ref:`/devices/DynapSE/jax-training.ipynb`
+    * :ref:`/devices/DynapSE/neuron-model.ipynb`
+    * :ref:`/devices/DynapSE/post-training.ipynb`
 """
 
 from .simulation import DynapSim, frozen_mismatch_prototype, dynamic_mismatch_prototype
