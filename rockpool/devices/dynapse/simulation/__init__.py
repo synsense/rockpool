fd8db8bcc6c600a01c168f7080c6fdf4bcda5b25
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2022-12-23 08:44:17 +0300

    fixed path reference

--- rockpool/devices/dynapse/simulation/__init__.py
+++ rockpool/devices/dynapse/simulation/__init__.py
@@ -2,7 +2,7 @@
 Dynap-SE2 Simulation Module
 
 This module implements a neuron model that solves analog VLSI circuit equations to provide a reliable simulated machine.
-JAX-backend Dynap-SE2 simulator named `DynapSim` does not simulate the hardware numerically precisely but executes a fast and an approximate simulation.
+JAX-backend Dynap-SE2 simulator named `devices.dynapse.DynapSim` does not simulate the hardware numerically precisely but executes a fast and an approximate simulation.
 It uses forward Euler updates to predict the time-dependent dynamics and solves the characteristic circuit transfer functions in time.
 
 This module also provides a surrogate function implementation to support gradient based optimization and a mismatch prototype to support analog device mismatch simulation.
