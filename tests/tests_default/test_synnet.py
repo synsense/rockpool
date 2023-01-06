e539cc8a9c534b8833a97f41af96e0e5dbe0cdef
Hannah Bos <hannah.bos@posteo.de>
2023-01-06 12:31:40 +0100

    fixed state test

--- tests/tests_default/test_synnet.py
+++ tests/tests_default/test_synnet.py
@@ -62,8 +62,8 @@ def test_synnet_forward():
     out, state, rec = model(inp, record=True)
 
     assert torch.allclose(out, rec["spk_out"]["spikes"])
-    # print(state["spk_out"], rec["spk_out"]["spikes"].size())
-    assert torch.allclose(state["spk_out"]['spikes'], rec["spk_out"]["spikes"][0])
+    assert torch.allclose(state["spk_out"]['spikes'], rec["spk_out"]["spikes"][0, -1])
+    assert torch.allclose(state["spk_out"]['vmem'], rec["spk_out"]["vmem"][0, -1])
 
 
 def test_synnet_record():
