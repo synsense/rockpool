e8d975ee89fd137ce4f8bbff9d127e6c95ca086a
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-01-09 15:01:41 +0100

    mapping init header

--- rockpool/devices/dynapse/mapping/__init__.py
+++ rockpool/devices/dynapse/mapping/__init__.py
@@ -1,2 +1,9 @@
+""" 
+Dynap-SE2 SNN graph mapping package
+
+Supported Operations:
+    * Try to extract a computational graph from any (almost, constraints apply) multi-layer LIF (jax or torch) network and convert it to a `DynapSim` computational graph
+    * Try to map the computational graph to hardware configuration by allocating hardware resources
+"""
 from .graph import *
 from .mapper import *
