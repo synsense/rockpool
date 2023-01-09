5a94bfa62072d9038a4074b4141131f126476fda
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-01-09 15:35:10 +0100

    low level simulated properties init header

--- rockpool/devices/dynapse/parameters/translation/low_level/__init__.py
+++ rockpool/devices/dynapse/parameters/translation/low_level/__init__.py
@@ -1,3 +1,8 @@
+"""
+Dynap-SE low level property conversion and computation utility class implementations
+
+* Non User Facing *
+"""
 from .currents import *
 from .layout import *
 from .weights import *
