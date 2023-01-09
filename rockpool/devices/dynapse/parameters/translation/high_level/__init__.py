e53715990f593b1f820d6c2c77ddbe451e81e75a
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-01-09 15:29:27 +0100

    high level property package init header

--- rockpool/devices/dynapse/parameters/translation/high_level/__init__.py
+++ rockpool/devices/dynapse/parameters/translation/high_level/__init__.py
@@ -1,2 +1,7 @@
-from .time import *
-from .gain import *
+"""
+Dynap-SE high level property conversion and computation utility class implementations
+
+* Non User Facing *
+"""
+from ._time import *
+from ._gain import *
