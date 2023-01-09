ccb3a3068763edde07395e4627ec8488c4a158d5
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-01-09 15:30:17 +0100

    High level property base class header

--- rockpool/devices/dynapse/parameters/translation/high_level/base.py
+++ rockpool/devices/dynapse/parameters/translation/high_level/base.py
@@ -1,10 +1,7 @@
 """
-Dynap-SE2 full board configuration classes and methods
+Dynap-SE2 high level property conversion abstract base class
 
-Project Owner : Dylan Muir, SynSense AG
-Author : Ugurcan Cakal
-E-mail : ugurcan.cakal@gmail.com
-03/05/2022
+* Non User Facing *
 """
 
 from __future__ import annotations
