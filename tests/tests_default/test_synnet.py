1569ecae79973b47e882924c83e7daab35933293
Hannah Bos <hannah.bos@posteo.de>
2023-01-11 16:37:54 +0100

    added test for time constants in synnet

--- tests/tests_default/test_synnet.py
+++ tests/tests_default/test_synnet.py
@@ -17,9 +17,9 @@ def test_synnet_init():
         n_channels=12,
         size_hidden_layers=[60, 20, 3],
         time_constants_per_layer=[3, 7, 1],
-        tau_syn_base=3,
-        tau_mem=10,
-        tau_syn_out=4,
+        tau_syn_base=0.031,
+        tau_mem=0.01,
+        tau_syn_out=0.004,
         threshold=1.3,
         threshold_out=7.2,
         train_threshold=True,
@@ -27,6 +27,7 @@ def test_synnet_init():
         max_spikes_per_dt=13,
         max_spikes_per_dt_out=2,
         p_dropout=0.2,
+        dt=10e-3,
     )
 
 
@@ -41,14 +42,15 @@ def test_synnet_forward():
         n_channels=n_neurons_input,
         size_hidden_layers=[20, 8],
         time_constants_per_layer=[8, 1],
-        tau_syn_base=3.2,
-        tau_mem=1.5,
-        tau_syn_out=2.6,
+        tau_syn_base=0.0032,
+        tau_mem=0.0015,
+        tau_syn_out=0.0026,
         threshold=1.0,
         threshold_out=0.001,
         max_spikes_per_dt=15,
         max_spikes_per_dt_out=12,
         p_dropout=0.1,
+        dt=1e-3,
     )
 
     # input params
@@ -62,8 +64,8 @@ def test_synnet_forward():
     out, state, rec = model(inp, record=True)
 
     assert torch.allclose(out, rec["spk_out"]["spikes"])
-    assert torch.allclose(state["spk_out"]["spikes"], rec["spk_out"]["spikes"][0, -1])
-    assert torch.allclose(state["spk_out"]["vmem"], rec["spk_out"]["vmem"][0, -1])
+    assert torch.allclose(state["spk_out"]['spikes'], rec["spk_out"]["spikes"][0, -1])
+    assert torch.allclose(state["spk_out"]['vmem'], rec["spk_out"]["vmem"][0, -1])
 
 
 def test_synnet_record():
@@ -77,14 +79,15 @@ def test_synnet_record():
         n_channels=n_neurons_input,
         size_hidden_layers=[20, 8, 8],
         time_constants_per_layer=[8, 1, 3],
-        tau_syn_base=3.2,
-        tau_mem=1.5,
-        tau_syn_out=2.6,
+        tau_syn_base=0.0032,
+        tau_mem=0.0015,
+        tau_syn_out=0.0026,
         threshold=1.0,
         threshold_out=100,
         max_spikes_per_dt=15,
         max_spikes_per_dt_out=12,
         p_dropout=0.1,
+        dt=1e-3,
     )
 
     # input params
@@ -104,14 +107,15 @@ def test_synnet_record():
         n_channels=n_neurons_input,
         size_hidden_layers=[20, 8],
         time_constants_per_layer=[8, 1],
-        tau_syn_base=3.2,
-        tau_mem=1.5,
-        tau_syn_out=2.6,
+        tau_syn_base=0.0032,
+        tau_mem=0.0015,
+        tau_syn_out=0.0026,
         threshold=1.0,
         threshold_out=100,
         max_spikes_per_dt=15,
         max_spikes_per_dt_out=12,
         p_dropout=0.0,
+        dt=2*1e-3,
     )
 
     # forward
@@ -124,6 +128,7 @@ def test_synnet_record():
 
 def test_synnet_backward():
     from rockpool.nn.networks import SynNet
+    from rockpool.nn.modules import LIFTorch
     import torch
 
     # - Ensure deterministic testing
@@ -136,15 +141,17 @@ def test_synnet_backward():
         n_channels=n_neurons_input,
         size_hidden_layers=[20, 8],
         time_constants_per_layer=[8, 1],
-        tau_syn_base=3.2,
-        tau_mem=1.5,
-        tau_syn_out=2.6,
+        neuron_model=LIFTorch,
+        tau_syn_base=0.032,
+        tau_mem=0.075,
+        tau_syn_out=0.046,
         threshold=1.0,
         threshold_out=1.0,
         train_threshold=True,
         max_spikes_per_dt=15,
         max_spikes_per_dt_out=12,
         p_dropout=0.1,
+        dt=1e-8,
     )
 
     # input params
@@ -157,11 +164,16 @@ def test_synnet_backward():
     inp.requires_grad = True
 
     # forward
-    out, state, rec = model(inp)
+    out, state, rec = model(inp, record=True)
 
     # backward
     out.sum().backward()
 
+    print(model.spk0.tau_syn, model.spk0.tau_mem)
+    print(model.spk1.tau_syn, model.spk1.tau_mem)
+    print(model.lin1.weight.grad)
+    print(rec['spk0'])
+    # print(out)
     assert not torch.all(inp.grad == 0)
     assert not torch.all(model.lin0.weight.grad == 0)
     assert not torch.all(model.lin1.weight.grad == 0)
@@ -185,14 +197,15 @@ def test_synnet_trainables():
         n_channels=n_neurons_input,
         size_hidden_layers=[20, 8],
         time_constants_per_layer=[8, 1],
-        tau_syn_base=3.2,
-        tau_mem=1.5,
-        tau_syn_out=2.6,
+        tau_syn_base=0.0032,
+        tau_mem=0.0015,
+        tau_syn_out=0.0026,
         threshold=1.0,
         threshold_out=1.0,
         max_spikes_per_dt=15,
         max_spikes_per_dt_out=12,
         p_dropout=0.1,
+        dt=1e-3,
     )
 
     assert model.lin0.weight.requires_grad is True
@@ -234,15 +247,16 @@ def test_synnet_reset():
         n_channels=n_neurons_input,
         size_hidden_layers=[20, 8],
         time_constants_per_layer=[8, 1],
-        tau_syn_base=3.2,
-        tau_mem=1.5,
-        tau_syn_out=2.6,
+        tau_syn_base=0.0032,
+        tau_mem=0.0015,
+        tau_syn_out=0.0026,
         threshold=1.0,
         threshold_out=1.0,
         train_threshold=True,
         max_spikes_per_dt=15,
         max_spikes_per_dt_out=12,
         p_dropout=0.1,
+        dt=1e-3,
     )
 
     # input params
@@ -282,15 +296,121 @@ def test_synnet_graph():
         n_channels=n_neurons_input,
         size_hidden_layers=[20, 8],
         time_constants_per_layer=[8, 1],
-        tau_syn_base=3.2,
-        tau_mem=1.5,
-        tau_syn_out=2.6,
+        tau_syn_base=0.0032,
+        tau_mem=0.0015,
+        tau_syn_out=0.0026,
         threshold=1.0,
         threshold_out=1.0,
         train_threshold=True,
         max_spikes_per_dt=15,
         max_spikes_per_dt_out=12,
         p_dropout=0.1,
+        dt=1e-3,
     )
 
     model.as_graph()
+
+
+def test_synnet_time_constants():
+    import torch
+    from rockpool.nn.networks import SynNet
+    from rockpool.nn.modules import LIFTorch
+
+    tau_mem = 0.01
+    tau_syn_base = 0.003
+    tau_syn_out = 0.004
+    dt = 1.3*1e-3
+    size_hidden_layers = [60, 3]
+    time_constants_per_layer = [3, 1]
+
+    model = SynNet(
+        n_classes=3,
+        n_channels=12,
+        size_hidden_layers=size_hidden_layers,
+        time_constants_per_layer=time_constants_per_layer,
+        tau_syn_base=tau_syn_base,
+        tau_mem=tau_mem,
+        tau_syn_out=tau_syn_out,
+        quantize_time_constants=False,
+        threshold=1.3,
+        threshold_out=7.2,
+        train_threshold=True,
+        neuron_model=LIFTorch,
+        max_spikes_per_dt=13,
+        max_spikes_per_dt_out=2,
+        p_dropout=0.2,
+        dt=dt,
+    )
+
+    assert model.spk0.tau_mem == tau_mem
+    assert model.spk1.tau_mem == tau_mem
+    assert model.spk_out.tau_mem == tau_mem
+
+    # check values and frequencies of time constants
+    tau_syn_hidden0 = torch.tensor([(tau_syn_base / dt) ** j * dt for j in range(1, time_constants_per_layer[0] + 1)])
+    torch.sort(tau_syn_hidden0)
+    tau_unique, frequencies = model.spk0.tau_syn.unique(return_counts=True)
+    assert torch.allclose(tau_unique, tau_syn_hidden0)
+    n_tau = time_constants_per_layer[0]
+    s = size_hidden_layers[0]
+    while n_tau >= 1:
+        n = torch.ceil(torch.tensor(s/n_tau))
+        assert torch.sum(frequencies[-n_tau]) == n
+        n_tau -= n
+
+    tau_syn_hidden1 = torch.tensor([(tau_syn_base / dt) ** j * dt for j in range(1, time_constants_per_layer[1] + 1)])
+    torch.sort(tau_syn_hidden1)
+    tau_unique, frequencies = model.spk1.tau_syn.unique(return_counts=True)
+    assert torch.allclose(tau_unique, tau_syn_hidden1)
+    n_tau = time_constants_per_layer[1]
+    s = size_hidden_layers[1]
+    while n_tau >= 1:
+        n = torch.ceil(torch.tensor(s / n_tau))
+        assert torch.sum(frequencies[-n_tau]) == n
+        n_tau -= n
+
+    assert model.spk_out.tau_syn == tau_syn_out
+
+
+def test_synnet_time_constants_quantized():
+    import torch
+    from rockpool.nn.networks import SynNet
+    from rockpool.nn.modules import LIFTorch
+    from rockpool.nn.modules.torch.lif_torch import tau_to_bitshift, bitshift_to_tau
+
+    tau_mem = 0.01
+    tau_syn_base = 0.003
+    tau_syn_out = 0.004
+    dt = 1.3*1e-3
+    size_hidden_layers = [60, 3]
+    time_constants_per_layer = [3, 1]
+
+    model = SynNet(
+        n_classes=3,
+        n_channels=12,
+        size_hidden_layers=size_hidden_layers,
+        time_constants_per_layer=time_constants_per_layer,
+        tau_syn_base=tau_syn_base,
+        tau_mem=tau_mem,
+        tau_syn_out=tau_syn_out,
+        quantize_time_constants=True,
+        threshold=1.3,
+        threshold_out=7.2,
+        train_threshold=True,
+        neuron_model=LIFTorch,
+        max_spikes_per_dt=13,
+        max_spikes_per_dt_out=2,
+        p_dropout=0.2,
+        dt=dt,
+    )
+
+    tau_mem_bitshift = torch.round(tau_to_bitshift(dt, torch.tensor(tau_mem))[0]).int()
+    tau_mem_rounded = bitshift_to_tau(dt, tau_mem_bitshift)[0].item()
+    assert model.spk0.tau_mem == tau_mem_rounded
+    assert model.spk1.tau_mem == tau_mem_rounded
+    assert model.spk_out.tau_mem == tau_mem_rounded
+
+    tau_syn_bitshift = torch.round(tau_to_bitshift(dt, torch.tensor(tau_syn_base))[0]).int()
+    tau_syn_rounded = bitshift_to_tau(dt, tau_syn_bitshift)[0].item()
+    assert model.spk0.tau_syn[0] == tau_syn_rounded
+    assert model.spk1.tau_syn[1] == tau_syn_rounded
