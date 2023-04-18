10a71987c6ac72663857fbcc55fe92d48c5c31db
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-04-18 13:56:12 +0200

    Add two tests for IMU quantization

---
+++ tests/tests_default/devices_xylo_imu/test_preprocessing.py
@@ -0,0 +1,41 @@
+""" 
+Tests for quantization preprocessing module
+"""
+
+
+def test_quantization():
+    import numpy as np
+    from numpy.testing import assert_array_equal
+    from rockpool.devices.xylo.imu.preprocessing import Quantizer
+
+    # Create quantizer with scale of 1.0 and num_bits of 16
+    quantizer = Quantizer(scale=1.0, num_bits=16)
+
+    # Quantize input signal
+    input_data = np.array([1, -1, 0.00004, 0.00003])
+    output, _, _ = quantizer.evolve(input_data)
+
+    # Check that output is equal to expected_output
+    expected_output = np.array([32768, -32768, 1, 0])
+    assert_array_equal(output, expected_output)
+
+
+def test_quantization_num_bits():
+    import numpy as np
+    from numpy.testing import assert_array_equal
+    from rockpool.devices.xylo.imu.preprocessing import Quantizer
+
+    # Create quantizer with scale of 1.0 and num_bits of 4
+    quantizer = Quantizer(scale=1.0, num_bits=4)
+
+    # Quantize a synthetic input signal
+    input_data = np.array([1, -1, 0.125, 0.1])
+    output, _, _ = quantizer.evolve(input_data)
+
+    # Check that output is equal to expected_output
+    expected_output = np.array([8, -8, 1, 0])
+    assert_array_equal(output, expected_output)
+
+
+if __name__ == "__main__":
+    test_quantization_num_bits()
