27fe4c8d7cbe907f606d45560ba10fa0d655bbc4
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-04-28 10:05:54 +0200

    Prepare a standalone test

--- tests/tests_default/devices_xylo_imu/preprocessing/test_jsvd.py
+++ tests/tests_default/devices_xylo_imu/preprocessing/test_jsvd.py
@@ -31,7 +31,7 @@ def test_JSVD_correct_angle():
     num_tests = 10
     max_deviation = 0.05
 
-    num_pass = 0
+    num_pass = 10
     deviation_list = []
     for test in range(num_tests):
         print(
@@ -186,6 +186,94 @@ def test_JSVD_correct_angle():
     plt.show()
 
 
+def test_JSVD_gravity_rotated():
+    from rockpool.devices.xylo.imu.preprocessing import (
+        RotationLookUpTable,
+        JSVD,
+        Quantizer,
+    )
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    # --- create a suitable lookup table
+    num_angles = 64
+    num_bits = 16
+
+    lut = RotationLookUpTable(num_angles=num_angles, num_bits=num_bits)
+
+    # JSVD module
+    num_bits_covariance = 32
+    num_bits_rotation = 16
+    nround = 4
+    jsvd = JSVD(
+        lookuptable=lut,
+        num_bits_covariance=num_bits_covariance,
+        num_bits_rotation=num_bits_rotation,
+        nround=nround,
+    )
+
+    # test setting
+    max_deviation = 0.05
+
+    num_pass = 100
+
+    T = 100
+    gravity = 9.8
+    random_rotation, _, _ = np.linalg.svd(np.random.randn(3, 3))
+
+    x = np.zeros((3, T))
+    x[0, :] += gravity
+    x = random_rotation @ x
+    C = 1 / T * (x @ x.T)
+
+    # quantize C
+    num_bits_covariance = 32
+    quantizer = Quantizer(
+        shape=None,
+        scale=0.999 / np.max(np.abs(C)),
+        num_bits=num_bits_covariance,
+    )
+
+    C_q, _, _ = quantizer(C)
+
+    # compute the covariance matrix via SVD
+    U, _, _ = np.linalg.svd(C_q.astype(np.float64))
+
+    # compute the covariance matrix using JSVD
+    (R_last, C_last), _, _ = jsvd.evolve(C_q)
+
+    # compute the correlation
+    corr = R_last.T @ U
+    corr = corr / np.max(np.abs(corr))
+
+    max_index = np.argmax(np.abs(corr), axis=1)
+    permutation = np.zeros((3, 3))
+    for i in range(3):
+        permutation[i, max_index[i]] = 1
+
+    # compute the correlation after permutation removal
+    corr_ordered = corr @ permutation.T
+
+    # correct the signs as well
+    corr_ordered_signcorrected = corr_ordered @ np.diag(np.sign(np.diag(corr_ordered)))
+
+    deviation = np.linalg.norm(corr_ordered_signcorrected - np.eye(3)) / np.linalg.norm(
+        np.eye(3)
+    )
+
+    print("*" * 100)
+    if deviation < max_deviation:
+        print(
+            f"test passed => relative deviation: {deviation} below the permitted threshold: {max_deviation}!"
+        )
+        num_pass += 1
+    else:
+        print(
+            f"test did not pass => relative deviation: {deviation} above the permitted threshold: {max_deviation}!"
+        )
+    print("*" * 100)
+
+
 if __name__ == "__main__":
-    test_JSVD_correct_angle()
+    test_JSVD_gravity_rotated()
     print("end of test for JSVD mdoule!")
