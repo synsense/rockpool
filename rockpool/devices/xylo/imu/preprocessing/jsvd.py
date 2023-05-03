367e39d0e33291a12a9dd289f57d4ef00a99ef4c
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-05-03 16:21:21 +0200

    Move type_check to utils

--- rockpool/devices/xylo/imu/preprocessing/jsvd.py
+++ rockpool/devices/xylo/imu/preprocessing/jsvd.py
@@ -7,50 +7,15 @@ computes the 3 x 3 rotation matrix and 3 x 3 diagonal matrix.
     (iii)   This yields a higher precision in implementation of JSVD.
 """
 import warnings
-from functools import wraps
 from typing import List, Tuple
 
 import numpy as np
 
 from rockpool.devices.xylo.imu.preprocessing import RotationLookUpTable
+from rockpool.devices.xylo.imu.preprocessing.utils import type_check
 from rockpool.nn.modules.module import Module
 
 __all__ = ["JSVD"]
-
-
-def type_check(func):
-    """Type-check decorator for IMU python simulation to make sure that all the input data are of type `python.object`.
-    This assures that the hardware and software will behave the same for all register sizes.
-
-    Args:
-        func (Callable): the function to be decorated.
-    """
-
-    def verify(input: List[np.ndarray]) -> None:
-        if isinstance(input, list):
-            if len(input) != 0:
-                for el in input:
-                    type_check(el)
-
-        elif isinstance(input, np.ndarray):
-            if input.dtype != object or type(input.ravel()[0]) != int:
-                raise ValueError(
-                    f"The elements of the following variable are not of type `python.object` integer. This may cause mismatch between hardware and python implementation."
-                    + f"problem with the follpowing variable:\n{input}\n"
-                    + f"To solve the problem make sure that all the arrays have `dtype=object`. You can use `Quantizer` class in `quantizer.py` module."
-                )
-
-    @wraps(func)
-    def inner_func(*args, **kwargs):
-        for arg in args:
-            verify(arg)
-
-        for key in kwargs:
-            verify(kwargs[key])
-
-        return func(*args, **kwargs)
-
-    return inner_func
 
 
 class JSVD(Module):
