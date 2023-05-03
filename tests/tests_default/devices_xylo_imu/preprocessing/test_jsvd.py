4de532a8217b493508e2e31cc9dc0cabeab90c2f
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-05-03 12:28:30 +0200

    Simplify the test

--- tests/tests_default/devices_xylo_imu/preprocessing/test_jsvd.py
+++ tests/tests_default/devices_xylo_imu/preprocessing/test_jsvd.py
@@ -1,289 +1,86 @@
 """Test JSVD computation module"""
 
 
-def test_JSVD_correct_angle():
+def test_JSVD_low_rank_gravity():
     from rockpool.devices.xylo.imu.preprocessing import (
         RotationLookUpTable,
         JSVD,
         Quantizer,
     )
-    import matplotlib.pyplot as plt
     import numpy as np
 
-    # --- create a suitable lookup table
-    num_angles = 64
-    num_bits = 16
-
-    lut = RotationLookUpTable(num_angles=num_angles, num_bits=num_bits)
-
-    # JSVD module
-    num_bits_covariance = 32
-    num_bits_rotation = 32
-    nround = 4
+    # - Init JSVD module
     jsvd = JSVD(
-        lookuptable=lut,
-        num_bits_covariance=num_bits_covariance,
-        num_bits_rotation=num_bits_rotation,
-        nround=nround,
+        lookuptable=RotationLookUpTable(num_angles=64, num_bits=16),
+        num_bits_covariance=32,
+        num_bits_rotation=32,
+        nround=4,
     )
 
-    # test setting
-    num_tests = 10
-    max_deviation = 0.05
+    num_test = 10
+    np.random.seed(2023)
 
-    num_pass = 0
     deviation_list = []
-    for test in range(num_tests):
-        print(
-            f"\ncorrelation test between JSVD and SVD: num-angles:{num_angles}, num-bits:{num_bits}, num-bits-covariance:{num_bits_covariance}, num-bits-rotation:{num_bits_rotation}, TEST #{test}\n"
-        )
-
+    for _ in range(num_test):
+        # - Create a random rotation matrix
         T = 100
+        num_ch = 3
         gravity = 10
-        random_rotation, _, _ = np.linalg.svd(np.random.randn(3, 3))
+        random_rotation, _, _ = np.linalg.svd(np.random.randn(num_ch, num_ch))
 
         # several variation of data
-        data_types = ["low-rank", "gravity-rotated", "noise-gravity-rotated"]
+        x_in = np.random.randn(num_ch, T)
+        x_in[0, :] += gravity
+        x_in = random_rotation @ x_in
+        U, D, V = np.linalg.svd(x_in)
+        D[0] = 0
 
-        data_type = data_types[-1]
+        x_in_low_rank = U @ np.diag(D) @ V[:num_ch, :]
 
-        if data_type == "gravity-rotated":
-            x = np.zeros((3, T))
-            x[0, :] += gravity
-            x = random_rotation @ x
-
-        elif data_type == "noise-gravity-rotated":
-            x = np.random.randn(3, T)
-            x[0, :] += gravity
-            x = random_rotation @ x
-            # x=np.tile(x[:,0].reshape(-1,1), T)
-
-        elif data_type == "low-rank":
-            x = np.random.randn(3, T)
-            x[0, :] += gravity
-            x = random_rotation @ x
-            U, D, V = np.linalg.svd(x)
-            D[0] = 0
-
-            x_low_rank = U @ np.diag(D) @ V[:3, :]
-            x = x_low_rank
-
-        else:
-            assert 1 == 0, "a data type is needed for investigation!"
-
-        C = 1 / T * (x @ x.T)
+        cov_in = 1 / T * (x_in_low_rank @ x_in_low_rank.T)
 
         # quantize C
-        num_bits_covariance = 32
-        quantizer = Quantizer(
-            shape=None,
-            scale=0.999 / np.max(np.abs(C)),
-            num_bits=num_bits_covariance,
+        Q = Quantizer(
+            scale=0.999 / np.max(np.abs(cov_in)), num_bits=jsvd.num_bits_covariance
         )
-
-        C_q, _, _ = quantizer(C)
+        cov_quant, _, _ = Q(cov_in)
 
         # compute the covariance matrix via SVD
-        U, Lambda, _ = np.linalg.svd(C_q.astype(np.float64))
+        U, _, _ = np.linalg.svd(cov_quant.astype(np.float64))
 
-        # compute the covariance matrix using JSVD2
-        (R_last, C_last), _, _ = jsvd.evolve(C_q)
+        # compute the covariance matrix using JSVD
+        (R_last, C_last), _, _ = jsvd.evolve(cov_quant)
 
-        R_last_norm = R_last @ np.diag(
-            1 / np.sqrt(np.sum(R_last**2, axis=0).astype(np.float64))
+        # compute the correlation
+        corr = R_last.T @ U
+        corr = corr / np.max(np.abs(corr))
+
+        max_index = np.argmax(np.abs(corr), axis=1)
+        permutation = np.zeros((3, 3))
+        for i in range(3):
+            permutation[i, max_index[i]] = 1
+
+        # compute the correlation after permutation removal
+        corr_ordered = corr @ permutation.T
+
+        # correct the signs as well
+        corr_ordered_signcorrected = corr_ordered @ np.diag(
+            np.sign(np.diag(corr_ordered))
         )
 
-        # this is the weighted ovariance version used as a metric
-        # U_orig, Lambda_orig, _ = np.linalg.svd(C)
-        C_est = R_last_norm @ np.diag(Lambda) @ R_last_norm.T
+        deviation = np.linalg.norm(
+            corr_ordered_signcorrected - np.eye(3)
+        ) / np.linalg.norm(np.eye(3))
 
-        deviation = np.linalg.norm(C_est - C_q) / np.linalg.norm(C_q)
-
-        # # compute the correlation
-        # corr = R_last.T @ U
-        # corr = corr / np.max(np.abs(corr))
-
-        # max_index = np.argmax(np.abs(corr), axis=1)
-        # permutation = np.zeros((3, 3))
-        # for i in range(3):
-        #     permutation[i, max_index[i]] = 1
-
-        # # compute the correlation after permutation removal
-        # corr_ordered = corr @ permutation.T
-
-        # # correct the signs as well
-        # corr_ordered_signcorrected = corr_ordered @ np.diag(
-        #     np.sign(np.diag(corr_ordered))
-        # )
-
-        # deviation = np.linalg.norm(
-        #     corr_ordered_signcorrected - np.eye(3)
-        # ) / np.linalg.norm(np.eye(3))
-
+        # Test
+        max_deviation = 0.05
+        assert (
+            deviation < max_deviation
+        ), "JSVD failed to estimate the covariance matrix correctly"
         deviation_list.append(deviation)
 
-        print("*" * 100)
-        if deviation < max_deviation:
-            print(
-                f"test #{test} passed => relative deviation: {deviation} below the permitted threshold: {max_deviation}!"
-            )
-            num_pass += 1
-        else:
-            print(
-                f"test #{test} did not pass => relative deviation: {deviation} above the permitted threshold: {max_deviation}!"
-            )
-        print("*" * 100)
-
-    # compute the last data sample after rotation removal
-    R_last_normalized = R_last / np.linalg.norm(R_last[:]) * np.sqrt(3)
-    x_rotation_removed = R_last_normalized.T @ x
-
-    U, D, _ = np.linalg.svd(x)
-    x_rotation_removed_exact = U.T @ x
-
-    # result of analysis
-    print("\n\n")
-    print("*" * 100)
-    print("*" * 100)
-    print(
-        f"{num_pass} out of {num_tests} were passed successfully with maximum relative deviation {max_deviation}!"
-    )
-    print("*" * 100)
-    print("*" * 100)
-
-    # plot the results
-    plt.figure(figsize=(12, 12))
-    plt.subplot(211)
-    plt.hist(deviation_list, 100)
-    plt.xlabel("relative deviation in JSVD")
-    plt.ylabel("histogram")
-    plt.grid(True)
-    plt.title("statistical analysis of the behavior of JSVD")
-
-    plt.subplot(212)
-    plt.semilogy(np.sort(deviation_list), np.linspace(1, 0, len(deviation_list)))
-    plt.xlabel("relative deviation")
-    plt.ylabel("CCDF")
-    plt.ylim([0.001, 1])
-    plt.plot([max_deviation, max_deviation], [0.001, 1], "r", linewidth=2)
-    plt.title(f"{num_pass} / {num_tests} tests below the max-deviation:{max_deviation}")
-
-    plt.grid(True)
-
-    plt.show(block=False)
-
-    # plot the last data sample
-    plt.figure(figsize=(12, 12))
-    plt.subplot(311)
-    plt.plot(x.T)
-    plt.grid(True)
-    plt.ylabel("channels")
-    plt.title("before rotation removal")
-
-    plt.subplot(312)
-    plt.plot(x_rotation_removed.T)
-    plt.grid(True)
-    plt.ylabel("channels")
-    plt.title("after JSVD rotation removal")
-
-    plt.subplot(313)
-    plt.plot(x_rotation_removed_exact.T)
-    plt.grid(True)
-    plt.xlabel("sample index")
-    plt.ylabel("channels")
-    plt.title("after EXACT rotation removal")
-
-    plt.show()
-
-
-def test_JSVD_gravity_rotated():
-    from rockpool.devices.xylo.imu.preprocessing import (
-        RotationLookUpTable,
-        JSVD,
-        Quantizer,
-    )
-    import matplotlib.pyplot as plt
-    import numpy as np
-
-    # --- create a suitable lookup table
-    num_angles = 64
-    num_bits = 16
-
-    lut = RotationLookUpTable(num_angles=num_angles, num_bits=num_bits)
-
-    # JSVD module
-    num_bits_covariance = 32
-    num_bits_rotation = 16
-    nround = 4
-    jsvd = JSVD(
-        lookuptable=lut,
-        num_bits_covariance=num_bits_covariance,
-        num_bits_rotation=num_bits_rotation,
-        nround=nround,
-    )
-
-    # test setting
-    max_deviation = 0.05
-
-    num_pass = 100
-
-    T = 100
-    gravity = 9.8
-    random_rotation, _, _ = np.linalg.svd(np.random.randn(3, 3))
-
-    x = np.zeros((3, T))
-    x[0, :] += gravity
-    x = random_rotation @ x
-    C = 1 / T * (x @ x.T)
-
-    # quantize C
-    num_bits_covariance = 32
-    quantizer = Quantizer(
-        shape=None,
-        scale=0.999 / np.max(np.abs(C)),
-        num_bits=num_bits_covariance,
-    )
-
-    C_q, _, _ = quantizer(C)
-
-    # compute the covariance matrix via SVD
-    U, _, _ = np.linalg.svd(C_q.astype(np.float64))
-
-    # compute the covariance matrix using JSVD
-    (R_last, C_last), _, _ = jsvd.evolve(C_q)
-
-    # compute the correlation
-    corr = R_last.T @ U
-    corr = corr / np.max(np.abs(corr))
-
-    max_index = np.argmax(np.abs(corr), axis=1)
-    permutation = np.zeros((3, 3))
-    for i in range(3):
-        permutation[i, max_index[i]] = 1
-
-    # compute the correlation after permutation removal
-    corr_ordered = corr @ permutation.T
-
-    # correct the signs as well
-    corr_ordered_signcorrected = corr_ordered @ np.diag(np.sign(np.diag(corr_ordered)))
-
-    deviation = np.linalg.norm(corr_ordered_signcorrected - np.eye(3)) / np.linalg.norm(
-        np.eye(3)
-    )
-
-    print("*" * 100)
-    if deviation < max_deviation:
-        print(
-            f"test passed => relative deviation: {deviation} below the permitted threshold: {max_deviation}!"
-        )
-        num_pass += 1
-    else:
-        print(
-            f"test did not pass => relative deviation: {deviation} above the permitted threshold: {max_deviation}!"
-        )
-    print("*" * 100)
+    print(deviation_list)
 
 
 if __name__ == "__main__":
-    test_JSVD_correct_angle()
-    print("end of test for JSVD mdoule!")
+    test_JSVD_low_rank_gravity()
