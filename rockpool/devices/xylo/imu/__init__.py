71b3fa29723b5f6ab174f1abfe131ce24c80487f
yuya ling <yuya.lin@synsense.ai>
2023-05-04 16:40:26 +0800

    add xyloimusim

--- rockpool/devices/xylo/imu/__init__.py
+++ rockpool/devices/xylo/imu/__init__.py
@@ -3,3 +3,4 @@ from .xylo_imu_devkit_utils import *
 from .xylo_mapper import *
 from .xylo_graph_modules import *
 from .xylo_imu_samna import *
+from rockpool.devices.xylo.syns61201 import XyloSim as XyloIMUSim
