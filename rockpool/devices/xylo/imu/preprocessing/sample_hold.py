1827d74406bd3d94695ecd918feacefb44fd5ee4
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-05-08 17:01:55 +0200

    Make this a rockpool module

--- rockpool/devices/xylo/imu/preprocessing/sample_hold.py
+++ rockpool/devices/xylo/imu/preprocessing/sample_hold.py
@@ -1,35 +1,45 @@
-# -----------------------------------------------------------
-# This module implement a simple sample-and-hold module.
-# This is not typically implemented like this in the hardware.
-# However we can still use this for comparison.
-#
-#
-# (C) Karla Burelo, Saeid Haghighatshoar
-# email: {karla.burelo, saeid.haghighatshoar}@synsense.ai
-#
-# last update: 28.08.2022
-# -----------------------------------------------------------
+"""
+This module implement a simple sample-and-hold module.
+This is not typically implemented like this in the hardware.
+However we can still use this for comparison.
+"""
+from typing import Optional, Tuple, Union, Dict
 import numpy as np
-from imu_preprocessing.util.type_decorator import type_check
+from rockpool.devices.xylo.imu.preprocessing.utils import type_check
+from rockpool.nn.modules.module import Module
 
 
-class SampleAndHold:
-    def __init__(self, sampling_period: int):
-        """this module samples and holds the signal in its last index (supposed to be the time-index)
+class SampleAndHold(Module):
+    """Samples and holds the signal in its last index (supposed to be the time-index)
 
-        Args:
-            sampling_period (int): sampling period.
-        """
+    Parameters:
+        sampling_period (int): sampling period.
+    """
+
+    def __init__(
+        self,
+        sampling_period: int,
+        shape: Optional[Union[Tuple, int]] = None,
+    ) -> None:
+        super().__init__(shape, spiking_input=False, spiking_output=False)
         self.sampling_period = sampling_period
 
     @type_check
-    def evolve(self, sig_in: np.ndarray):
-        """this modules samples and holds the input signal in its last index.
-        We assume that the last index is the time-dimension, so the sample-and-hold is done always along the last axes.
+    def evolve(
+        self, sig_in: np.ndarray, record: bool = False
+    ) -> Tuple[np.ndarray, Dict, Dict]:
+        """Assume that the last index is the time-dimension, so the sample-and-hold is done always along the last axes.
         Use swapaxes to change the axes if needed before calling the function.
 
         Args:
             sig_in (np.ndarray): input signal of shape (*,*,...,*, T) where T is the time-dimension along which sample-and-hold is done.
+            record (bool, optional): record flag to match with the other rockpool modules. Practically useless. Defaults to False.
+
+        Returns:
+            Tuple[np.ndarray, Dict, Dict]:
+                data: the python-object quantized version of the input signal.
+                state_dict: empty dictionary.
+                record_dict: empty dictionary.
         """
         # sample and hold the last component
         sig_in_shape = sig_in.shape
@@ -54,15 +64,8 @@ class SampleAndHold:
         # return to the original shape
         sig_out = sig_out.reshape(sig_in_shape)
 
-        return sig_out
+        return sig_out, {}, {}
 
-    # add the call version for further convenience
-    def __call__(self, *args, **kwargs):
-        """
-        This function simply calls the `evolve()` function and is added for further convenience.
-        """
-        return self.evolve(*args, **kwargs)
-
-    def __str__(self):
+    def __str__(self) -> str:
         string = "Sample-and-Hold maodule:\n" + f"period: {self.sampling_period}"
         return string
