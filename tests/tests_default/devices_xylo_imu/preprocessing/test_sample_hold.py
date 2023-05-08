a7645a1415afe785649974182371f3b6ff57d3a2
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-05-08 17:37:22 +0200

    Refactor the test

--- tests/tests_default/devices_xylo_imu/preprocessing/test_sample_hold.py
+++ tests/tests_default/devices_xylo_imu/preprocessing/test_sample_hold.py
@@ -1,59 +1,15 @@
-# -----------------------------------------------------------
-# This module tests the Sample-and-Hold mdoule with different set of parameters.
-# 
-#
-#
-# (C) Saeid Haghighatshoar
-# email: saeid.haghighatshoar@synsense.ai
-#
-#
-# last update: 28.08.2022
-# -----------------------------------------------------------
-
-# required packages
-import numpy as np
-from imu_preprocessing.sample_and_hold import SampleAndHold
-from imu_preprocessing.quantizer import Quantizer
-
-
-# varios test modules
 def test_sample_and_hold():
-    print('\nTesting the sample-and-hold module: random sampling is used!')
-    # sample-and-hold module
-    sh = SampleAndHold(
-        sampling_period=10
-    )
-    
-    # quantizer
-    Q = Quantizer()
-    
-    
-    # sample and hold of a signal of size larger than period
-    for length in np.arange(2,100):
-        sig_in = np.random.randn(length)
-        sig_in_q = Q.quantize(
-            sig_in,
-            scale=0.99/np.max(np.abs(sig_in)),
-            num_bits=30,
-        )
-        
-        sig_out_q = sh.evolve(sig_in_q)
-        sig_out_q_diff = np.diff(sig_out_q)
-        
-        
-        assert sum(np.abs(sig_out_q_diff)>0) == (length-1)//sh.sampling_period
-        
-    print('all test were passed successfully!')
-    
-    
+    import numpy as np
+    from rockpool.devices.xylo.imu.preprocessing import SampleAndHold, Quantizer
+    from rockpool.nn.combinators import Sequential
 
-# main module
-def main():
-    test_sample_and_hold()
-    
-    
-if __name__ == '__main__':
-    main()
-    print('end of test for sample-and-hold module!')
-    
-    
+    # sample and hold of a signal of size larger than period
+    for length in np.arange(2, 100):
+        sig_in = np.random.randn(1, length)
+        mod = Sequential(
+            Quantizer(scale=0.99 / np.max(np.abs(sig_in)), num_bits=30),
+            SampleAndHold(sampling_period=10),
+        )
+        out, _, _ = mod(sig_in)
+        out_diff = np.diff(out.flatten())
+        assert np.sum(np.abs(out_diff) > 0) == (length - 1) // mod[1].sampling_period
