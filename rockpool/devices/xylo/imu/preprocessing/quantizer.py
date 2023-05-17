625616fa7288680996a072b58e1f77b0cc52ffa2
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-05-17 10:48:32 +0200

    Fix docstring

--- rockpool/devices/xylo/imu/preprocessing/quantizer.py
+++ rockpool/devices/xylo/imu/preprocessing/quantizer.py
@@ -3,6 +3,7 @@ from typing import Dict, Optional, Tuple, Union
 import numpy as np
 
 from rockpool.nn.modules.module import Module
+from rockpool.parameters import SimulationParameter
 
 __all__ = ["Quantizer"]
 
@@ -15,15 +16,10 @@ class Quantizer(Module):
     when the register sizes are chosen to be larger than numpy.int64 bit length.
 
     For example:
-        - if we cast the input data into np.int64, it will work very well as far as the length of registers in the hardware are choosen
+        - if we cast the input data into np.int64, it will work very well as far as the length of registers in the hardware are chosen
         such that the outcome does not become larger than 64 bits.
         - this works very well in general but may have issues if we use larger register length in hardware.
-        - the compatibility between the hardware and software (independent of register length) is quarrenteed using this quantizer.
-
-    Parameters:
-        shape (Optional[Union[Tuple, int]], optional): Network shape. Defaults to None.
-        scale (float, optional): scale applied before quantization. Defaults to 1.0.
-        num_bits (int, optional): the number of bits in the fractional part of the quantized signal. Defaults to 16.
+        - the compatibility between the hardware and software (independent of register length) is guaranteed using this quantizer.
     """
 
     def __init__(
@@ -32,9 +28,20 @@ class Quantizer(Module):
         scale: float = 1.0,
         num_bits: int = 16,
     ) -> None:
+        """Object constructor.
+
+        Args:
+            shape (Optional[Union[Tuple, int]], optional): Network shape. Defaults to None.
+            scale (float, optional): scale applied before quantization. Defaults to 1.0.
+            num_bits (int, optional): the number of bits in the fractional part of the quantized signal. Defaults to 16.
+        """
         super().__init__(shape, spiking_input=False, spiking_output=False)
-        self.scale = scale
-        self.num_bits = num_bits
+
+        self.scale = SimulationParameter(scale, shape=(1,))
+        """scale factor applied before quantization"""
+
+        self.num_bits = SimulationParameter(num_bits, shape=(1,))
+        """The number of bits in the fractional part of the quantized signal"""
 
     def evolve(
         self, input_data: np.ndarray, record: bool = False
