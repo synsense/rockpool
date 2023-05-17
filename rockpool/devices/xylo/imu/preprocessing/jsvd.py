1f6406bf5a96edfecafc2ffe8dbb4193836fa396
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-05-17 16:06:49 +0200

    Move decorators to utils

--- rockpool/devices/xylo/imu/preprocessing/jsvd.py
+++ rockpool/devices/xylo/imu/preprocessing/jsvd.py
@@ -13,45 +13,10 @@ from typing import List, Tuple
 import numpy as np
 
 from rockpool.devices.xylo.imu.preprocessing.lookup import RotationLookUpTable
+from rockpool.devices.xylo.imu.preprocessing.utils import type_check
 from rockpool.nn.modules.module import Module
 
 __all__ = ["JSVD"]
-
-
-def type_check(func):
-    """Type-check decorator for IMU python simulation to make sure that all the input data are of type `python.object`.
-    This assures that the hardware and software will behave the same for all register sizes.
-
-    Args:
-        func (Callable): the function to be decorated.
-    """
-
-    def verify(input: List[np.ndarray]) -> None:
-        if isinstance(input, list):
-            if len(input) != 0:
-                for el in input:
-                    type_check(el)
-
-        elif isinstance(input, np.ndarray):
-            if input.dtype != object or type(input.ravel()[0]) != int:
-                raise TypeError(
-                    f"The elements of the following variable are not of type `python.object` integer. This may cause mismatch between hardware and python implementation."
-                    + f"problem with the following variable:\n{input}\n"
-                    + f"To solve the problem make sure that all the arrays have `dtype=object`. You can use `Quantizer` class in `quantizer.py` module."
-                )
-
-    @wraps(func)
-    def inner_func(*args, **kwargs):
-        for arg in args:
-            verify(arg)
-
-        for key in kwargs:
-            verify(kwargs[key])
-
-        return func(*args, **kwargs)
-
-    return inner_func
-
 
 class JSVD(Module):
     def __init__(
