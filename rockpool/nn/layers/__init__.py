6c5fc88b30e0f0bb33ef911d757ad760fe9ce45c
Dylan Muir <dylan.muir@synsense.ai>
2023-05-19 17:14:55 +0200

    Better backend management for numba modules, and xylo tests

--- rockpool/nn/layers/__init__.py
+++ rockpool/nn/layers/__init__.py
@@ -5,23 +5,35 @@ Package of converted layers from Rockpool v1
 # - Layer base class
 from .layer import *
 
+from rockpool.utilities.backend_management import backend_available, missing_backend_shim
+
 # - Brian modules
 try:
     from .iaf_brian import *
     from .exp_synapses_brian import *
 except:
-    pass
+    if not backend_available("brian"):
+        FFIAFBrian = missing_backend_shim("FFIAFBrian", "brian")
+        RecIAFBrian = missing_backend_shim("RecIAFBrian", "brian")
+        FFIAFBrianBase = missing_backend_shim("FFIAFBrianBase", "brian")
+        FFIAFSpkInBrian = missing_backend_shim("FFIAFSpkInBrian", "brian")
+        RecIAFBrianBase = missing_backend_shim("RecIAFBrianBase", "brian")
+        RecIAFSpkInBrian = missing_backend_shim("RecIAFSpkInBrian", "brian")
+        FFExpSynBrian = missing_backend_shim("FFExpSynBrian", "brian")
+    else:
+        raise
 
 # - Native (numpy / scipy) modules
-try:
-    from .updown import *
-    from .event_pass import *
-except:
-    pass
+from .updown import *
+from .event_pass import *
 
 # - Numba modules
 try:
     from .spike_bt import *
     from .spike_ads import *
 except:
-    pass
+    if not backend_available("numba"):
+        RecFSSpikeEulerBT = missing_backend_shim("RecFSSpikeEulerBT", "numba")
+        RecFSSpikeADS = missing_backend_shim("RecFSSpikeADS", "numba")
+    else:
+        raise
