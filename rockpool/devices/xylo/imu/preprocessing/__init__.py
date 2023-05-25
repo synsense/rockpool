6483563a8ccac82e676f6b0597b0a6f78ac08c56
Dylan Richard Muir <dylan.muir@synsense.ai>
2023-05-25 14:39:26 +0200

    Added some docs to imu preprocessing package

--- rockpool/devices/xylo/imu/preprocessing/__init__.py
+++ rockpool/devices/xylo/imu/preprocessing/__init__.py
@@ -1,3 +1,9 @@
+"""
+Modules for pre-processing IMU data, as implemented in Xylo IMU
+
+Defines the modules :py:class:`.JVSD`, :py:class:`.RotationLookUpTable`, :py:class:`.Quantizer`, :py:class:`.RotationRemoval`, :py:class:`.SampleAndHold`, :py:class:`.SubSpace`.
+"""
+
 from .jsvd import *
 from .lookup import *
 from .quantizer import *
