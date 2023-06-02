47aa0915442b65c7c3cd0bb1a925ffb2906b3614
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-06-02 11:50:46 +0200

    Improve docstrings

--- rockpool/devices/xylo/imu/preprocessing/spike_encoder.py
+++ rockpool/devices/xylo/imu/preprocessing/spike_encoder.py
@@ -7,14 +7,20 @@ from rockpool.devices.xylo.imu.preprocessing.utils import type_check
 
 
 class ScaleSpikeEncoder:
-    def __init__(self, num_scale_bits: int, num_out_bits: int):
-        """this module does spike encoding as follows:
-                (i)     compute the absolute value of the input sigal (full-wave rectification)
-                (ii)    down-scales the input signal by right-bit-shift by `num_scale_bit` (e.g. multiplying with 1/2^num_scale_bits)
-                (iii)   truncate the output so that it can fit within `num_out_bits`
+    """
+    Encode spikes as follows
+
+    (i)     Apply full-wave rectification to the input signal
+    (ii)    Down-scale the input signal by right-bit-shift by `num_scale_bit` (e.g. multiplying with 1/2^num_scale_bits)
+    (iii)   Truncate the output so that it can fit within `num_out_bits`
+    """
+
+    def __init__(self, num_scale_bits: int, num_out_bits: int) -> None:
+        """
+        Object constructor
 
         Args:
-            num_scale_bits (int): number of right-bit-shifs needed for down-scaling the iinput signal.
+            num_scale_bits (int): number of right-bit-shifts needed for down-scaling the input signal.
             num_out_bits (int): number of bits devoted to storing the output spike encoding.
         """
         self.num_scale_bits = num_scale_bits
@@ -35,17 +41,24 @@ class ScaleSpikeEncoder:
 
         return sig_in
 
-    def __str__(self):
-        string = (
-            "scale-and-quantize spike encpder:\n"
-            + f"number of right bit-shifts for down-scaling: {self.num_scale_bits}\n"
-            + f"number of bits used for spike encoding (clipped if less than the max amplitude): {self.num_out_bits}"
-        )
-        return string
-
 
 class IAFSpikeEncoder:
-    def __init__(self, iaf_threshold: int):
+    """
+    Synchronous integrate and fire spike encoder
+
+    More specifically, denoting a specific filter output by $$y(t)$$,
+    this module computes $$s(t) = \\sum_{n=0}^t |y(n)|$$ and as soon as it passes the firing threshold $$\\theta$$.
+    Say at a time $$t=t_0$$, it produces a spike at $$t=t_0$$ and reduces the counter by $$\theta$$ and keeps accumulating the input signal
+    $$s(t) = s(t_0) - \\theta + \\sum_{n=t_0+1}^t |y(n)|$$ until the next threshold crossing and spike generation happens.
+    """
+
+    def __init__(self, iaf_threshold: int) -> None:
+        """
+        Object constructor
+
+        Args:
+            iaf_threshold (int): the threshold of the IAF neuron (quantized)
+        """
         self.iaf_threshold = iaf_threshold
 
     @type_check
@@ -75,17 +88,3 @@ class IAFSpikeEncoder:
         spikes[spikes > 1] = 1
 
         return spikes
-
-    # add the call version for further convenience
-    def __call__(self, *args, **kwargs):
-        """
-        This function simply calls the `evolve()` function and is added for further convenience.
-        """
-        return self.evolve(*args, **kwargs)
-
-    def __str__(self):
-        string = (
-            "Integratep-and-Fire (IAF) spike encoder:\n"
-            + f"IAF threshold: {self.iaf_threshold}"
-        )
-        return string
