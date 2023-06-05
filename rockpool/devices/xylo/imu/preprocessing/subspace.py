5076b7501e1d9b5998378450f14d44ba3d2e7727
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-06-05 17:33:28 +0200

    Change parameter order

--- rockpool/devices/xylo/imu/preprocessing/subspace.py
+++ rockpool/devices/xylo/imu/preprocessing/subspace.py
@@ -22,17 +22,17 @@ num_bits_multiplier = 31
 
 class SubSpace(Module):
     """Averaging and covariance estimation for 3D IMU signals
-    BxTx3 -> BxTx3x3
+    BxTx3 -> BxTx(3x3)
     """
 
     def __init__(
-        self, num_avg_bitshift: int, shape: Optional[Union[Tuple, int]] = (3, 9)
+        self, shape: Optional[Union[Tuple, int]] = (3, 9), num_avg_bitshift: int = 4
     ) -> None:
         """Object Constructor
 
         Args:
             shape (Optional[Union[Tuple, int]], optional): The number of input and output channels. Defaults to (3,9)..
-            num_avg_bitshift (int): number of bitshifts used in the low-pass filter implementation.
+            num_avg_bitshift (int): number of bitshifts used in the low-pass filter implementation. Defaults to 4.
                 The effective window length of the low-pass filter will be `2**num_avg_bitshift`
         """
         if shape[1] != shape[0] ** 2:
