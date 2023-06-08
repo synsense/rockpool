c1221146d38a9052c8a5a00131d4767ed988a0fa
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-06-08 13:59:22 +0200

    Handle samna dependency

--- rockpool/devices/xylo/imu/preprocessing/__init__.py
+++ rockpool/devices/xylo/imu/preprocessing/__init__.py
@@ -4,8 +4,12 @@ Modules for pre-processing IMU data, as implemented in Xylo IMU
 Defines the modules :py:class:`.JVSD`, :py:class:`.RotationLookUpTable`, :py:class:`.Quantizer`, :py:class:`.RotationRemoval`, :py:class:`.SampleAndHold`, :py:class:`.SubSpace`.
 """
 
+from rockpool.utilities.backend_management import (
+    backend_available,
+    missing_backend_shim,
+)
+
 from .filterbank import *
-from .imuif_sim import *
 from .jsvd import *
 from .lookup import *
 from .quantizer import *
@@ -14,3 +18,8 @@ from .rotation_removal import *
 from .sample_hold import *
 from .spike_encoder import *
 from .subspace import *
+
+if backend_available("samna"):
+    from .imuif_sim import *
+else:
+    IMUIFSim = missing_backend_shim("IMUIFSim", "samna")
