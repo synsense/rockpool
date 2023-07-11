fe5f156f1426884a94f4da447ddb7842f671b0c6
Peng Zhou <zhoupengfun@gmail.com>
2023-07-11 17:19:22 -0600

    rockpool-nir-rockpool pipeline working

--- rockpool/nn/modules/torch/nir.py
+++ rockpool/nn/modules/torch/nir.py
@@ -1,21 +1,108 @@
+from os import PathLike
 from typing import Optional, Union
-
-import rockpool
+from rockpool.nn.modules.torch import LIFTorch, ExpSynTorch, LinearTorch
+from rockpool.nn.combinators import Sequential
 import torch
 import nir
 from nirtorch import extract_nir_graph
 
-__all__ = ["to_nir"]
+__all__ = ["to_nir", "from_nir"]
+
+def expsyntorch_from_nir(
+        node: nir.LI, shape: Union[tuple, int]=None,
+):
+    # if node.v_leak != 0:
+    #     raise ValueError("`v_leak` must be 0")
+
+    return ExpSynTorch(
+        shape=shape,
+        tau=node.tau,
+        dt=node.tau / (1+node.r),
+    )
+
+def cubalif_from_nir(
+    node: nir.CubaLIF, shape: Union[tuple, int]=None,
+):
+    # if torch.sum(node.v_leak) != 0:
+    #     raise ValueError("`v_leak` must be 0")
+
+    return LIFTorch(
+        shape=shape,
+        tau_mem=node.tau_mem,
+        tau_syn=node.tau_syn,
+        threshold=node.v_threshold,
+        dt=node.tau_mem / (1+node.r),
+    )
+
+
+def linear_from_nir(
+    node: nir.Linear,
+):
+    return LinearTorch(
+        shape=(node.weight.shape[0], node.weight.shape[1]),
+        weight=node.weight,
+    )
+
+def affline_from_nir(
+    node: nir.Affine,
+):
+
+    return LinearTorch(
+        shape=(node.weight.shape[0], node.weight.shape[1]),
+        weight=node.weight,
+        bias=node.bias,
+    )
+
+node_conversion_functions = {
+    nir.LI: expsyntorch_from_nir,
+    nir.CubaLIF: cubalif_from_nir,
+    nir.Linear: linear_from_nir,
+    nir.Affine: affline_from_nir,
+}
+
+def from_nir(
+    source: Union[PathLike, nir.NIR], shape: Union[tuple, int]=None,
+):
+    """Generates a rockpool model from a NIR representation.
+
+    Parameters:
+        source: If string or Path will try to load from file. Otherwise should be NIR object
+    """
+
+    # Convert nodes to rockpool layers
+    # layers = [
+    #     node_conversion_functions[type(node)](node)
+    #     for node in source.nodes
+    # ]
+    layers = []
+    for node in source.nodes:
+
+        if type(node) in [nir.LI, nir.CubaLIF]:
+            num_neuron = store_node.weight.shape[1]
+            layer = node_conversion_functions[type(node)](node, num_neuron)
+        elif type(node) in [nir.Linear, nir.Affine]:
+            layer = node_conversion_functions[type(node)](node)
+        store_node = node
+        layers.append(layer)
+
+    edge_array = torch.tensor(source.edges)
+    # subtract source edges and check if all edges are sequential
+    edge_array = edge_array - edge_array[:, :1]
+    if edge_array[:, 0].sum() == 0 and edge_array[:, 1].sum() == edge_array.shape[0]:
+        return Sequential(*layers)
+    else:
+        raise NotImplementedError("Only sequential models are supported at the moment")
+
 
 def _extract_rockpool_module(module) -> Optional[nir.NIRNode]:
-    if type(module) == rockpool.nn.modules.ExpSynTorch:
+    if type(module) == ExpSynTorch:
         return nir.LI(
             tau=module.tau.detach(),
             v_leak=torch.zeros_like(module.tau).detach(),
             r=(module.tau * torch.exp(-module.dt/module.tau)/module.dt).detach(),
         )
 
-    if type(module) == rockpool.nn.modules.LIFTorch:
+    if type(module) == LIFTorch:
         return nir.CubaLIF(
             tau_syn=module.tau_syn.detach(),
             tau_mem=module.tau_mem.detach(),
@@ -24,8 +111,8 @@ def _extract_rockpool_module(module) -> Optional[nir.NIRNode]:
             v_threshold=module.threshold,
         )
 
-    elif isinstance(module, rockpool.nn.modules.LinearTorch):
-        if module.bias is None:  # Add zero bias if none is present
+    elif isinstance(module, LinearTorch):
+        if module.bias is None:
             return nir.Linear(module.weight.detach())
         else:
             return nir.Affine(module.weight.detach(), module.bias.detach())
--- tests/tests_default/test_nir.py
+++ tests/tests_default/test_nir.py
@@ -1,15 +1,15 @@
 import nir
 import torch
-import pytest
-from rockpool.nn.combinators import Sequential
-from rockpool.nn.modules import LinearTorch, ExpSynTorch, LIFTorch, to_nir
+# from rockpool.nn.combinators import Sequential
+import rockpool
+from rockpool.nn.modules import LinearTorch, ExpSynTorch, LIFTorch, to_nir, from_nir
 
 def test_from_sequential_to_nir():
     Nin = 2
     Nhidden = 4
     Nout = 2
     dt = 1e-3
-    net = Sequential(
+    net = rockpool.nn.combinators.Sequential(
         LinearTorch((Nin, Nhidden), has_bias=False),
         ExpSynTorch(Nhidden, dt=dt),
         LinearTorch((Nhidden, Nout), has_bias=False),
@@ -29,7 +29,7 @@ def test_from_sequential_to_nir_2():
     Nhidden = 4
     Nout = 2
     dt = 1e-3
-    net = Sequential(
+    net = rockpool.nn.combinators.Sequential(
         LinearTorch((Nin, Nhidden), has_bias=False),
         LIFTorch(Nhidden, dt=dt),
         LinearTorch((Nhidden, Nout), has_bias=False),
@@ -52,3 +52,29 @@ def test_from_linear_to_nir():
     graph = to_nir(m, torch.randn(1, in_features))
     assert len(graph.nodes) == 1
     assert graph.nodes[0].weight.shape == torch.Size([in_features, out_features])
+
+
+def test_from_nir_to_sequential():
+    timesteps=6
+
+    orig_model = rockpool.nn.combinators.Sequential(
+        LinearTorch(shape=(5, 4)),
+        ExpSynTorch(tau=10.0, shape=4),
+        LinearTorch(shape=(4, 3)),
+        LIFTorch(tau_mem=10.0, shape=3),
+    )
+    nir_graph = to_nir(orig_model, torch.randn(timesteps, 5))
+
+    convert_model = from_nir(nir_graph)
+
+    for (key1, value1), (key2, value2) in zip(orig_model.modules().items(), convert_model.modules().items()):
+        assert key1 == key2
+        assert value2 == value2
+
+    torch.testing.assert_allclose(orig_model[0].weight, convert_model[0].weight)
+    # torch.testing.assert_allclose(orig_model[0].bias, convert_model[0].bias)
+    assert type(orig_model[1]) == type(convert_model[1])
+    assert type(orig_model[2]) == type(convert_model[2])
+    torch.testing.assert_allclose(orig_model[2].weight, convert_model[2].weight)
+    # torch.testing.assert_allclose(orig_model[2].bias, convert_model[2].bias)
+    # TODO: noise is not working
