0408c84a59d945360e30eec2a9c6b1d3449942c3
Dylan Richard Muir <dylan.muir@synsense.ai>
2023-07-26 17:54:42 +0200

    Added unit test for IMUData

---
+++ tests/tests_default/devices_xylo_imu/test_IMUData.py
@@ -0,0 +1,15 @@
+def test_IMUData():
+    import pytest
+
+    pytest.importorskip("samna")
+
+    from rockpool.devices.xylo.imu import IMUData, find_xylo_imu_boards
+    import numpy as np
+
+    hdks = find_xylo_imu_boards()
+
+    if len(hdks) < 1:
+        pytest.skip("A Xylo IMU device is required for this test")
+
+    imu = IMUData(hdks[0])
+    data = imu(np.zeros((0, 100, 0)))
