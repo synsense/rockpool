03fdecf93b9c80c48a6cf6ab4a18de6f1bdc84d0
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-08-22 14:12:58 +0200

    Remove diluting and undiluting operations
    * It's not possible to change SNN core clock, useless.

--- rockpool/devices/xylo/imu/preprocessing/identity_net.py
+++ rockpool/devices/xylo/imu/preprocessing/identity_net.py
@@ -26,9 +26,8 @@ class IdentityNet(Module):
     A simple identity network, to be used as a dummy network which aimed to return the input as the output.
     Since there is no other option to record the output of IMUIF directly, `IdentityNet` will serve as a buffer on SNN core, making it possible to implement a `IMUIFSamna` module
 
-    NOTE : NOT SURE IF THIS IS THE BEST WAY TO DO IT
-    NOTE : THIS IS A HACKY SOLUTION, TO BE REPLACED BY A BETTER ONE
-    NOTE : With a speed_up_factor >= 2, the output of the network is not the same as the input. However, it's not possible to implement this on the SNN core, so we have to live with it for now
+    NOTE : THIS IS A HACKY SOLUTION
+        * In the case the hidden state reading gets buggy, it'll fail on hardware
     """
 
     def __init__(
@@ -36,7 +35,6 @@ class IdentityNet(Module):
         device: Optional[XyloIMUHDK] = None,
         n_channel: int = 15,
         clock_rate: int = 200,
-        speed_up_factor: int = 2,
     ) -> None:
         """
         Object constructor
@@ -45,17 +43,15 @@ class IdentityNet(Module):
             device (Optional[XyloIMUHDK], optional): The device to use. If None, use XyloSim. Defaults to None.
             n_channel (int, optional): Number of channels employed. Defaults to 15.
             clock_rate (int, optional): The nominal clock rate of the input unit. Defaults to 200.
-            speed_up_factor (int, optional): The speed up factor, that is, the ratio between the clock rate of the input unit and the clock rate of the SNN core. Defaults to 2.
         """
         super().__init__(
             shape=(n_channel, n_channel), spiking_input=True, spiking_output=True
         )
 
         self.clock_rate = SimulationParameter(clock_rate, shape=())
-        self.speed_up_factor = SimulationParameter(speed_up_factor, shape=())
-        self.dt = SimulationParameter(1 / (clock_rate * speed_up_factor), shape=())
+        self.dt = SimulationParameter(1 / clock_rate, shape=())
 
-        # Hard-coded specs for the network, to be replaced by a yaml file
+        # Hard-coded specs for the network
         self.__specs = {
             "weights_in": np.expand_dims(2 * np.eye(n_channel, dtype="int8"), axis=-1),
             "weights_out": 2 * np.eye(n_channel, dtype="int8"),
@@ -98,56 +94,15 @@ class IdentityNet(Module):
             print(msg)
         return __config
 
-    def dilute(self, spike_train: FloatVector) -> FloatVector:
-        """
-        Dilute the input spike train by a factor of `speed_up_factor`
-        Inject zero columns between the consecutive columns of the input spike train
-
-        Args:
-            spike_train (FloatVector): The input spike train
-
-        Returns:
-            FloatVector: The diluted spike train
-        """
-        spike_train = np.array(spike_train)
-        __T, __C = spike_train.shape
-        new_T = __T * self.speed_up_factor
-
-        # Create an array of zeros for the new columns
-        sparsed_spike_train = np.zeros((new_T, __C), dtype=spike_train.dtype)
-        for i in range(0, __T):
-            sparsed_spike_train[i * self.speed_up_factor, :] = spike_train[i, :]
-
-        return sparsed_spike_train
-
-    def undilute(self, spike_train: FloatVector) -> FloatVector:
-        """
-        Reverse the `dilute` operation. Remove the injected zero columns between the consecutive columns of the input spike train
-
-        Args:
-            spike_train (FloatVector): The diluted spike train
-
-        Returns:
-            FloatVector: The undiluted spike train
-        """
-        spike_train = np.array(spike_train)
-        __T, __C = spike_train.shape
-        new_T = int(__T / self.speed_up_factor)
-
-        # Create an array of zeros for the new columns
-        restored_spike_train = np.zeros((new_T, __C))
-        for i in range(0, new_T):
-            restored_spike_train[i, :] = spike_train[i * self.speed_up_factor, :]
-
-        return restored_spike_train
-
     def evolve(
         self, input_data: FloatVector, record: bool = True
     ) -> Tuple[np.ndarray, dict, dict]:
         """
-        Evolve the underlying network for one time step. The input data is diluted by a factor of `speed_up_factor` before being fed to the network.
-        Dilution is done by injecting zero columns between the consecutive columns of the input spike train
-        This makes sure that the neuron and synapse are drained enough for a fresh start in the next time step
+        Evolve the underlying network for one time step.
+        Read the output spikes from the hidden neurons instead of the output neurons.
+        Readout neurons cannot be used reliably because:
+            * Buffering the input spikes and reading the same exact output from the readout neurons is technically beyond the limits of the SNN core
+            * Readout neurons do not have the capacity to spike more than once per time step, but the hidden spikes can
 
         Args:
             input_data (FloatVector): The input to the network.
@@ -159,13 +114,6 @@ class IdentityNet(Module):
                 - dict: The state of the network.
                 - dict: The record dictionary.
         """
-        # Dilute the input data
-        input_data = self.dilute(input_data)
-        out, state, rec = self.model(input_data, record=False)
-
-        # Process the output and restore the original shape
-        __T, __C = out.shape
-        out = np.vstack((out, np.zeros((1, __C))))
-        out = self.undilute(out[1:])
-
+        _, state, rec = self.model(input_data, record=True)
+        out = rec["Spikes"]
         return out, state, rec
