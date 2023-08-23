3caceacb5b94740af80ef5aae9f815b502d12de8
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-08-23 15:12:18 +0200

    Fix missing Any

--- rockpool/devices/xylo/imu/imuif_samna.py
+++ rockpool/devices/xylo/imu/imuif_samna.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Any
 import numpy as np
 from rockpool.nn.modules import Module
 from rockpool.parameters import SimulationParameter
