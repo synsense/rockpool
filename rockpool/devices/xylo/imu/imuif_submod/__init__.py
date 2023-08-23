4b34b08bf16c9b19dec9d72b6e5d39d2974a2951
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-08-23 12:28:19 +0200

    Quantizer is not a imu-if submodule
    * It's a signal transform to apply

--- rockpool/devices/xylo/imu/imuif_submod/__init__.py
+++ rockpool/devices/xylo/imu/imuif_submod/__init__.py
@@ -4,7 +4,6 @@ IMU-IF submodules, as implemented in Xylo IMU
 from .filterbank import *
 from .jsvd import *
 from .lookup import *
-from .quantizer import *
 from .rotation_removal import *
 from .sample_hold import *
 from .spike_encoder import *
