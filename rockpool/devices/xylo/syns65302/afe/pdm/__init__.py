370f630ef44991cdf857166fb05e591804a6db6b
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-10-09 18:11:31 +0200

    Separate MicrophonePDM

--- rockpool/devices/xylo/syns65302/afe/pdm/__init__.py
+++ rockpool/devices/xylo/syns65302/afe/pdm/__init__.py
@@ -1,2 +1,3 @@
-from .pdm_adc import PDM_ADC, MicrophonePDM, PolyPhaseFIR_DecimationFilter
+from .pdm_adc import PDM_ADC, PolyPhaseFIR_DecimationFilter
 from .delta_sigma import *
+from .microphone_pdm import *
