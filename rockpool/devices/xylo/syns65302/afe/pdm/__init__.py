f78ea3fc209768f0197833f1b7ebbbcdefcb5042
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-10-09 18:01:51 +0200

    Separate DeltaSigma

--- rockpool/devices/xylo/syns65302/afe/pdm/__init__.py
+++ rockpool/devices/xylo/syns65302/afe/pdm/__init__.py
@@ -1,1 +1,3 @@
-from .pdm_adc import PDM_ADC, PDM_Microphone, PolyPhaseFIR_DecimationFilter, DeltaSigma
+from .pdm_adc import PDM_ADC, PDM_Microphone, PolyPhaseFIR_DecimationFilter
+from .delta_sigma import *
+
