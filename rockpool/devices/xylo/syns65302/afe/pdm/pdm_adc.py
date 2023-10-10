6b6b741478a2cc78ff0b6babead2e690528fa061
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-10-10 11:07:10 +0200

    Create the PDMADC rockpool module

--- rockpool/devices/xylo/syns65302/afe/pdm/pdm_adc.py
+++ rockpool/devices/xylo/syns65302/afe/pdm/pdm_adc.py
@@ -12,6 +12,10 @@ module implemented here yield an ADC for the input analog audio signal.
 The low-pass filtering is implemented as a **polyphase** filter structure to consume as less power as possible.
 """
 
+from typing import Tuple
+
+import numpy as np
+
 from rockpool.devices.xylo.syns65302.afe.params import (
     AUDIO_CUTOFF_FREQUENCY,
     AUDIO_CUTOFF_FREQUENCY_WIDTH,
@@ -23,55 +27,83 @@ from rockpool.devices.xylo.syns65302.afe.params import (
     PDM_SAMPLING_RATE,
 )
 from rockpool.nn.combinators import Sequential
+from rockpool.nn.modules.module import Module
 
 from .microphone_pdm import MicrophonePDM
 from .poly_phase_fir import PolyPhaseFIR
 
-__all__ = ["PDM_ADC"]
+__all__ = ["PDMADC"]
 
 
-def PDM_ADC(
-    sdm_order: int = DELTA_SIGMA_ORDER,
-    sdm_OSR: int = PDM_FILTER_DECIMATION_FACTOR,
-    fs: int = PDM_SAMPLING_RATE,
-    cutoff: float = AUDIO_CUTOFF_FREQUENCY,
-    cutoff_width: float = AUDIO_CUTOFF_FREQUENCY_WIDTH,
-    filt_length: int = DECIMATION_FILTER_LENGTH,
-    num_bits_filter_Q: int = NUM_BITS_FILTER_Q,
-    num_bits_output: int = NUM_BITS_PDM_ADC,
-):
+class PDMADC(Module):
     """
     Analog-to-Digital (ADC) module for Xylo-A3 chip consisting of
         (i)  PDM microphone converting the input analog audio signal into PDM bit-stream.
         (ii) low-pass filtering and decimation module converting the binary PDM stream into the target sampled audio signal.
-
-    Args:
-        sdm_order (int, optional): order of deltasigma modulator. Defaults to DELTA_SIGMA_ORDER.
-        sdm_OSR (int, optional): oversampling rate of PDM microphone : ratio between PDM clock and rate of target audio signal. Defaults to PDM_FILTER_DECIMATION_FACTOR.
-        This is the same as decimation factor in PDM low-pass filter. Defaults to PDM_FILTER_DECIMATION_FACTOR.
-        fs (int, optional): sampling rate/clock rate of PDM module. Defaults to PDM_SAMPLING_RATE.
-        cutoff (float, optional): cutoff frequency of the low-pass filter used for recovery of target sampled audio from PDM bit-stream. Defaults to AUDIO_CUTOFF_FREQUENCY.
-        cutoff_width (float, optional): transition withs of the low-pass filter. Defaults to AUDIO_CUTOFF_FREQUENCY_WIDTH (is set to 20% of the cutoff frequency).
-        filt_length (int, optional): length of the FIR low-pass filter. Defaults to DECIMATION_FILTER_LENGTH.
-        num_bits_filter_Q (int, optional): number of bits used for quantizing the filter taps. Defaults to NUM_BITS_FILTER_Q.
-        num_bits_output (int, optional): target number of bits in the final sampled audio obtained after low-pass filtering and decimation.
-        This is equivalent to the number of bits in the equivalent ADC. Defaults to NUM_BITS_PDM_ADC.
     """
-    # two modules of equivalent ADC
-    return Sequential(
-        MicrophonePDM(
-            sdm_order=sdm_order,
-            sdm_OSR=sdm_OSR,
-            bandwidth=cutoff,
-            fs=fs,
-        ),
-        PolyPhaseFIR(
-            decimation_factor=sdm_OSR,
-            cutoff=cutoff,
-            cutoff_width=cutoff_width,
-            filt_length=filt_length,
-            num_bits_filter_Q=num_bits_filter_Q,
-            num_bits_output=num_bits_output,
-            fs=fs,
-        ),
-    )
+
+    def __init__(
+        self,
+        sdm_order: int = DELTA_SIGMA_ORDER,
+        sdm_OSR: int = PDM_FILTER_DECIMATION_FACTOR,
+        fs: int = PDM_SAMPLING_RATE,
+        cutoff: float = AUDIO_CUTOFF_FREQUENCY,
+        cutoff_width: float = AUDIO_CUTOFF_FREQUENCY_WIDTH,
+        filt_length: int = DECIMATION_FILTER_LENGTH,
+        num_bits_filter_Q: int = NUM_BITS_FILTER_Q,
+        num_bits_output: int = NUM_BITS_PDM_ADC,
+    ) -> None:
+        """
+        Instantiate a `PDMADC` object
+
+        Args:
+            sdm_order (int, optional): order of deltasigma modulator. Defaults to DELTA_SIGMA_ORDER.
+            sdm_OSR (int, optional): oversampling rate of PDM microphone : ratio between PDM clock and rate of target audio signal. Defaults to PDM_FILTER_DECIMATION_FACTOR.
+            This is the same as decimation factor in PDM low-pass filter. Defaults to PDM_FILTER_DECIMATION_FACTOR.
+            fs (int, optional): sampling rate/clock rate of PDM module. Defaults to PDM_SAMPLING_RATE.
+            cutoff (float, optional): cutoff frequency of the low-pass filter used for recovery of target sampled audio from PDM bit-stream. Defaults to AUDIO_CUTOFF_FREQUENCY.
+            cutoff_width (float, optional): transition withs of the low-pass filter. Defaults to AUDIO_CUTOFF_FREQUENCY_WIDTH (is set to 20% of the cutoff frequency).
+            filt_length (int, optional): length of the FIR low-pass filter. Defaults to DECIMATION_FILTER_LENGTH.
+            num_bits_filter_Q (int, optional): number of bits used for quantizing the filter taps. Defaults to NUM_BITS_FILTER_Q.
+            num_bits_output (int, optional): target number of bits in the final sampled audio obtained after low-pass filtering and decimation.
+            This is equivalent to the number of bits in the equivalent ADC. Defaults to NUM_BITS_PDM_ADC.
+        """
+
+        self.model = Sequential(
+            MicrophonePDM(
+                sdm_order=sdm_order,
+                sdm_OSR=sdm_OSR,
+                bandwidth=cutoff,
+                fs=fs,
+            ),
+            PolyPhaseFIR(
+                decimation_factor=sdm_OSR,
+                cutoff=cutoff,
+                cutoff_width=cutoff_width,
+                filt_length=filt_length,
+                num_bits_filter_Q=num_bits_filter_Q,
+                num_bits_output=num_bits_output,
+                fs=fs,
+            ),
+        )
+
+    def evolve(
+        self,
+        input_data: Tuple[np.ndarray, float],
+        record: bool = False,
+        *args,
+        **kwargs,
+    ) -> np.ndarray:
+        """
+        Take the 1D raw analog signal and processes it to produce the 14-bit 48.8 Kbps sampled audio signal.
+
+        Args:
+            input_data (np.ndarray): the input signal (BxTx3)
+            record (bool, optional): record flag to match with the other rockpool modules.
+
+        Returns:
+            np.ndarray: Output signal after rotation removal (BxTx3)
+            Dict[str, Any]: state dictionary
+            Dict[str, Any]: record dictionary
+        """
+        return self.model.evolve(input_data, record=record, *args, **kwargs)
