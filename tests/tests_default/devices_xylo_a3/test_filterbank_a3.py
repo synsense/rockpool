20f97421d54482c9a3cc8bec2e542242982bc44e
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-10-17 17:10:39 +0200

    Implement filter selection tests

--- tests/tests_default/devices_xylo_a3/test_filterbank_a3.py
+++ tests/tests_default/devices_xylo_a3/test_filterbank_a3.py
@@ -39,3 +39,54 @@ def test_filterbank():
 
     assert sig_filtered.shape[1] == NUM_FILTERS
     assert sig_filtered.shape[0] == len(sig_out)
+
+
+@pytest.mark.parametrize(
+    "select_filters",
+    [
+        (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15),
+        (15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0),
+        (10, 3, 7, 0, 14),
+    ],
+)
+def test_valid_filters(select_filters: tuple):
+    """
+    Test that the filterbank module can be instantiated with valid filter indices
+
+    Args:
+        select_filters (tuple): The indices of the filters to be used in the filter bank
+    """
+    from rockpool.devices.xylo.syns65302 import ChipButterworth
+
+    _ch = ChipButterworth(select_filters=select_filters)
+    assert _ch.size_out == len(select_filters)
+
+
+@pytest.mark.parametrize(
+    "select_filters", [(0, 1, 2, 16), (0, 1, 2, -1), (0, 0, 1, 2), (0, 0, 1, 2)]
+)
+def test_invalid_filters_value_error(select_filters: tuple):
+    """
+    Test that the filterbank module cannot be instantiated with invalid filter indices raising a ValueError
+
+    Args:
+        select_filters (tuple): The indices of the filters to be used in the filter bank
+    """
+    from rockpool.devices.xylo.syns65302 import ChipButterworth
+
+    with pytest.raises(ValueError):
+        _ch = ChipButterworth(select_filters=select_filters)
+
+
+@pytest.mark.parametrize("select_filters", [[0, 1, 2], (0, 1, 2, "a"), (0, 1, 2, 3.5)])
+def test_invalid_filters_type_error(select_filters: tuple):
+    """
+    Test that the filterbank module cannot be instantiated with invalid filter indices raising a ValueError
+
+    Args:
+        select_filters (tuple): The indices of the filters to be used in the filter bank
+    """
+    from rockpool.devices.xylo.syns65302 import ChipButterworth
+
+    with pytest.raises(TypeError):
+        _ch = ChipButterworth(select_filters=select_filters)
