8a20a1a12d40562cab18c23167765ba8f64c5169
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-10-17 14:21:45 +0200

    Add submodules

--- rockpool/devices/xylo/syns65302/afe_sim.py
+++ rockpool/devices/xylo/syns65302/afe_sim.py
@@ -11,11 +11,15 @@ from __future__ import annotations
 from typing import Any, Dict, Optional, Tuple, Union
 import numpy as np
 
-
+from rockpool.devices.xylo.syns65302.afe.digital_filterbank import ChipButterworth
+from rockpool.devices.xylo.syns65302.afe.divisive_normalization import (
+    DivisiveNormalization,
+)
+from rockpool.devices.xylo.syns65302.afe.raster import Raster
 from rockpool.nn.modules.module import Module
 from rockpool.utilities.backend_management import backend_available
-
 from rockpool.parameters import SimulationParameter
+from rockpool.nn.combinators import Sequential
 
 if backend_available("samna"):
     print("from samna.xyloA3.configuration import InputInterfaceConfig")
@@ -40,13 +44,17 @@ class AFESim(Module):
     ) -> None:
         super().__init__(shape=shape, spiking_input=False, spiking_output=True)
 
+        self.filter_bank = ChipButterworth()
+        self.divisive_norm = DivisiveNormalization()
+        self.raster = Raster(shape=(self.size_out, self.size_out))
+        self.model = Sequential(self.filter_bank, self.divisive_norm, self.raster)
+
     def evolve(
         self, input_data: np.ndarray, record: bool = False
     ) -> Tuple[np.ndarray, Dict[str, Any], Dict[str, Any]]:
-        # - Auto batch and shape check
-        input_data, _ = self._auto_batch(input_data)
+        __out, __state, __rec = self.model.evolve(input_data, record=record)
 
-        return input_data, {}, {}
+        return __out, __state, __rec
 
     @classmethod
     def from_config(cls, config: Any) -> AFESim:
