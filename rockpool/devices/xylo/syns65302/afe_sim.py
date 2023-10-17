0e9574db7da9154c82b424f76dc1fe4ebd47bbf5
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-10-17 17:19:40 +0200

    Leave filter validation to FilterBank

--- rockpool/devices/xylo/syns65302/afe_sim.py
+++ rockpool/devices/xylo/syns65302/afe_sim.py
@@ -40,42 +40,28 @@ class AFESim(Module):
 
     def __init__(
         self,
-        select_filters: Tuple[int] = (
-            0,
-            1,
-            2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8,
-            9,
-            10,
-            11,
-            12,
-            13,
-            14,
-            15,
-        ),
+        select_filters: Optional[Tuple[int]] = None,
     ) -> None:
         """
         AFESim constructor
 
         Args:
-            select_filters (Tuple[int], optional): The indices of the filters to be used in the filter bank. Defaults to ( 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ).
+            select_filters (Optional[Tuple[int]], optional): The indices of the filters to be used in the filter bank. Defaults to None: use all filters.
+                i.e. select_filters = (0,2,4,8,15) will use Filter 0, Filter 2, Filter 4, Filter 8, and Filter 15.
         """
-        if self.validate_filter_selection(select_filters):
-            self.__select_filters = select_filters
-        else:
-            raise ValueError("Invalid filter selection.")
 
-        shape = (1, len(self.__select_filters))
-        super().__init__(shape=shape, spiking_input=False, spiking_output=True)
+        __filter_bank = ChipButterworth(select_filters=select_filters)
 
-        self.filter_bank = ChipButterworth()
+        super().__init__(
+            shape=__filter_bank.shape, spiking_input=False, spiking_output=True
+        )
+
+        self.filter_bank = __filter_bank
         self.divisive_norm = DivisiveNormalization()
-        self.raster = Raster(shape=(self.size_out, self.size_out))
+        self.raster = Raster(
+            shape=(self.filter_bank.size_out, self.filter_bank.size_out)
+        )
+
         self.model = Sequential(self.filter_bank, self.divisive_norm, self.raster)
 
     def evolve(
@@ -84,40 +70,6 @@ class AFESim(Module):
         __out, __state, __rec = self.model.evolve(input_data, record=record)
 
         return __out, __state, __rec
-
-    @staticmethod
-    def validate_filter_selection(select_filters: Tuple[int]) -> bool:
-        """
-        Validate the filter selection, check if the range and the values are valid.
-
-        Args:
-            select_filters (Tuple[int]): The indices of the filters to be used in the filter bank.
-
-        Raises:
-            TypeError: select_filters must be of type tuple.
-            TypeError: Filter index is not an integer.
-            ValueError: Filter index is out of range. Valid indices are between 0 and 15.
-            ValueError: All filter indices in select_filters must be unique.
-
-        Returns:
-            bool: True if the filter selection is valid.
-        """
-        if not isinstance(select_filters, tuple):
-            raise TypeError("select_filters must be of type tuple.")
-
-        # Check if all elements are integers and are within the allowed range
-        for filter_index in select_filters:
-            if not isinstance(filter_index, int):
-                raise TypeError(f"Filter index {filter_index} is not an integer.")
-            if filter_index < 0 or filter_index > 15:
-                raise ValueError(
-                    f"Filter index {filter_index} is out of range. Valid indices are between 0 and 15."
-                )
-
-        if len(select_filters) != len(set(select_filters)):
-            raise ValueError("All filter indices in select_filters must be unique.")
-
-        return True
 
     @classmethod
     def from_config(cls, config: Any) -> AFESim:
