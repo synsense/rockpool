c56c5e1570a5e18437f2dcb8e80b3b486fe03f53
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-10-18 15:29:19 +0200

    Improve docstring

--- rockpool/devices/xylo/syns65302/afe_sim.py
+++ rockpool/devices/xylo/syns65302/afe_sim.py
@@ -45,11 +45,12 @@ class AFESim(Module):
     def __init__(
         self,
         select_filters: Optional[Tuple[int]] = None,
+        spike_gen_mode: str = "divisive_norm",
         dn_rate_scale_bitshift: Tuple[int] = (6, 0),
         dn_low_pass_bitshift: int = 12,
-        dn_EPS: Union[int, List[int]] = 1,
-        fixed_threshold_vec: Union[int, List[int]] = 2 ** (14 - 1 + 8 + 6),
-        down_sampling_factor: int = 6,
+        dn_EPS: Union[int, Tuple[int]] = 1,
+        fixed_threshold_vec: Union[int, Tuple[int]] = 2**27,
+        down_sampling_factor: int = 50,
     ) -> None:
         """
         AFESim constructor
@@ -57,6 +58,11 @@ class AFESim(Module):
         Args:
             select_filters (Optional[Tuple[int]], optional): The indices of the filters to be used in the filter bank. Defaults to None: use all filters.
                 i.e. select_filters = (0,2,4,8,15) will use Filter 0, Filter 2, Filter 4, Filter 8, and Filter 15.
+
+            spike_gen_mode (str, optional): The spike generation mode of the AFE. There are two ways to generate spikes, "divisive_norm" and "threshold". Defaults to "divisive_norm".
+                When "divisive_norm" is selected, adaptive thresholds apply, and `dn_rate_scale_bitshift`, `dn_low_pass_bitshift`, `dn_EPS` parameters are used.
+                When "threshold" is selected, fixed thresholds apply, and `fixed_threshold_vec` parameter is used.
+                For detailed information, please check `DivisiveNormalization` module
 
             dn_rate_scale_bitshift (Tuple[int], optional):  A tuple containing two bitshift values that determine how much the spike rate should be scaled compared with the sampling rate of the input audio. The first value is `b1` and the second is `b2`. Defaults to (6, 0).
                 A bitshift of size specified by the tuple as `(b1, b2)` yields a spike rate scaling of fs/(2^b1 - 2^b2) where fs is the sampling rate of the input audio.
@@ -67,10 +73,33 @@ class AFESim(Module):
                 The default value of 12, implies an averaging window of size 4096 clock periods. For an audio of clock rate 50K, this yields an averaging window of size 80 ms.
                 Use `.from_specification()` method to perform a parameter search for b values given the target averaging window size.
 
-            dn_EPS (Union[int, np.ndarray]): lower bound on spike generation threshold. Defaults to 1.
+            dn_EPS (Union[int, Tuple[int]]): lower bound on spike generation threshold. Defaults to 1.
                 Using this parameter we can control the noise level in the sense that if average power in a channel is less than EPS, the spike rate of that channel is somehow diminished during spike generation.
 
+            fixed_threshold_vec (Union[int, Tuple[int]]): A tuple containing threshold values per channel which determine the spike generation threshold. Defaults to 2 ** (27) = 2 ** (14 - 1 + 8 + 6).
+                Thresholds of size `size_out`, in case of a singular value, broadcasted. These thresholds are used only when the `spike_gen_mode = "threshold"`.
+                The default value 2**27 is to ensure a spike rate of around 1K for an input sinusoid signal quantized to 14 bits.
+
+                .. seealso::
+                    How to set the value of threshold for a target spike rate?
+
+                    In the current implementation, input audio to filters has 14 bits which is further left-bit-shifted by 8 bits to improve numerical precision, thus, 22 bits.
+                    This implies that the output signal may have a maximum amplitude of at most `2^21 - 1 ~ 2^22`, for example, when fed by a sinusoid signal
+                    within the passband of the filter.
+                    For a target rate of around 1K. e.g., 1 spike every 50 clock period for an audio of sampling rate 50K, then we need to choose a threshold as large as
+                    `50 x 2^22 ~ 2^27`.
+
+            down_sampling_factor (int): The down-sampling factor of the raster module. Determines how many time-steps will be accumulated into a single time-step. Defaults to 50.
+                Resulting dt = 0.001024
         """
+
+        if spike_gen_mode not in ["divisive_norm", "threshold"]:
+            raise ValueError(
+                f"Invalid spike_gen_mode: {spike_gen_mode}. Valid options are: 'divisive_norm' and 'threshold'"
+            )
+
+        else:
+            enable_DN_channel = True if spike_gen_mode == "divisive_norm" else False
 
         __filter_bank = ChipButterworth(select_filters=select_filters)
 
@@ -81,8 +110,10 @@ class AFESim(Module):
         __sub_shape = (__filter_bank.size_out, __filter_bank.size_out)
 
         self.filter_bank = __filter_bank
+
         self.divisive_norm = DivisiveNormalization(
             shape=__sub_shape,
+            enable_DN_channel=enable_DN_channel,
             spike_rate_scale_bitshift1=dn_rate_scale_bitshift[0],
             spike_rate_scale_bitshift2=dn_rate_scale_bitshift[1],
             low_pass_bitshift=dn_low_pass_bitshift,
@@ -92,13 +123,14 @@ class AFESim(Module):
         )
         self.raster = Raster(
             shape=__sub_shape,
-            rate_downsample_factor=2**down_sampling_factor,
+            rate_downsample_factor=down_sampling_factor,
             max_num_spikes=MAX_SPIKES_INPUT,
             fs=AUDIO_SAMPLING_RATE,
         )
 
         self.model = Sequential(self.filter_bank, self.divisive_norm, self.raster)
-        self.dt = SimulationParameter((2**down_sampling_factor) / AUDIO_SAMPLING_RATE)
+        self.dt = SimulationParameter((down_sampling_factor) / AUDIO_SAMPLING_RATE)
+        self.spike_gen_mode = spike_gen_mode
 
     def evolve(
         self, input_data: np.ndarray, record: bool = False
