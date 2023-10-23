1cf278453293d2cfc33289d53d8475f6f48ebdd8
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-10-23 12:47:14 +0200

    Add docstring to tests

--- tests/tests_default/devices_xylo_a3/test_afesim_a3.py
+++ tests/tests_default/devices_xylo_a3/test_afesim_a3.py
@@ -2,6 +2,8 @@ import pytest
 
 
 def test_import():
+    """Test that the AFESim module can be imported"""
+
     pytest.importorskip("samna")
     from rockpool.devices.xylo.syns65302 import AFESim
 
@@ -12,7 +14,16 @@ def test_import():
     "rate_scale_factor,rate_scale_bitshift",
     [(1, (1, 0)), (16, (5, 4)), (32, (6, 5)), (63, (6, 0))],
 )
-def test_dn_rate_scale_bitshift_known_feasible(rate_scale_factor, rate_scale_bitshift):
+def test_dn_rate_scale_bitshift_known_feasible(
+    rate_scale_factor: int, rate_scale_bitshift: tuple
+) -> None:
+    """
+    Test that the dn_rate_scale_bitshift is computed correctly for known feasible values
+
+    Args:
+        rate_scale_factor (float): Target `rate_scale_factor` for the `DivisiveNormalization` module.
+        rate_scale_bitshift (tuple): Expected `rate_scale_bitshift` calculated given the target `rate_scale_factor`.
+    """
     pytest.importorskip("samna")
     from rockpool.devices.xylo.syns65302 import AFESim
 
@@ -21,7 +32,13 @@ def test_dn_rate_scale_bitshift_known_feasible(rate_scale_factor, rate_scale_bit
 
 
 @pytest.mark.parametrize("rate_scale_factor", [-10, "a", 0, 5, 61])
-def test_dn_rate_scale_bitshift_known_raising_error(rate_scale_factor):
+def test_dn_rate_scale_bitshift_known_raising_error(rate_scale_factor: int):
+    """
+    Test that the dn_rate_scale_bitshift raises a ValueError for known infeasible values
+
+    Args:
+        rate_scale_factor (int): Target `rate_scale_factor` for the `DivisiveNormalization` module.
+    """
     pytest.importorskip("samna")
     from rockpool.devices.xylo.syns65302 import AFESim
 
