145c1ec9817a3ff700c389bfbcae487f972c43fb
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-10-23 12:36:20 +0200

    Test `get_dn_rate_scale_bitshift`

--- tests/tests_default/devices_xylo_a3/test_afesim_a3.py
+++ tests/tests_default/devices_xylo_a3/test_afesim_a3.py
@@ -6,3 +6,24 @@ def test_import():
     from rockpool.devices.xylo.syns65302 import AFESim
 
     assert AFESim is not None
+
+
+@pytest.mark.parametrize(
+    "rate_scale_factor,rate_scale_bitshift",
+    [(1, (1, 0)), (16, (5, 4)), (32, (6, 5)), (63, (6, 0))],
+)
+def test_dn_rate_scale_bitshift_known_feasible(rate_scale_factor, rate_scale_bitshift):
+    pytest.importorskip("samna")
+    from rockpool.devices.xylo.syns65302 import AFESim
+
+    bitshift = AFESim.get_dn_rate_scale_bitshift(rate_scale_factor=rate_scale_factor)
+    assert bitshift == rate_scale_bitshift
+
+
+@pytest.mark.parametrize("rate_scale_factor", [-10, "a", 0, 5, 61])
+def test_dn_rate_scale_bitshift_known_raising_error(rate_scale_factor):
+    pytest.importorskip("samna")
+    from rockpool.devices.xylo.syns65302 import AFESim
+
+    with pytest.raises(ValueError):
+        AFESim.get_dn_rate_scale_bitshift(rate_scale_factor=rate_scale_factor)
