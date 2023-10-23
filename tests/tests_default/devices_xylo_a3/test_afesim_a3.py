e378cec473cee689f0f710dcae09e8b5446adf31
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-10-23 14:34:38 +0200

    Add unit tests for `get_dn_low_pass_bitshift`

--- tests/tests_default/devices_xylo_a3/test_afesim_a3.py
+++ tests/tests_default/devices_xylo_a3/test_afesim_a3.py
@@ -18,7 +18,7 @@ def test_dn_rate_scale_bitshift_known_feasible(
     rate_scale_factor: int, rate_scale_bitshift: tuple
 ) -> None:
     """
-    Test that the dn_rate_scale_bitshift is computed correctly for known feasible values
+    Test that the `dn_rate_scale_bitshift` is computed correctly for known feasible values
 
     Args:
         rate_scale_factor (float): Target `rate_scale_factor` for the `DivisiveNormalization` module.
@@ -34,7 +34,7 @@ def test_dn_rate_scale_bitshift_known_feasible(
 @pytest.mark.parametrize("rate_scale_factor", [-10, "a", 0, 5, 61])
 def test_dn_rate_scale_bitshift_known_raising_error(rate_scale_factor: int):
     """
-    Test that the dn_rate_scale_bitshift raises a ValueError for known infeasible values
+    Test that the `get_dn_rate_scale_bitshift` raises a `ValueError` for known infeasible values
 
     Args:
         rate_scale_factor (int): Target `rate_scale_factor` for the `DivisiveNormalization` module.
@@ -44,3 +44,41 @@ def test_dn_rate_scale_bitshift_known_raising_error(rate_scale_factor: int):
 
     with pytest.raises(ValueError):
         AFESim.get_dn_rate_scale_bitshift(rate_scale_factor=rate_scale_factor)
+
+
+@pytest.mark.parametrize(
+    "low_pass_averaging_window,low_pass_bitshift",
+    [(4e-5, 1), (1e-3, 6), (20e-3, 10), (84e-3, 12)],
+)
+def test_dn_low_pass_bitshift(low_pass_averaging_window: float, low_pass_bitshift: int):
+    """
+    Test that the `dn_low_pass_bitshift` is computed correctly for known feasible values
+
+    Args:
+        low_pass_averaging_window (float): Target `low_pass_averaging_window` for the `DivisiveNormalization` module.
+        low_pass_bitshift (int): Expected `low_pass_bitshift` calculated given the target `low_pass_averaging_window`.
+    """
+    pytest.importorskip("samna")
+    from rockpool.devices.xylo.syns65302 import AFESim
+
+    bitshift = AFESim.get_dn_low_pass_bitshift(
+        low_pass_averaging_window=low_pass_averaging_window
+    )
+    assert bitshift == low_pass_bitshift
+
+
+@pytest.mark.parametrize("low_pass_averaging_window", [-10, 1e-6, "a", 85e-3])
+def test_dn_low_pass_bitshift_known_raising_error(low_pass_averaging_window: int):
+    """
+    Test that the `get_dn_low_pass_bitshift` raises a `ValueError` for known infeasible values
+
+    Args:
+        low_pass_averaging_window (int): Target `low_pass_averaging_window` for the `DivisiveNormalization` module.
+    """
+    pytest.importorskip("samna")
+    from rockpool.devices.xylo.syns65302 import AFESim
+
+    with pytest.raises(ValueError):
+        AFESim.get_dn_low_pass_bitshift(
+            low_pass_averaging_window=low_pass_averaging_window
+        )
