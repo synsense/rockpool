bca50d89d72b28ef7764e2e10ce021a6fb9c9144
ugurcan.cakal <ugurcan.cakal@synsense.ai>
2023-12-15 15:01:35 +0100

    Minor fixes

--- rockpool/devices/xylo/syns65302/__init__.py
+++ rockpool/devices/xylo/syns65302/__init__.py
@@ -1,5 +1,6 @@
 """
-Package to support the Xylo HW SYNS65302 (Xylo™ Audio v3)
+Package to support the Xylo HW SYNS65302 (Xylo™ Audio 3)
+
 Includes simulation, interfacing and deployment modules.
 
 Provides the modules :py:class:`.AFESimExternal`, :py:class:`.AFESimAGC`, and :py:class:`.AFESimPDM`.
