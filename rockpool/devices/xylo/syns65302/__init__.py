cbd33ac9f34e4ca65e8e96b59631dc738bd54325
Vanessa Leite <vanessa.leite@synsense.ai>
2024-03-21 16:18:16 +0100

    Add classes to init to fix broken tests

--- rockpool/devices/xylo/syns65302/__init__.py
+++ rockpool/devices/xylo/syns65302/__init__.py
@@ -8,9 +8,23 @@ Provides the modules :py:class:`.AFESimExternal`, :py:class:`.AFESimAGC`, and :p
 
 from .afe.params import *
 from .afe_sim import *
-from .xylo_samna import *
 from .xylo_mapper import *
 from .xylo_samna_pdm import *
-from .xylo_monitor import *
-
 from ..syns63300 import XyloSim
+
+try:
+    from .xylo_samna import *
+    from .xylo_a3_devkit_utils import *
+    from .xylo_monitor import *
+except:
+    if not backend_available("samna"):
+        AFESamna = missing_backend_shim("AFESamna", "samna")
+        XyloSamna = missing_backend_shim("XyloSamna", "samna")
+        XyloMonitor = missing_backend_shim("XyloSamna", "samna")
+        config_from_specification = missing_backend_shim(
+            "config_from_specification", "samna"
+        )
+        save_config = missing_backend_shim("save_config", "samna")
+        load_config = missing_backend_shim("load_config", "samna")
+    else:
+        raise
