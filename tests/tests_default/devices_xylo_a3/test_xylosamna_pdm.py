3686ba0b7422b9d89eafed891cfe4ba80455a301
Vanessa Leite <vanessa.leite@synsense.ai>
2024-11-29 06:22:55 +0100

    Add test with power measurement
    Delete XyloSamnaPDM instances when they are not in use anymore

--- tests/tests_default/devices_xylo_a3/test_xylosamna_pdm.py
+++ tests/tests_default/devices_xylo_a3/test_xylosamna_pdm.py
@@ -14,7 +14,6 @@ def test_imports():
     import rockpool.devices.xylo.syns65302.xa3_devkit_utils as putils
 
 
-@pytest.mark.skip(reason="Needs version of samna 0.39.5.7 that is not out yet")
 def test_xylosamna_pdm():
     import pytest
 
@@ -26,14 +25,12 @@ def test_xylosamna_pdm():
         mapper,
     )
     import rockpool.devices.xylo.syns65302.xa3_devkit_utils as putils
-    from rockpool import TSEvent, TSContinuous
     from rockpool.nn.combinators import Sequential
     from rockpool.nn.modules import LIF, Linear
     from rockpool.transform.quantize_methods import channel_quantize
 
     import numpy as np
     import samna
-    import os
 
     # - Get a Xylo HDK board
     xylo_hdk_nodes = putils.find_xylo_a3_boards()
@@ -70,7 +67,9 @@ def test_xylosamna_pdm():
 
     input_pdm = np.loadtxt("tests/tests_default/models/xylo_a3_input_pdm.txt")
 
-    out, _, rd = xmod(input_pdm, record=True)
+    _, _, rd = xmod(input_pdm, record=True)
+    del xmod
+
     dur = 200e-3
     # result of the same data simulation
     a = np.loadtxt("tests/tests_default/models/xylo_a3_afe_sim_pdm_output.txt")
@@ -82,3 +81,82 @@ def test_xylosamna_pdm():
 
     # some error marging accepted
     assert np.all(b)
+
+
+def test_xylosamna_pdm_with_power():
+    import pytest
+
+    pytest.importorskip("samna")
+
+    from rockpool.devices.xylo.syns65302 import (
+        XyloSamnaPDM,
+        config_from_specification,
+        mapper,
+    )
+    import rockpool.devices.xylo.syns65302.xa3_devkit_utils as putils
+    from rockpool.nn.combinators import Sequential
+    from rockpool.nn.modules import LIF, Linear
+    from rockpool.transform.quantize_methods import channel_quantize
+
+    import numpy as np
+    import samna
+
+    # - Get a Xylo HDK board
+    xylo_hdk_nodes = putils.find_xylo_a3_boards()
+
+    if len(xylo_hdk_nodes) == 0:
+        pytest.skip("A connected XyloAudio 3 HDK is required to run this test")
+
+    daughterboard = xylo_hdk_nodes[0]
+
+    # create a network
+    net = Sequential(
+        Linear((16, 63)),
+        LIF((63, 63)),
+        Linear((63, 32)),
+        LIF(32),
+    )
+
+    net[0].weight *= 0.05
+
+    spec = mapper(net.as_graph())
+    Q_spec = spec
+    Q_spec.update(channel_quantize(**Q_spec))
+    config, is_valid, msg = config_from_specification(**Q_spec)
+
+    if not is_valid:
+        print(msg)
+
+    # - Create a Xylo module with PDM input
+    dn = True
+    config.operation_mode = samna.xyloAudio3.OperationMode.AcceleratedTime
+
+    xmod_spike = XyloSamnaPDM(daughterboard, config, dt=1024e-6, dn_active=dn)
+    assert xmod_spike != None
+
+    input_pdm = np.loadtxt("tests/tests_default/models/xylo_a3_input_pdm.txt")
+
+    _, _, rd_spike = xmod_spike(input_pdm, record=True, record_power=True)
+    del xmod_spike
+
+    dur = 200e-3
+    # result of the same data simulation
+    a = np.loadtxt("tests/tests_default/models/xylo_a3_afe_sim_pdm_output.txt")
+    b = np.sum(rd_spike["Spikes_in"].T, axis=1) / dur
+
+    result = [abs(i - j) / i for i, j in zip(a, b)]
+
+    b = np.array([element < 0.05 for element in result])
+
+    # some error marging accepted
+    assert np.all(b)
+
+    xmod_vmem = XyloSamnaPDM(
+        daughterboard, config, dt=1024e-6, dn_active=dn, output_mode="Vmem"
+    )
+    assert xmod_vmem != None
+    _, _, rd_vmem = xmod_vmem(input_pdm, record=True, record_power=True)
+    del xmod_vmem
+
+    # We expected the power measured in vmem to be higher because more memories are active
+    assert np.mean(rd_vmem["digital_power"]) > np.mean(rd_spike["digital_power"])
