143626cc9a90d7e11aa885fafb115ba8c55eb916
Vanessa Leite <vanessa.leite@synsense.ai>
2025-07-01 18:26:00 +0200

    Update tests to remove deletion of classes
    This step is not pythonic and was introduced as a way to deal with
    a previous issue

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup_args = {
         ],
         "xylo": [
             "xylosim",
-            "samna>=0.46.0",
+            "samna>=0.46.1",
             "bitstruct",
         ],
         "exodus": [
