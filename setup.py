31cd7933011a3423f50589c6069ac67fcf97996b
Vanessa Leite <vanessa.leite@synsense.ai>
2026-06-17 16:02:47 +0200

    Needs unreleased version of samna
    Samna also had missing code for second synapse for XyloA3.
    Samna code was not merged yet but should be soon.
    Rockpool CI should fail until the release.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup_args = {
         ],
         "xylo": [
             "xylosim",
-            "samna>=0.46.1",
+            "samna>=0.48.6",
             "bitstruct",
             "rockpool[jax]",
             "packaging",
