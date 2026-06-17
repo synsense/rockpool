363e6a7db68b0956a0965d14967a567de3ead265
Vanessa Leite <vanessa.leite@synsense.ai>
2026-06-17 14:15:21 +0200

    Separate and fix tests for isyn2
    We need to keep the tests for the networks without second synapse as they were
    and we need to add tests for the ones with second synapse

--- tests/tests_default/devices_xylo_a3/test_xylosamna.py
+++ tests/tests_default/devices_xylo_a3/test_xylosamna.py
@@ -45,7 +45,6 @@ def test_XyloSamna():
         dash_mem=2 * np.ones(Nhidden),
         dash_mem_out=3 * np.ones(Nout),
         dash_syn=4 * np.ones(Nhidden),
-        dash_syn_2=2 * np.ones(Nhidden, dtype=int),
         dash_syn_out=3 * np.ones(Nout),
         threshold=128 * np.ones(Nhidden),
         threshold_out=256 * np.ones(Nout),
@@ -54,6 +53,10 @@ def test_XyloSamna():
         weight_shift_out=1,
         aliases=None,
     )
+
+    if not valid:
+        print(msg)
+        assert False
 
     config.input_source = samna.xyloAudio3.InputSource.SpikeEvents
     config.operation_mode = samna.xyloAudio3.OperationMode.AcceleratedTime
@@ -70,16 +73,12 @@ def test_XyloSamna():
     assert modXyloSamna != None
     assert modXyloSamna._output_mode == "Vmem"
 
-    del modXyloSamna
-
     # - Make a XyloSamna module for Isyn
     modXyloSamna = XyloSamna(device=daughterboard, config=config, output_mode="Isyn")
     output_ts, _, _ = modXyloSamna.evolve(input_spikes, record=True)
 
     assert modXyloSamna != None
     assert modXyloSamna._output_mode == "Isyn"
-
-    del modXyloSamna
 
     # - Make a XyloSamna module for Spike
     modXyloSamna = XyloSamna(device=daughterboard, config=config)
@@ -88,7 +87,80 @@ def test_XyloSamna():
     assert modXyloSamna != None
     assert modXyloSamna._output_mode == "Spike"
 
-    del modXyloSamna
+
+def test_XyloSamna_isyn2():
+    import pytest
+
+    pytest.importorskip("samna")
+
+    from rockpool.devices.xylo.syns65302 import XyloSamna, config_from_specification
+    import rockpool.devices.xylo.syns65302.xa3_devkit_utils as putils
+
+    import numpy as np
+    import samna
+
+    # - Get a Xylo HDK board
+    xylo_hdk_nodes = putils.find_xylo_a3_boards()
+
+    if len(xylo_hdk_nodes) == 0:
+        pytest.skip("A connected XyloAudio 3 HDK is required to run this test")
+
+    daughterboard = xylo_hdk_nodes[0]
+
+    # - Make a Xylo configuration
+    Nin = 3
+    Nhidden = 5
+    Nout = 2
+
+    config, valid, msg = config_from_specification(
+        weights_in=np.random.uniform(-127, 127, size=(Nin, Nhidden, 2)),
+        weights_out=np.random.uniform(-127, 127, size=(Nhidden, Nout)),
+        weights_rec=np.random.uniform(-127, 127, size=(Nhidden, Nhidden, 2)),
+        dash_mem=2 * np.ones(Nhidden),
+        dash_mem_out=3 * np.ones(Nout),
+        dash_syn=4 * np.ones(Nhidden),
+        dash_syn_2=2 * np.ones(Nhidden, dtype=int),
+        dash_syn_out=3 * np.ones(Nout),
+        threshold=128 * np.ones(Nhidden),
+        threshold_out=256 * np.ones(Nout),
+        weight_shift_in=1,
+        weight_shift_rec=1,
+        weight_shift_out=1,
+        aliases=None,
+    )
+
+    if not valid:
+        print(msg)
+        assert False
+
+    config.input_source = samna.xyloAudio3.InputSource.SpikeEvents
+    config.operation_mode = samna.xyloAudio3.OperationMode.AcceleratedTime
+
+    # - Simulate with random input
+    T = 100
+    f = 0.4
+    input_spikes = np.random.rand(T, Nin) < f
+
+    # - Make a XyloSamna module for Vmem
+    modXyloSamna = XyloSamna(device=daughterboard, config=config, output_mode="Vmem")
+    output_ts, _, _ = modXyloSamna.evolve(input_spikes, record=True)
+
+    assert modXyloSamna != None
+    assert modXyloSamna._output_mode == "Vmem"
+
+    # - Make a XyloSamna module for Isyn
+    modXyloSamna = XyloSamna(device=daughterboard, config=config, output_mode="Isyn")
+    output_ts, _, _ = modXyloSamna.evolve(input_spikes, record=True)
+
+    assert modXyloSamna != None
+    assert modXyloSamna._output_mode == "Isyn"
+
+    # - Make a XyloSamna module for Spike
+    modXyloSamna = XyloSamna(device=daughterboard, config=config)
+    output_ts, _, _ = modXyloSamna.evolve(input_spikes, record=True)
+
+    assert modXyloSamna != None
+    assert modXyloSamna._output_mode == "Spike"
 
 
 def test_XyloSamna_power():
@@ -122,6 +194,83 @@ def test_XyloSamna_power():
         dash_mem=2 * np.ones(Nhidden),
         dash_mem_out=3 * np.ones(Nout),
         dash_syn=4 * np.ones(Nhidden),
+        dash_syn_out=3 * np.ones(Nout),
+        threshold=128 * np.ones(Nhidden),
+        threshold_out=256 * np.ones(Nout),
+        weight_shift_in=1,
+        weight_shift_rec=1,
+        weight_shift_out=1,
+        aliases=None,
+    )
+
+    if not valid:
+        print(msg)
+        assert False
+
+    config.input_source = samna.xyloAudio3.InputSource.SpikeEvents
+    config.operation_mode = samna.xyloAudio3.OperationMode.AcceleratedTime
+
+    # - Simulate with random input
+    T = 100
+    f = 0.4
+    input_spikes = np.random.rand(T, Nin) < f
+
+    # - Make a XyloSamna module for Vmem
+    modXyloSamna = XyloSamna(device=daughterboard, config=config, output_mode="Vmem")
+    _, _, rd = modXyloSamna.evolve(input_spikes, record=True, record_power=True)
+
+    assert modXyloSamna != None
+    assert modXyloSamna._output_mode == "Vmem"
+    assert "io_power" in rd
+
+    # - Make a XyloSamna module for Isyn
+    modXyloSamna = XyloSamna(device=daughterboard, config=config, output_mode="Isyn")
+    _, _, rd = modXyloSamna.evolve(input_spikes, record=True, record_power=True)
+
+    assert modXyloSamna != None
+    assert modXyloSamna._output_mode == "Isyn"
+    assert "io_power" in rd
+
+    # - Make a XyloSamna module for Spike
+    modXyloSamna = XyloSamna(device=daughterboard, config=config)
+    _, _, rd = modXyloSamna.evolve(input_spikes, record=True, record_power=True)
+
+    assert modXyloSamna != None
+    assert modXyloSamna._output_mode == "Spike"
+    assert "io_power" in rd
+
+
+def test_XyloSamna_power_isyn2():
+    import pytest
+
+    pytest.importorskip("samna")
+
+    from rockpool.devices.xylo.syns65302 import XyloSamna, config_from_specification
+    import rockpool.devices.xylo.syns65302.xa3_devkit_utils as putils
+
+    import numpy as np
+    import samna
+
+    # - Get a Xylo HDK board
+    xylo_hdk_nodes = putils.find_xylo_a3_boards()
+
+    if len(xylo_hdk_nodes) == 0:
+        pytest.skip("A connected XyloAudio 3 HDK is required to run this test")
+
+    daughterboard = xylo_hdk_nodes[0]
+
+    # - Make a Xylo configuration
+    Nin = 3
+    Nhidden = 5
+    Nout = 2
+
+    config, valid, msg = config_from_specification(
+        weights_in=np.random.uniform(-127, 127, size=(Nin, Nhidden, 2)),
+        weights_out=np.random.uniform(-127, 127, size=(Nhidden, Nout)),
+        weights_rec=np.random.uniform(-127, 127, size=(Nhidden, Nhidden, 2)),
+        dash_mem=2 * np.ones(Nhidden),
+        dash_mem_out=3 * np.ones(Nout),
+        dash_syn=4 * np.ones(Nhidden),
         dash_syn_2=2 * np.ones(Nhidden, dtype=int),
         dash_syn_out=3 * np.ones(Nout),
         threshold=128 * np.ones(Nhidden),
@@ -131,6 +280,10 @@ def test_XyloSamna_power():
         weight_shift_out=1,
         aliases=None,
     )
+
+    if not valid:
+        print(msg)
+        assert False
 
     config.input_source = samna.xyloAudio3.InputSource.SpikeEvents
     config.operation_mode = samna.xyloAudio3.OperationMode.AcceleratedTime
@@ -198,6 +351,10 @@ def test_save_load():
         weight_shift_out=1,
         aliases=None,
     )
+
+    if not valid:
+        print(msg)
+        assert False
 
     save_config(config, "../test_samna_config.json")
     conf2 = load_config("../test_samna_config.json")
@@ -275,7 +432,6 @@ def test_xylo_vs_xylosim_acceleratedtime():
         dash_mem=2 * np.ones(Nhidden),
         dash_mem_out=3 * np.ones(Nout),
         dash_syn=4 * np.ones(Nhidden),
-        dash_syn_2=2 * np.ones(Nhidden),
         dash_syn_out=3 * np.ones(Nout),
         threshold=128 * np.ones(Nhidden),
         threshold_out=256 * np.ones(Nout),
@@ -284,6 +440,10 @@ def test_xylo_vs_xylosim_acceleratedtime():
         weight_shift_out=1,
         aliases=None,
     )
+
+    if not valid:
+        print(msg)
+        assert False
 
     config.input_source = samna.xyloAudio3.InputSource.SpikeEvents
     config.operation_mode = samna.xyloAudio3.OperationMode.AcceleratedTime
@@ -334,6 +494,98 @@ def test_xylo_vs_xylosim_acceleratedtime():
     assert np.all(rec_sim["Spikes"] == rec_xylo["Spikes"])
 
 
+@pytest.mark.skip("XyloSim is using XyloIMUSim and doesnt have isyn2")
+def test_xylo_vs_xylosim_acceleratedtime_isyn2():
+    import pytest
+
+    pytest.importorskip("samna")
+    pytest.importorskip("xylosim")
+
+    # - Samna imports
+    import samna
+
+    from rockpool.devices.xylo.syns65302 import xa3_devkit_utils as putils
+    import rockpool.devices.xylo.syns65302 as x
+
+    import numpy as np
+
+    # Make a Xylo configuration
+    Nin = 3
+    Nhidden = 5
+    Nout = 2
+    T = 1000
+
+    config, valid, msg = x.config_from_specification(
+        weights_in=np.random.uniform(-127, 127, size=(Nin, Nhidden, 2)),
+        weights_out=np.random.uniform(-127, 127, size=(Nhidden, Nout)),
+        weights_rec=np.random.uniform(-127, 127, size=(Nhidden, Nhidden, 2)),
+        dash_mem=2 * np.ones(Nhidden),
+        dash_mem_out=3 * np.ones(Nout),
+        dash_syn=4 * np.ones(Nhidden),
+        dash_syn_2=2 * np.ones(Nhidden),
+        dash_syn_out=3 * np.ones(Nout),
+        threshold=128 * np.ones(Nhidden),
+        threshold_out=256 * np.ones(Nout),
+        weight_shift_in=1,
+        weight_shift_rec=1,
+        weight_shift_out=1,
+        aliases=None,
+    )
+
+    if not valid:
+        print(msg)
+        assert False
+
+    config.input_source = samna.xyloAudio3.InputSource.SpikeEvents
+    config.operation_mode = samna.xyloAudio3.OperationMode.AcceleratedTime
+
+    # - Create XyloSim object
+    mod_xylo_sim_vmem = x.XyloSim.from_config(config, output_mode="Vmem", dt=1.0 / 200)
+    mod_xylo_sim_isyn = x.XyloSim.from_config(config, output_mode="Isyn", dt=1.0 / 200)
+    mod_xylo_sim_spike = x.XyloSim.from_config(config, dt=1.0 / 200)
+    mod_xylo_sim_vmem.timed()
+    mod_xylo_sim_isyn.timed()
+    mod_xylo_sim_spike.timed()
+
+    # - Generate random input
+    input_raster = np.random.randint(0, 16, (T, Nin))
+
+    # - Simulate the evolution of the network on Xylo
+    # mod_xylo_sim_spike.reset_state()
+    out_sim, _, rec_sim = mod_xylo_sim_spike.evolve(
+        input_raster.clip(0, 15), record=True
+    )
+
+    # - Get a Xylo HDK board
+    xylo_hdk_nodes = putils.find_xylo_a3_boards()
+
+    if len(xylo_hdk_nodes) == 0:
+        pytest.skip("A connected XyloAudio 3 HDK is required to run this test")
+
+    daughterboard = xylo_hdk_nodes[0]
+
+    # - Make sure the board is in a clean state after running other tests
+    daughterboard.reset_board_soft()
+
+    # - Init Xylo
+    mod_xylo_spike = x.XyloSamna(daughterboard, config)
+
+    print(len(input_raster))
+
+    # - Evolve Xylo
+    mod_xylo_spike.reset_state()
+    out_xylo, _, rec_xylo = mod_xylo_spike.evolve(input_raster, record=True)
+
+    # - Assert equality for all outputs and recordings
+    assert np.all(out_sim == out_xylo)
+    assert np.all(rec_sim["Vmem"] == rec_xylo["Vmem"])
+    assert np.all(rec_sim["Isyn"] == rec_xylo["Isyn"])
+    assert np.all(rec_sim["Isyn2"] == rec_xylo["Isyn2"])
+    assert np.all(rec_sim["Vmem_out"] == rec_xylo["Vmem_out"])
+    assert np.all(rec_sim["Isyn_out"] == rec_xylo["Isyn_out"])
+    assert np.all(rec_sim["Spikes"] == rec_xylo["Spikes"])
+
+
 def test_xylo_vs_xylosim_manual():
     import pytest
 
@@ -361,7 +613,6 @@ def test_xylo_vs_xylosim_manual():
         dash_mem=2 * np.ones(Nhidden),
         dash_mem_out=3 * np.ones(Nout),
         dash_syn=4 * np.ones(Nhidden),
-        dash_syn_2=2 * np.ones(Nhidden, dtype=int),
         dash_syn_out=3 * np.ones(Nout),
         threshold=128 * np.ones(Nhidden),
         threshold_out=256 * np.ones(Nout),
@@ -370,6 +621,10 @@ def test_xylo_vs_xylosim_manual():
         weight_shift_out=1,
         aliases=None,
     )
+
+    if not valid:
+        print(msg)
+        assert False
 
     config.input_source = samna.xyloAudio3.InputSource.SpikeEvents
     config.operation_mode = samna.xyloAudio3.OperationMode.Manual
@@ -415,6 +670,98 @@ def test_xylo_vs_xylosim_manual():
     # FIXME: These values are shifited by 1 event!
     # assert np.all(rec_sim["Vmem"] == rec_xylo["Vmem"])
     # assert np.all(rec_sim["Isyn"] == rec_xylo["Isyn"])
+    # assert np.all(rec_sim["Vmem_out"] == rec_xylo["Vmem_out"])
+    # assert np.all(rec_sim["Isyn_out"] == rec_xylo["Isyn_out"])
+    # assert np.all(rec_sim["Spikes"] == rec_xylo["Spikes"])
+
+
+@pytest.mark.skip("XyloSim is using XyloIMUSim and doesnt have isyn2")
+def test_xylo_vs_xylosim_manual_isyn2():
+    import pytest
+
+    pytest.importorskip("samna")
+    pytest.importorskip("xylosim")
+
+    # - Samna imports
+    import samna
+
+    from rockpool.devices.xylo.syns65302 import xa3_devkit_utils as putils
+    import rockpool.devices.xylo.syns65302 as x
+
+    import numpy as np
+
+    # Make a Xylo configuration
+    Nin = 3
+    Nhidden = 5
+    Nout = 2
+    T = 1000
+
+    config, valid, msg = x.config_from_specification(
+        weights_in=np.random.uniform(-127, 127, size=(Nin, Nhidden, 2)),
+        weights_out=np.random.uniform(-127, 127, size=(Nhidden, Nout)),
+        weights_rec=np.random.uniform(-127, 127, size=(Nhidden, Nhidden, 2)),
+        dash_mem=2 * np.ones(Nhidden),
+        dash_mem_out=3 * np.ones(Nout),
+        dash_syn=4 * np.ones(Nhidden),
+        dash_syn_2=2 * np.ones(Nhidden, dtype=int),
+        dash_syn_out=3 * np.ones(Nout),
+        threshold=128 * np.ones(Nhidden),
+        threshold_out=256 * np.ones(Nout),
+        weight_shift_in=1,
+        weight_shift_rec=1,
+        weight_shift_out=1,
+        aliases=None,
+    )
+
+    if not valid:
+        print(msg)
+        assert False
+
+    config.input_source = samna.xyloAudio3.InputSource.SpikeEvents
+    config.operation_mode = samna.xyloAudio3.OperationMode.Manual
+
+    # - Create XyloSim object
+    mod_xylo_sim_vmem = x.XyloSim.from_config(config, output_mode="Vmem", dt=1.0 / 200)
+    mod_xylo_sim_isyn = x.XyloSim.from_config(config, output_mode="Isyn", dt=1.0 / 200)
+    mod_xylo_sim_spike = x.XyloSim.from_config(config, dt=1.0 / 200)
+    mod_xylo_sim_vmem.timed()
+    mod_xylo_sim_isyn.timed()
+    mod_xylo_sim_spike.timed()
+
+    # - Generate random input
+    input_raster = np.random.randint(0, 16, (T, Nin))
+
+    # - Simulate the evolution of the network on Xylo
+    # TODO: reset state is not working yet
+    # mod_xylo_sim_spike.reset_state()
+    out_sim, _, rec_sim = mod_xylo_sim_spike.evolve(
+        input_raster.clip(0, 15), record=True
+    )
+
+    # - Get a Xylo HDK board
+    xylo_hdk_nodes = putils.find_xylo_a3_boards()
+
+    if len(xylo_hdk_nodes) == 0:
+        pytest.skip("A connected XyloAudio 3 HDK is required to run this test")
+
+    daughterboard = xylo_hdk_nodes[0]
+
+    # - Make sure the board is in a clean state after running other tests
+    daughterboard.reset_board_soft()
+
+    # - Init Xylo
+    mod_xylo_spike = x.XyloSamna(daughterboard, config)
+
+    # - Evolve Xylo
+    mod_xylo_spike.reset_state()
+    out_xylo, _, rec_xylo = mod_xylo_spike._evolve_manual(input_raster, record=True)
+
+    # - Assert equality for all outputs and recordings
+    assert np.all(out_sim == out_xylo)
+    # FIXME: These values are shifited by 1 event!
+    # assert np.all(rec_sim["Vmem"] == rec_xylo["Vmem"])
+    # assert np.all(rec_sim["Isyn"] == rec_xylo["Isyn"])
+    # assert np.all(rec_sim["Isyn2"] == rec_xylo["Isyn2"])
     # assert np.all(rec_sim["Vmem_out"] == rec_xylo["Vmem_out"])
     # assert np.all(rec_sim["Isyn_out"] == rec_xylo["Isyn_out"])
     # assert np.all(rec_sim["Spikes"] == rec_xylo["Spikes"])
